f5fad44db09e51270ceb427a29bad7cd3ff2ae65
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-03-25 12:01:49 +0100

    ponovni commit

--- Model/Square.cs
+++ Model/Square.cs
@@ -5,14 +5,19 @@ using System.Text;
 namespace Model
 {
    public class Square
-    {
+    { 
+        
+        
+        public readonly int Row;
+        public readonly int Column;
+
+
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
         }
-        public readonly int Row;
-        public readonly int Column;
+       
     }
 }
 
