d8bdf261df8d9038983a81229ad4551c768bb73d
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-03-25 21:13:39 +0100

    create grid and tests for it

---
+++ Model/Grid.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vsite.Oom.Battleship.Model
+{
+  public  class Grid
+    {
+        public Grid(int rows,int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+
+       public  IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {   
+           throw new NotImplementedException();
+        }
+        public readonly int Rows;
+        public readonly int Columns;
+    }
+}
---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+namespace Vsite.Oom.Battleship.Model.UnitTests
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void GerAvailablePlacementsForShipReturns2PlacementsForShipOfLength3InHorizontalGridx4 ()
+        {
+            Grid g = new Grid(1, 4);
+            var result = g.GetAvailablePlacements(3);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(3, result.First().Count());
+            Assert.AreEqual(3, result.Last().Count());
+        }
+        [TestMethod]
+        public void GerAvailablePlacementsForShipReturns3PlacementsForShipOfLength3InVerticalGrid5x1()
+        {
+            Grid g = new Grid(5,1);
+            var result = g.GetAvailablePlacements(3);
+            Assert.AreEqual(3, result.Count());
+            foreach(var sequence in result )
+            Assert.AreEqual(3, sequence.Count());
+        
+        }
+    }
+}
