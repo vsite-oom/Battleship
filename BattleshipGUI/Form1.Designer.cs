afe1cedb3c3bf4f0c6f0102a84d09f19a7bc09bc
Auro Mazzi <auro.mazzi@gmail.com>
2020-04-27 00:25:17 +0200

    - Create new project for GUI - Add form - Add horizontal and vertical labels -Add deploy fleet button

---
+++ BattleshipGUI/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows.Forms;
+
+namespace Vsite.Oom.Battleship.Model.BattleshipGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(35, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 340);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Deploy Fleet";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 414);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Battleship";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+    }
+}
+
---
+++ BattleshipGUI/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vsite.Oom.Battleship.Model.BattleshipGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int buttonSize = panel1.Height / columns;
+            AddHorizontalLabels(buttonSize);
+            AddVerticalLabels(buttonSize);
+        }
+
+        void AddHorizontalLabels(int buttonSize)
+        {
+            int left = panel1.Left;
+            for (int c = 0; c < columns; ++c)
+            {
+                Label label = new Label
+                {
+                    Top = panel1.Top - buttonSize,
+                    Left = left,
+                    Width = buttonSize,
+                    Height = buttonSize,
+                    Text = ((char)(c + 'A')).ToString(),
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+                Controls.Add(label);
+                left += buttonSize;
+            }
+        }
+
+        void AddVerticalLabels(int buttonSize)
+        {
+            int top = panel1.Top;
+            for (int r = 0; r < rows; ++r)
+            {
+                Label label = new Label
+                {
+                    Top = top,
+                    Left = panel1.Left - buttonSize,
+                    Width = buttonSize,
+                    Height = buttonSize,
+                    Text = (r + 1).ToString(),
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+                Controls.Add(label);
+                top += buttonSize;
+            }
+        }
+
+        int rows = 10;
+        int columns = 10;
+
+    }
+
+}
