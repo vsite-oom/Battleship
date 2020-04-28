3630a7ec4fa61e8205e27719f256e2f07a695913
Robert Bakota <robakota@vsite.hr>
2020-04-28 23:58:54 +0200

    GUI Form with empty grid drawn

---
+++ Flota/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Flota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSetFleet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(405, 405);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnSetFleet
+            // 
+            this.btnSetFleet.Location = new System.Drawing.Point(337, 426);
+            this.btnSetFleet.Name = "btnSetFleet";
+            this.btnSetFleet.Size = new System.Drawing.Size(75, 23);
+            this.btnSetFleet.TabIndex = 1;
+            this.btnSetFleet.Text = "Složi";
+            this.btnSetFleet.UseVisualStyleBackColor = true;
+            this.btnSetFleet.Click += new System.EventHandler(this.btnSetFleet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 461);
+            this.Controls.Add(this.btnSetFleet);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSetFleet;
+    }
+}
+
---
+++ Flota/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flota
+{
+    public partial class Form1 : Form
+    {
+        const int sz = 40;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.LightGray);
+            SolidBrush sb = new SolidBrush(Color.White);
+            Rectangle[] rects = new Rectangle[100];
+            for (int y = 0; y < 10; ++y)
+            {
+                for (int x = 0; x < 10; ++x)
+                {
+                    rects[y * 10 + x] = new Rectangle(x * sz, y * sz, sz, sz);
+                }
+            }
+            g.FillRectangle(sb, 0, 0, 401, 401);
+            g.DrawRectangles(p, rects);
+        }
+
+        private void btnSetFleet_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
