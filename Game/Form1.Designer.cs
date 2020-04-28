28f60f187a14384b0af0884d7e6b9e6950534b40
Ivan Mihin <ivmihin@vsite.hr>
2020-04-28 22:21:45 +0200

    Add GUI for fleet placement on grid

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridPanel = new System.Windows.Forms.Panel();
+            this.randomizeFleet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridPanel.Location = new System.Drawing.Point(35, 35);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.Size = new System.Drawing.Size(411, 411);
+            this.gridPanel.TabIndex = 0;
+            this.gridPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.gridPanel_Paint);
+            // 
+            // randomizeFleet
+            // 
+            this.randomizeFleet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randomizeFleet.Location = new System.Drawing.Point(145, 480);
+            this.randomizeFleet.Name = "randomizeFleet";
+            this.randomizeFleet.Size = new System.Drawing.Size(200, 50);
+            this.randomizeFleet.TabIndex = 1;
+            this.randomizeFleet.Text = "RANDOMIZE";
+            this.randomizeFleet.UseVisualStyleBackColor = true;
+            this.randomizeFleet.Click += new System.EventHandler(this.randomizeFleet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.randomizeFleet);
+            this.Controls.Add(this.gridPanel);
+            this.Name = "Form1";
+            this.Text = "Battleships";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gridPanel;
+        private System.Windows.Forms.Button randomizeFleet;
+    }
+}
+
