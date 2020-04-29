47de4cb2d8ea9ea5a5218c10b82209d7340dc91b
Domagoj Salkić <dosalkic@vsite.hr>
2020-04-29 19:41:56 +0200

    Add RulesSingleton

---
+++ Model/RulesSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class RulesSingleton
+    {
+        private RulesSingleton()
+        {
+            //e.g. read data from configuration file
+            Rows = 10;
+            Columns = 10;
+            ShipLengths = new int[] {5, 4, 4, 3, 3, 3, 2, 2, 2, 2};
+        }
+
+        public static RulesSingleton Instance { get; } = new RulesSingleton();
+
+        public readonly int Rows;
+        public readonly int Columns;
+        public readonly int[] ShipLengths;
+    }
+}
