10913ffe6590eafcb21e14dabac8892ad157612c
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-04-29 22:08:16 +0200

    Added test for square state

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
+using static Vsite.Oom.Battleship.Model.Ship;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
 {
@@ -18,7 +20,18 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
         {
-            throw new NotImplementedException();
+            Square s1 = new Square(1, 4);
+            Square s2 = new Square(1, 5);
+            Square s3 = new Square(1, 6);
+            Ship ship = new Ship(new List<Square> { s1, s2, s3 });
+
+            var result = ship.Hit(s1);
+            result = ship.Hit(s2);
+            result = ship.Hit(s3);
+
+            Assert.AreEqual(SquareState.Sunken, s1.SquareState);
+            Assert.AreEqual(SquareState.Sunken, s2.SquareState);
+            Assert.AreEqual(SquareState.Sunken, s3.SquareState);
         }
     }
 }
