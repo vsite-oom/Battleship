beba3be9e33f0d1c786a8dbf6af27c15b3f58e34
Roland Rostohar <rorostoh@vsite.hr>
2020-04-29 23:07:30 +0200

    Added test for hit result

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -12,6 +13,23 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
 			Square s = new Square(1, 8);
 			Assert.AreEqual(1, s.Row);
 			Assert.AreEqual(8, s.Column);
+		}
+
+		[TestMethod]
+		public void WhenShipIsSunkAllSquaresAreMarkedSunk()
+		{
+			Ship ship = new Ship(new List<Square>{new Square(1,1), new Square(1,2)
+				,new Square(1,3) });
+
+			foreach (Square square in ship.Squares)
+			{
+				ship.Hit(square);
+			}
+
+			Assert.AreEqual(HitResult.Sunk, ship.Hit(new Square(1, 1)));
+			Assert.AreEqual(HitResult.Sunk, ship.Hit(new Square(1, 2)));
+			Assert.AreEqual(HitResult.Sunk, ship.Hit(new Square(1, 3)));
+
 		}
 	}
 }
