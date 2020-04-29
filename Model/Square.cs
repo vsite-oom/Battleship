8237fa40fca5f57f91410e5b1b1248b56c39f39f
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-04-29 21:24:32 +0200

    Add SquareState

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vsite.Oom.Battleship.Model.Ship;
 
 namespace Model
 {
+    public enum SquareState 
+    { 
+        None,
+        Missed,
+        Hit,
+        Sunken
+    }
     public class Square:IEquatable<Square>
     {
         public Square(int row, int column)
@@ -13,10 +22,31 @@ namespace Model
             Row = row;
             Column = column;
             Hit = false;
+            SquareState = SquareState.None;
+        }
+
+        public void SetState(HitResult hitResult)
+        {
+            switch (hitResult)
+            {
+                case HitResult.Missed:
+                    SquareState = SquareState.Missed;
+                    break;
+                case HitResult.Hit:
+                    SquareState = SquareState.Hit;
+                    break;
+                case HitResult.Sunken:
+                    SquareState = SquareState.Sunken;
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
         }
 
         public readonly int Row;
         public readonly int Column;
+        public SquareState SquareState { get; private set; }
 
         public bool Equals(Square other)
         {
