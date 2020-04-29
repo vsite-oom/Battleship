b4f597d76fa64f6363b814c1e68f47b4db417b8d
Ivan Mihin <ivmihin@vsite.hr>
2020-04-29 21:36:42 +0200

    Add Test

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -17,7 +19,15 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
         {
-            throw new NotImplementedException();
+            List<Square> squares = new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            Ship ship = new Ship(squares);
+
+            foreach (var square in squares)
+            {
+                ship.Hit(square);
+            }
+
+            Assert.AreEqual(SquareState.Sunken, ship.Squares.First().SquareState);
         }
     }
 }
