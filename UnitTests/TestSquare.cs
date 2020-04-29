76c43b215465e73ae0b80c330d689f8059522733
Ivan Batinić <ivek81cro@gmail.com>
2020-04-29 21:24:50 +0200

    Add SquareState

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -12,6 +13,14 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Square s = new Square(1, 8);
             Assert.AreEqual(1, s.Row);
             Assert.AreEqual(8, s.Column);
+        }
+
+        [TestMethod]
+        public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
+        {
+            Ship s = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            
+            throw new NotImplementedException();
         }
     }
 }
