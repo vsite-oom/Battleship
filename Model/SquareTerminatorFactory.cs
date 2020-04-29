75d929013490b4e9e31a5e6f1a07fc7d4832112f
Ivan Zoric <ivzoric@vsite.hr>
2020-04-29 21:01:09 +0200

    Add SquareTerminatorFactory

---
+++ Model/SquareTerminatorFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+     public enum ShipAdJoining
+    {
+        None,
+        Corners
+    }
+    public static class SquareTerminatorFactory
+    {
+        public static ISquareTerminator Create(ShipAdJoining adJoining,int rows,int columns)
+        {
+            switch (adJoining)
+            {
+                case ShipAdJoining.None:
+                    return new squareTerminator(rows, columns);
+                case ShipAdJoining.Corners:
+                    return null;
+                default:
+                    Debug.Assert(false);
+                    return null;
+            }
+        }
+    }
+}
--- UnitTests/TestShipwright.cs
+++ UnitTests/TestShipwright.cs
@@ -14,5 +14,13 @@ namespace Vsite.Oom.Battleship.Model
             var fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
             Assert.AreEqual(10, fleet.Ships.Count());
         }
+        [TestMethod]
+        public void CreateFleetMethodCreatesShipsForAGivenTerminator()
+        {
+            var terminator = SquareTerminatorFactory.Create(ShipAdJoining.None,10,10);
+            shipwright sw = new shipwright(10, 10,terminator);
+            var fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
+            Assert.AreEqual(10, fleet.Ships.Count());
+        }
     }
 }
