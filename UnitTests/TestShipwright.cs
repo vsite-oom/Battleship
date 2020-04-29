99201efc4336c01639577830a8cb6755ac926dd8
Domagoj Salkić <dosalkic@vsite.hr>
2020-04-29 21:00:52 +0200

    Add SquareTerminatorFactory

--- UnitTests/TestShipwright.cs
+++ UnitTests/TestShipwright.cs
@@ -13,6 +13,15 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             Shipwright sw = new Shipwright(10,10);
             var fleet = sw.CreateFleet(new int[] {5, 4, 4, 3, 3, 3, 2, 2, 2, 2});
             Assert.AreEqual(10, fleet.Ships.Count());
+        }        
+        
+        [TestMethod]
+        public void CreateFleetMethodCreatesShipsForAGivenTerminator()
+        {
+            var terminator = SquareTerminatorFactory.Create(ShipAdjoining.None, 10, 10);
+            Shipwright sw = new Shipwright(10,10, terminator);
+            var fleet = sw.CreateFleet(new int[] {5, 4, 4, 3, 3, 3, 2, 2, 2, 2});
+            Assert.AreEqual(10, fleet.Ships.Count());
         }
     }
 }
