367ac01ef41d289c9f93cf32781b9183486e7032
Ivan Batinić <ivek81cro@gmail.com>
2020-04-29 21:01:14 +0200

    Add SquareTerminatiorFactory

--- UnitTests/TestShipwright.cs
+++ UnitTests/TestShipwright.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Oom.Battleship.Model;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
 {
@@ -10,6 +11,15 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void CreateFleetCreateShips()
         {
+            Shipwright sw = new Shipwright(10, 10);
+            var fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
+            Assert.AreEqual(10, fleet.Ships.Count());
+        }
+
+        [TestMethod]
+        public void CreateFleetCreateShipsForAGivenTermnator()
+        {
+            var terminator = SqureTerminatorFactory.Create(ShipAdjoining.None, 10, 10);
             Shipwright sw = new Shipwright(10, 10);
             var fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
             Assert.AreEqual(10, fleet.Ships.Count());
