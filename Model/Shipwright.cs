eefc8e07abfe61297e3fdc2f5b4a70cc8db034cc
Ivan Batinić <ivek81cro@gmail.com>
2020-04-29 20:33:05 +0200

    Refactor SquareTerminator

--- Model/Shipwright.cs
+++ Model/Shipwright.cs
@@ -36,10 +36,9 @@ namespace Vsite.Oom.Battleship.Model
             List<int> lengths = new List<int>(shipLengths.OrderByDescending(x => x));
 
             Grid grid = new Grid(rows, columns);
-            SquareTerminator terminator = new SquareTerminator(grid);
+            SquareTerminator terminator = new SquareTerminator(rows, columns);
             Fleet fleet = new Fleet();
 
-            Random random = new Random();
             while (lengths.Count() > 0)
             {
                 var placements = grid.GetAvailablePlacements(lengths[0]);
@@ -54,6 +53,8 @@ namespace Vsite.Oom.Battleship.Model
             return fleet;
         }
 
+
+        Random random = new Random();
         private readonly int rows;
         private readonly int columns;
     }
