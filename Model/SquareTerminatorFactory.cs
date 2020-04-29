f30cb0fa21ebca73d67b387b235807239c3342a7
Paola Kolar <kolar.paola@gmail.com>
2020-04-29 21:00:49 +0200

    Add SquareTerminatorFactory

---
+++ Model/SquareTerminatorFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public enum ShipAdjoining
+    {
+        None,
+        Corners
+    }
+    public static class SquareTerminatorFactory
+    {
+        public static ISquareTerminator Create(ShipAdjoining adjoining, int rows, int columns)
+        {
+            switch (adjoining) {
+                case ShipAdjoining.None:
+                    return new SquareTerminator(rows, columns);
+                case ShipAdjoining.Corners:
+                    return null;
+                default:
+                    Debug.Assert(false);
+                    return null;
+            } 
+        }
+    }
+}
