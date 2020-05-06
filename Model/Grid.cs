37099e1f670ea3d53e849c2abea8c1a3c7ed3986
Domagoj Salkić <dosalkic@vsite.hr>
2020-05-06 20:46:12 +0200

    Create Gunner and implement shooting tactics change

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -51,6 +51,11 @@ namespace Vsite.Oom.Battleship.Model
             }
         }
 
+        public void MarkHitResult(Square square, HitResult hitResult)
+        {
+
+        }
+
         private IEnumerable<Placement> GetAvailableHorizontalPlacements(int length)
         {
             var result = new List<List<Square>>();
