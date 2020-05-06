eaf74e03220cab71cb141e96a47df8effddb97d4
Paola Kolar <kolar.paola@gmail.com>
2020-05-06 20:46:12 +0200

    Create Gunner and implement shooting tactics change

---
+++ Model/Gunner.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random,
+        Surrounding,
+        Inline
+    }
+    public class Gunner
+    {
+        public Gunner(int rows, int columns, IEnumerable<int> shipLengths)
+        {
+            evidenceGrid = new Grid(rows, columns);
+            
+            shipsToShoot = new List<int>(shipLengths.OrderByDescending(l => l));
+            ShootingTactics = ShootingTactics.Random;
+        }
+        public Square NextTarget()
+        {
+            // TODO: implement correctly!
+            lastTarget = new Square(0, 0);
+            return lastTarget;
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        {
+            evidenceGrid.MarkHitResult(lastTarget, hitResult);
+
+            switch (hitResult)
+            {
+                case HitResult.Missed:
+                    return;
+                case HitResult.Sunken:
+                    // eliminate squares around the ship
+                    ShootingTactics = ShootingTactics.Random;
+                    return;
+                case HitResult.Hit:
+                    switch (ShootingTactics)
+                    {
+                        case ShootingTactics.Random:
+                            ShootingTactics = ShootingTactics.Surrounding;
+                            return;
+                        case ShootingTactics.Surrounding:
+                            ShootingTactics = ShootingTactics.Inline;
+                            return;
+                        case ShootingTactics.Inline:
+                            return;
+                    }
+                    break;
+                   
+            }
+
+        }
+
+        private Square lastTarget;
+
+        private Grid evidenceGrid;
+
+        private List<int> shipsToShoot;
+        public ShootingTactics ShootingTactics { get; private set; }
+    }
+}
