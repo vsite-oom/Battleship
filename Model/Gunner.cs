4bcbd37cf225df0d1ca6881fc9efd9c5bba21ca1
Ante Mišković <amiskovic0101@gmail.com>
2020-05-06 21:35:15 +0200

    Implement sinking ship logic

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -20,6 +20,7 @@ namespace Vsite.Oom.Battleship.Model
             evidenceGrid = new Grid(rows, columns);
             shipsToShoot = new List<int>(shipLengths.OrderByDescending(l => l));
             ShootingTactics = ShootingTactics.Random;
+            squareTerminator = new SquareTerminator(rows, columns);
         }
         public Square NextTarget()
         {
@@ -35,13 +36,24 @@ namespace Vsite.Oom.Battleship.Model
                 case HitResult.Missed:
                     return;
                 case HitResult.Sunken:
-                    //eliminate squares around the ship
+                    squaresHit.Add(lastTarget);
+                    squaresHit.OrderBy(s => s.Row+ s.Column);
+                    var toEliminate = squareTerminator.ToEliminate(squaresHit);
+                    foreach(var sq in toEliminate)
+                        evidenceGrid.MarkHitResult(sq, HitResult.Missed);
+                    foreach (var sq in squaresHit)
+                        evidenceGrid.MarkHitResult(sq, HitResult.Sunken);
+
+                    int length = squaresHit.Count();
+                    shipsToShoot.Remove(length);
+                    squaresHit.Clear();
                     ShootingTactics = ShootingTactics.Random;
                     return;
                 case HitResult.Hit:
+                    squaresHit.Add(lastTarget);
+                    squaresHit.OrderBy(s => s.Row + s.Column);
                     switch (ShootingTactics)
                     {
-
                         case ShootingTactics.Random:
                             ShootingTactics = ShootingTactics.Surrounding;
                             return;
@@ -92,8 +104,10 @@ namespace Vsite.Oom.Battleship.Model
         private Square lastTarget;
         private Grid evidenceGrid;
         private List<int> shipsToShoot;
+        private List<Square> squaresHit = new List<Square>();
         public ShootingTactics ShootingTactics { get; private set; }
         private Random random = new Random();
+        private ISquareTerminator squareTerminator;
 
     }
 }
