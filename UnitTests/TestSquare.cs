f3f107ba5a2f3ceb43ddf19ff6d855c601166210
Paško Berišić <paberisi@vsite.hr>
2020-05-06 18:28:45 +0200

    Commit proslog predavanj (29.49.2020.) kasnim ....

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Vsite.Oom.Battleship.Model.UnitTests
@@ -17,10 +18,13 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
         [TestMethod]
         public void WhenShipIsSunkenAllSquaresAreMarkedSunken()
         {
-            // napravi ship
-            // gadaj polja dok ne skines cijeli broj
-            // provjeri LOOOL
-            throw new NotImplementedException();
+            Ship ship = new Ship(new List<Square> { new Square(2, 5), new Square(2, 6), new Square(2, 7) });
+
+            foreach (var square in ship.Squares)
+                ship.Hit(square);
+
+            foreach (var square in ship.Squares)
+                Assert.AreEqual(SquareState.Sunken, square.SquareState);
         }
     }
 }
