d821d4497a9ec8f9928c09f738e3a9439482af4e
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-05-06 21:35:05 +0200

    Impl sinking ship logic

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -24,6 +24,7 @@ namespace Vsite.Oom.Battleship.Model
             shipsToShoot = new List<int>(
             shipLengths.OrderByDescending(l => l));
             ShootingTactics = ShootingTactics.Random;
+            squareTerminator = new SquareTerminator(rows, columns);
         }
         public Square NextTarget()
         {
@@ -40,10 +41,23 @@ namespace Vsite.Oom.Battleship.Model
                 case HitResult.Missed:
                     return;
                 case HitResult.Sunken:
-                    //eliminate squares around the ship
+                    squaresHit.Add(lastTarget);
+                    squaresHit.OrderBy(s => s.Row + s.Column);
+                    var toEliminate = squareTerminator.ToEliminate(squaresHit);
+                    foreach (var sq in toEliminate)
+                        evidenceGrid.MarkHitResult(sq, HitResult.Missed);
+                    foreach (var sq in squaresHit)
+                        evidenceGrid.MarkHitResult(sq, HitResult.Sunken);
+                    evidenceGrid.EliminateSquares(toEliminate);
+                    int length = squaresHit.Count();
+                    shipsToShoot.Remove(length);
+                    squaresHit.Clear();
                     ShootingTactics = ShootingTactics.Random;
+                   
                     return;
                 case HitResult.Hit:
+                    squaresHit.Add(lastTarget);
+                    squaresHit.OrderBy(s => s.Row + s.Column);
                     switch (ShootingTactics)
                     {
                         case ShootingTactics.Random:
@@ -99,10 +113,17 @@ namespace Vsite.Oom.Battleship.Model
    
 
         private Square lastTarget;
+
         private Grid evidenceGrid;
+
         private List<int> shipsToShoot;
 
+        private List<Square> squaresHit = new List<Square>();
+
+        private ISquareTerminator squareTerminator;
+
         private Random random = new Random();
+
         public ShootingTactics ShootingTactics { get; private set; }
     }
 }
