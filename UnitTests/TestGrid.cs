c6e8b71231d2867452c8e6974d272a99eac3860a
Mario Bilic <mario.bilic4@gmail.com>
2020-05-13 20:36:34 +0200

    implement GetSquaresNextTo

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -51,5 +51,65 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             foreach (var sequence in result)
                 Assert.AreEqual(2, sequence.Count());
         }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns3SquaresForGrid1x5RightToSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Right);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid1x5RightToSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Left);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid1x5AboveAndBelowToSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Up);
+            Assert.AreEqual(0, result.Count());
+            result = g.GetSquaresNextTo(new Square(0, 1), Direction.Down);
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns3SquaresForGrid5x1BelowSquare1x0()
+        {
+            Grid g = new Grid(5, 1);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Down);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid5x1AboveSquare1x0()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Up);
+            g.EliminateSquares(new List<Square> { new Square(1, 0) });
+            var result = g.GetAvailablePlacements(2);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid5x1RightAndLeftToSquare1x0()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Right);
+            Assert.AreEqual(0, result.Count());
+            result = g.GetSquaresNextTo(new Square(1, 0), Direction.Left);
+            Assert.AreEqual(0, result.Count());
+        }
     }
 }
