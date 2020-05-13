0482f81a1ebf92e6ef418d3ca3f587fe68fd217c
esvegot <esvegot@HR-00004162>
2020-05-13 20:34:20 +0200

    Implement GetSquaresNextTo() method

--- UnitTests/TestGrid.cs
+++ UnitTests/TestGrid.cs
@@ -50,5 +50,60 @@ namespace Vsite.Oom.Battleship.Model.UnitTests
             foreach (var sequence in result)
                 Assert.AreEqual(2, sequence.Count());
         }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns3SquaresForGrid1x5RightToSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Right);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid1x5LeftToSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Left);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns0SquaresForGrid1x5AboveAndBelowSquare0x1()
+        {
+            Grid g = new Grid(1, 5);
+            var result = g.GetSquaresNextTo(new Square(0, 1), Direction.Up);
+            Assert.AreEqual(0, result.Count());
+
+            result = g.GetSquaresNextTo(new Square(0, 1), Direction.Down);
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns3SquaresForGrid5x1BelowToSquare1x0()
+        {
+            Grid g = new Grid(5, 1);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Down);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns1SquaresForGrid5x1AboveToSquare1x0()
+        {
+            Grid g = new Grid(5, 1);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Up);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresNextToReturns0SquaresForGrid5x1AboveAndBelowSquare1x0()
+        {
+            Grid g = new Grid(5, 1);
+            var result = g.GetSquaresNextTo(new Square(1, 0), Direction.Right);
+            Assert.AreEqual(0, result.Count());
+
+            result = g.GetSquaresNextTo(new Square(1, 0), Direction.Left);
+            Assert.AreEqual(0, result.Count());
+        }
+
     }
 }
