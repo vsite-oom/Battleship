d1927350c2144e6a3e7f55b27be89a6d0eb7b858
Ante Mišković <amiskovic0101@gmail.com>
2020-05-13 20:34:21 +0200

    Implement GetSquaresNextTo Method.

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,13 @@ using System.Threading.Tasks;
 namespace Vsite.Oom.Battleship.Model
 {
     using Placement = IEnumerable<Square>;
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
     public class Grid
     {
         public Grid(int rows, int columns)
@@ -48,6 +56,49 @@ namespace Vsite.Oom.Battleship.Model
         {
             squares[square.Row, square.Column].SetState(hitResult);
         }
+        public IEnumerable<Square> GetSquaresNextTo(Square square, Direction direction)
+        {
+            List<Square> result = new List<Square>();
+            int row = square.Row;
+            int column = square.Column;
+
+            int deltaRow = 0;
+            int deltaColumn = 0;
+            int maxCount = 0;
+            switch (direction)
+            {
+                case Direction.Right:
+                    ++column;
+                    deltaColumn = +1;
+                    maxCount = Columns - column;
+                    break;
+                case Direction.Down:
+                    ++row;
+                    deltaRow = +1;
+                    maxCount = Rows - row;
+                    break;
+                case Direction.Left:
+                    maxCount = column;
+                    --column;
+                    deltaColumn = -1;
+                    break;
+                case Direction.Up:
+                    maxCount = row;
+                    --row;
+                    deltaRow = -1;
+                    break;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+            for (int i = 0; i < maxCount && IsAvailable(row, column); ++i)
+            {
+                result.Add(squares[row, column]);
+                row += deltaRow;
+                column += deltaColumn;
+            }
+            return result;
+        }
         private IEnumerable<Placement> GetAvailableHorizontalPlacements(int length)
         {
             var result = new List<List<Square>>();
@@ -76,7 +127,7 @@ namespace Vsite.Oom.Battleship.Model
                 LimitedQueue<Square> passed = new LimitedQueue<Square>(length);
                 for (int r = 0; r < Rows; ++r)
                 {
-                    if (IsAvailable(r,c))
+                    if (IsAvailable(r, c))
                         passed.Enqueue(squares[r, c]);
                     else
                         passed.Clear();
