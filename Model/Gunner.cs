9fa8f5b830b6cc3c3ea94db45f3772fb772799f6
Paola Kolar <kolar.paola@gmail.com>
2020-05-15 09:29:24 +0200

    Fix failing tests for shooting tactics

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -39,7 +39,7 @@ namespace Vsite.Oom.Battleship.Model
                     return;
                 case HitResult.Sunken:
                     squaresHit.Add(lastTarget);
-                    squaresHit.OrderBy(s => s.Row + s.Column);
+                    squaresHit = squaresHit.OrderBy(s => s.Row + s.Column).ToList();
                     var toEliminate = squareTerminator.ToEliminate(squaresHit);
                     foreach(var sq in toEliminate)
                         evidenceGrid.MarkHitResult(sq, HitResult.Missed);
@@ -52,7 +52,7 @@ namespace Vsite.Oom.Battleship.Model
                     return;
                 case HitResult.Hit:
                     squaresHit.Add(lastTarget);
-                    squaresHit.OrderBy(s => s.Row + s.Column);
+                    squaresHit = squaresHit.OrderBy(s => s.Row + s.Column).ToList();
                     switch (ShootingTactics)
                     {
                         case ShootingTactics.Random:
@@ -99,7 +99,6 @@ namespace Vsite.Oom.Battleship.Model
             int index = random.Next(0,around.Count);
             return around[index].First();
         }
-
 
         private Square SelectRandomly()
         {
