2fdc7236fc58fc1e9e9a26fa26e79bd945766b2a
Ivan Blažeka <ivblazek@vsite.hr>
2020-05-20 21:04:44 +0200

    Extract shooting tactics into separated classes

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -22,11 +22,12 @@ namespace Vsite.Oom.Battleship.Model
             shipsToShoot = new List<int>(shipLengths.OrderByDescending(l => l));
             ShootingTactics = ShootingTactics.Random;
             squareTerminator = new SquareTerminator(rows, columns);
+            targetSelect = new RandomShooting(evidenceGrid);
         }
 
         public Square NextTarget()
         {
-            lastTarget =SelectTarget();
+            lastTarget =targetSelect.NextTarget(shipsToShoot[0]);
             return lastTarget;
         }
 
@@ -60,6 +61,7 @@ namespace Vsite.Oom.Battleship.Model
             if(hitResult == HitResult.Sunken)
             {
                 ShootingTactics = ShootingTactics.Random;
+                targetSelect = new RandomShooting(evidenceGrid);
                 return;
             }
             if(hitResult == HitResult.Hit)
@@ -68,85 +70,16 @@ namespace Vsite.Oom.Battleship.Model
                 {
                     case ShootingTactics.Random:
                         ShootingTactics = ShootingTactics.Surrounding;
+                        targetSelect = new SurroundShooting(evidenceGrid, squaresHit);
                         return;
                     case ShootingTactics.Surrounding:
                         ShootingTactics = ShootingTactics.Inline;
+                        targetSelect = new InlineShooting(evidenceGrid, squaresHit);
                         return;
                     case ShootingTactics.Inline:
                         return;
                 }
             }
-        }
-
-        private Square SelectTarget()
-        {
-            switch (ShootingTactics)
-            {
-                case ShootingTactics.Random:
-                    return SelectRandomly();
-                case ShootingTactics.Surrounding:
-                    return SelectFromAround();
-                case ShootingTactics.Inline:
-                    return SelectInline();
-                default:
-                    Debug.Assert(false);
-                    return null;
-            }
-        }
-
-        private Square SelectRandomly()
-        {
-            var placements = evidenceGrid.GetAvailablePlacements(shipsToShoot[0]);
-            // create simple array of sqaures from arrays of arrays
-            var allCandidates = placements.SelectMany(seq => seq);
-            // create groups with individual squares
-            var groups = allCandidates.GroupBy(sq => sq);
-            // find the number of squares in largest group
-            var maxCount = groups.Max(g => g.Count());
-            // filter only froups that have maxCount elements
-            var largestGroups = groups.Where(g => g.Count() == maxCount);
-            // fetch keys from each group (i.e. square that represents the group)
-            var mostCommon = largestGroups.Select(g => g.Key);
-            if (mostCommon.Count() == 1)
-                return mostCommon.First();
-            int index = random.Next(0, mostCommon.Count());
-            return mostCommon.ElementAt(index);
-        }
-
-        private Square SelectFromAround()
-        {
-            List<IEnumerable<Square>> around = new List<IEnumerable<Square>>();
-            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
-            {
-                var l = evidenceGrid.GetSquaresNextTo(squaresHit.First(), direction);
-                if (l.Count() > 0)
-                    around.Add(l);
-            }
-            if (around.Count == 1)
-                return around[0].First();
-
-            var ordered = around.OrderByDescending(ls => ls.Count());
-            int maxLen = ordered.First().Count();
-            if (maxLen > shipsToShoot[0] - 1)
-                maxLen = shipsToShoot[0] - 1;
-            var longest = ordered.Where(ls => ls.Count() >= maxLen);
-            int index = random.Next(0, longest.Count());
-            return longest.ElementAt(index).First();
-        }
-
-        private Square SelectInline()
-        {
-            var l = evidenceGrid.GetSquaresInline(squaresHit);
-            if (l.Count() == 1)
-                return l.ElementAt(0).First();
-            
-            var ordered = l.OrderByDescending(ls => ls.Count());
-            int maxLen = ordered.First().Count();
-            if (maxLen > shipsToShoot[0] - squaresHit.Length)
-                maxLen = shipsToShoot[0] - squaresHit.Length;
-            var longest = ordered.Where(ls => ls.Count() >= maxLen);
-            int index = random.Next(0, longest.Count());
-            return longest.ElementAt(index).First();
         }
 
         private Square lastTarget;
@@ -160,6 +93,8 @@ namespace Vsite.Oom.Battleship.Model
         private Random random = new Random();
 
         private ISquareTerminator squareTerminator;
+
+        private ITargetSelect targetSelect;
 
         public ShootingTactics ShootingTactics { get; private set; }
     }
