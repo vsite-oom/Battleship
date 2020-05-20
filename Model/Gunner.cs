374f68903a1bae18d75ad4afeee6df1c76c1b855
Ivan Mihin <ivmihin@vsite.hr>
2020-05-20 20:24:15 +0200

    Refactor ProcessHitResult method

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -33,41 +33,52 @@ namespace Vsite.Oom.Battleship.Model
         public void ProcessHitResult(HitResult hitResult)
         {
             evidenceGrid.MarkHitResult(lastTarget, hitResult);
-            switch (hitResult)
+            if (hitResult == HitResult.Missed)
             {
-                case HitResult.Missed:
-                    return;
-                case HitResult.Sunken:
-                    squaresHit.Add(lastTarget);
-                    var toEliminate = squareTerminator.ToEliminate(squaresHit);
-                    foreach (var sq in toEliminate)
-                    {
-                        evidenceGrid.MarkHitResult(sq, HitResult.Missed);
-                    }
-                    foreach (var sq in squaresHit)
-                    {
-                        evidenceGrid.MarkHitResult(sq, HitResult.Sunken);
-                    }
-                    evidenceGrid.EliminateSquares(toEliminate);
-                    int length = squaresHit.Length;
-                    shipsToShoot.Remove(length);
-                    squaresHit.Clear();
-                    ShootingTactics = ShootingTactics.Random;
-                    return;
-                case HitResult.Hit:
-                    squaresHit.Add(lastTarget);
-                    switch (ShootingTactics)
-                    {
-                        case ShootingTactics.Random:
-                            ShootingTactics = ShootingTactics.Surrounding;
-                            return;
-                        case ShootingTactics.Surrounding:
-                            ShootingTactics = ShootingTactics.Inline;
-                            return;
-                        case ShootingTactics.Inline:
-                            return;
-                    }
-                    break;
+                return;
+            }
+            squaresHit.Add(lastTarget);
+            if (hitResult == HitResult.Sunken)
+            {
+                var toEliminate = squareTerminator.ToEliminate(squaresHit);
+                foreach (var sq in toEliminate)
+                {
+                    evidenceGrid.MarkHitResult(sq, HitResult.Missed);
+                }
+                foreach (var sq in squaresHit)
+                {
+                    evidenceGrid.MarkHitResult(sq, HitResult.Sunken);
+                }
+                evidenceGrid.EliminateSquares(toEliminate);
+                int length = squaresHit.Length;
+                shipsToShoot.Remove(length);
+                squaresHit.Clear();
+            }
+
+            ChangeTactics(hitResult); 
+        }
+
+        private void ChangeTactics(HitResult hitResult)
+        {
+            if (hitResult == HitResult.Sunken)
+            {
+                ShootingTactics = ShootingTactics.Random;
+                return;
+            }
+            
+            if (hitResult == HitResult.Hit)
+            {
+                switch (ShootingTactics)
+                {
+                    case ShootingTactics.Random:
+                        ShootingTactics = ShootingTactics.Surrounding;
+                        return;
+                    case ShootingTactics.Surrounding:
+                        ShootingTactics = ShootingTactics.Inline;
+                        return;
+                    case ShootingTactics.Inline:
+                        return;
+                }
             }
         }
 
