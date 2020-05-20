63043ffafeb572e626fc4e78db43e25ffe9c1abc
Ivan Batinić <ivek81cro@gmail.com>
2020-05-20 21:04:13 +0200

    Extrect shooting tactics in separate classes

---
+++ Model/InlineShooting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class InlineShooting : ITargetSelect
+    {
+        public InlineShooting(Grid evidenceGrid, SortedSquares squaresHit)
+        {
+            this.squaresHit = squaresHit;
+            this.evidenceGrid = evidenceGrid;
+        }
+        public Square NextTarget(int shipLength)
+        {
+            var l = evidenceGrid.GetSquaresInline(squaresHit);
+
+            if (l.Count() == 1)
+                return l.ElementAt(0).First();
+            var order = l.OrderByDescending(ls => ls.Count());
+            int maxLen = order.First().Count();
+            if (maxLen > shipLength - squaresHit.Length)
+                maxLen = shipLength - squaresHit.Length;
+            var longest = order.Where(ls => ls.Count() >= maxLen);
+            int index = random.Next(0, longest.Count());
+
+            return longest.ElementAt(index).First();
+        }
+
+        private Random random = new Random();
+        private readonly Grid evidenceGrid;
+        private readonly SortedSquares squaresHit;
+    }
+}
---
+++ Model/SurroundShooting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SurroundShooting : ITargetSelect
+    {
+        public SurroundShooting(Grid evidenceGrid, SortedSquares squaresHit)
+        {
+            this.squaresHit = squaresHit;
+            this.evidenceGrid = evidenceGrid;
+        }
+
+        public Square NextTarget(int shipLength)
+        {            
+            List<IEnumerable<Square>> arround = new List<IEnumerable<Square>>();
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var l = evidenceGrid.GetSquaresNextTo(squaresHit.First(), direction);
+
+                if (l.Count() > 0)
+                    arround.Add(l);
+            }
+
+            if (arround.Count == 1)
+                return arround[0].First();
+            var order = arround.OrderByDescending(ls => ls.Count());
+            int maxLen = order.First().Count();
+            if (maxLen > shipLength - 1)
+                maxLen = shipLength - 1;
+            var longest = order.Where(ls => ls.Count() >= maxLen);
+            int index = random.Next(0, longest.Count());
+
+            return longest.ElementAt(index).First();
+        }
+
+        private Random random = new Random();
+        private readonly SortedSquares squaresHit;
+        private readonly Grid evidenceGrid;
+    }
+}
