953316bb1d1c0c1d69095f0c364d3c68f3ae17f2
Ivan Blažeka <ivblazek@vsite.hr>
2020-05-20 17:52:24 +0200

    Change GUI from TableLayoutPanel to CheckBoxes as Buttons

--- GUI/Battlefield.Designer.cs
+++ GUI/Battlefield.Designer.cs
@@ -28,45 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BattleshipGrid = new System.Windows.Forms.TableLayoutPanel();
             this.buttonArrange = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // BattleshipGrid
-            // 
-            this.BattleshipGrid.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
-            this.BattleshipGrid.ColumnCount = 10;
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53F));
-            this.BattleshipGrid.Location = new System.Drawing.Point(20, 20);
-            this.BattleshipGrid.Margin = new System.Windows.Forms.Padding(0);
-            this.BattleshipGrid.Name = "BattleshipGrid";
-            this.BattleshipGrid.RowCount = 10;
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.BattleshipGrid.Size = new System.Drawing.Size(511, 511);
-            this.BattleshipGrid.TabIndex = 0;
-            this.BattleshipGrid.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.BattleshipGrid_CellPaint);
-            // 
             // buttonArrange
             // 
-            this.buttonArrange.Location = new System.Drawing.Point(555, 508);
+            this.buttonArrange.Location = new System.Drawing.Point(555, 497);
             this.buttonArrange.Name = "buttonArrange";
             this.buttonArrange.Size = new System.Drawing.Size(75, 23);
             this.buttonArrange.TabIndex = 1;
@@ -79,19 +46,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(643, 561);
             this.Controls.Add(this.buttonArrange);
-            this.Controls.Add(this.BattleshipGrid);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Battlefield";
-            this.Text = "Form1";
+            this.Text = "Potapanje brodova";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel BattleshipGrid;
         private System.Windows.Forms.Button buttonArrange;
     }
 }
--- GUI/Battlefield.cs
+++ GUI/Battlefield.cs
@@ -16,19 +16,19 @@ namespace GUI
         public Battlefield()
         {
             InitializeComponent();
-        }
 
-        private void BattleshipGrid_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
-        {
-            for (int i = 0; i < fleet.Ships.Count(); ++i)
+            for (int i = 0; i < numberOfRows; ++i)
             {
-                if (fleet.Ships.ElementAt(i).Squares.Contains(new Square(e.Row, e.Column)))
+                for (int j = 0; j < numberOfColumns; ++j)
                 {
-                    e.Graphics.FillRectangle(Brushes.Blue, e.CellBounds);
-                    break;
+                    CheckBox cb = new CheckBox();
+                    cb.Appearance = Appearance.Button;
+                    cb.Location = new System.Drawing.Point(20 + j * 50, 20 + i * 50);
+                    cb.Size = new System.Drawing.Size(50, 50);
+                    cb.Enabled = false;
+                    this.Controls.Add(cb);
+                    BattleGrid.Add(cb);
                 }
-                else
-                    e.Graphics.FillRectangle(Brushes.White, e.CellBounds);
             }
         }
 
@@ -36,9 +36,28 @@ namespace GUI
         {
             Shipwright sw = new Shipwright(10, 10);
             fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
-            BattleshipGrid.Invalidate();
+
+            for (int i = 0; i < numberOfRows; ++i)
+            {
+                for (int j = 0; j < numberOfColumns; ++j)
+                {
+                    for (int k = 0; k < fleet.Ships.Count(); ++k)
+                    {
+                        if (fleet.Ships.ElementAt(k).Squares.Contains(new Square(i, j)))
+                        {
+                            BattleGrid[i * 10 + j].BackColor = System.Drawing.Color.Blue;
+                            break;
+                        }
+                        else
+                            BattleGrid[i * 10 + j].BackColor = System.Drawing.Color.White;
+                    }
+                }
+            }
         }
 
         Fleet fleet = new Fleet();
+        List<CheckBox> BattleGrid = new List<CheckBox>();
+        int numberOfRows = 10;
+        int numberOfColumns = 10;
     }
 }
