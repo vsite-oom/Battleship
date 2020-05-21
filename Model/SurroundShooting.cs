9286fb8f4bacea901562318a13bc7ad99061a0b2
Mario Bilic <mario.bilic4@gmail.com>
2020-05-21 22:19:23 +0200

    Extract ShootingTacitcs into separate classes

---
+++ Model/SurroundShooting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SurroundShooting : ITargetSelect
+    {
+        public SurroundShooting(Grid evidenceGrid, SortedSquares squaresHit)
+        {
+            this.squaresHit = squaresHit;
+            this.evidenceGrid = evidenceGrid;
+        }
+        public Square NextTarget(int shipLength)
+        {
+            List<IEnumerable<Square>> around = new List<IEnumerable<Square>>();
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var l = evidenceGrid.GetSquaresNextTo(squaresHit.First(), direction);
+                if (l.Count() > 0)
+                    around.Add(l);
+            }
+            if (around.Count == 1)
+                return around[0].First();
+
+            var ordered = around.OrderByDescending(ls => ls.Count());
+            int maxLen = ordered.First().Count();
+            if (maxLen > shipLength - 1)
+                maxLen = shipLength - 1;
+            var longest = ordered.Where(ls => ls.Count() >= maxLen);
+            int index = random.Next(0, longest.Count());
+            return longest.ElementAt(index).First();
+        }
+
+        private Random random = new Random();
+        private readonly Grid evidenceGrid;
+        private readonly SortedSquares squaresHit;
+    }
+}
