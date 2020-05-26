f5f218778a0b83ad2e87ca34226bc20f36bfd496
Roland Rostohar <rorostoh@vsite.hr>
2020-05-26 12:42:15 +0200

    Refactor processHitResult method

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -46,48 +46,59 @@ namespace Vsite.Oom.Battleship.Model
 
 			evidenceGrid.MarkHitResult(lastTarget, hitResult);
 
-			switch (hitResult)
+			if (hitResult == HitResult.Missed)
+				return;
+
+			squaresHit.Add(lastTarget);
+
+			if (hitResult == HitResult.Sunk)
 			{
-				case HitResult.Missed:
-					return;
-				case HitResult.Sunk:
-					squaresHit.Add(lastTarget);
-					var toEliminate = squareTerminator.ToEliminate(squaresHit);
+				var toEliminate = squareTerminator.ToEliminate(squaresHit);
 
-					foreach (var sq in toEliminate)
-						evidenceGrid.MarkHitResult(sq,HitResult.Missed);
+				foreach (var sq in toEliminate)
+					evidenceGrid.MarkHitResult(sq, HitResult.Missed);
 
-					foreach (var sq in squaresHit)
-						evidenceGrid.MarkHitResult(sq, HitResult.Sunk);
+				foreach (var sq in squaresHit)
+					evidenceGrid.MarkHitResult(sq, HitResult.Sunk);
 
 
-					int length = squaresHit.Length;
-					shipsToShoot.Remove(length);
-					squaresHit.Clear();
-
-					ShootingTactics = ShootingTactics.Random;
-					return;
-				case HitResult.Hit:
-
-					squaresHit.Add(lastTarget);
-
-					switch (ShootingTactics)
-					{
-						case ShootingTactics.Random:
-						ShootingTactics = ShootingTactics.Surrounding;
-							return;
-
-						case ShootingTactics.Surrounding:
-						ShootingTactics = ShootingTactics.Inline;
-							return;
-
-						case ShootingTactics.Inline:
-							return;
-
-					}
-					break;
+				int length = squaresHit.Length;
+				shipsToShoot.Remove(length);
+				squaresHit.Clear();
 			}
 
+			ChangeTactics(hitResult);
+
+
+
+		}
+
+		private void ChangeTactics(HitResult hitResult)
+		{
+
+			if (hitResult == HitResult.Sunk)
+			{
+				ShootingTactics = ShootingTactics.Random;
+				return;
+			}
+
+			if (hitResult == HitResult.Hit)
+			{
+				switch (ShootingTactics)
+				{
+					case ShootingTactics.Random:
+						ShootingTactics = ShootingTactics.Surrounding;
+						return;
+
+					case ShootingTactics.Surrounding:
+						ShootingTactics = ShootingTactics.Inline;
+						return;
+
+					case ShootingTactics.Inline:
+						return;
+
+				}
+			}
 		}
 
 		private Square SelectTarget()
