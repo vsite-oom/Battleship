9254e006ba4f9156b173a55290edb3c07dbb3fcc
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-05-27 20:59:55 +0200

    add shooting tactics factory

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -24,7 +24,8 @@ namespace Vsite.Oom.Battleship.Model
             shipsToShoot = new List<int>(shipLengths.OrderByDescending(l => l));
             ShootingTactics = ShootingTactics.Random;
             squareTerminator = new SquareTerminator(rows, columns);
-            targetSelect = new RandomShooting(evidenceGrid, shipsToShoot);
+            shootingTacticsFactory = new ShootingTacticsFactory(evidenceGrid,squaresHit,shipsToShoot);
+            targetSelect = shootingTacticsFactory.GetTactics(ShootingTactics.Random);
         }
         public Square NextTarget()
         {
@@ -69,7 +70,7 @@ namespace Vsite.Oom.Battleship.Model
             if (hitResult == HitResult.Sunken)
             {
                 ShootingTactics = ShootingTactics.Random;
-                targetSelect = new RandomShooting(evidenceGrid, shipsToShoot);
+                targetSelect = shootingTacticsFactory.GetTactics(ShootingTactics);
                 return;
             }
             if (hitResult == HitResult.Hit)
@@ -78,13 +79,11 @@ namespace Vsite.Oom.Battleship.Model
                 {
                     case ShootingTactics.Random:
                         ShootingTactics = ShootingTactics.Surrounding;
-                        targetSelect = new SurroundingShooting(evidenceGrid, squaresHit, shipsToShoot);
-                        return;
+                        break;
                     case ShootingTactics.Surrounding:
 
                         ShootingTactics = ShootingTactics.Inline;
-                        targetSelect = new InlineShooting(evidenceGrid, squaresHit,shipsToShoot);
-                        return;
+                        break;
                     case ShootingTactics.Inline:
                         return;
                 }
@@ -103,6 +102,7 @@ namespace Vsite.Oom.Battleship.Model
         private Random random = new Random();
         private ITargetSelect targetSelect;
         private ISquareTerminator squareTerminator;
+        private ShootingTacticsFactory shootingTacticsFactory;
         public ShootingTactics ShootingTactics { get; private set; }
     }
 }
