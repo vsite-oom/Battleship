cfc1e2cf66fd215e43f7fd2e6630f748d6ece7f0
Borna Brišar Ivković <bobrisar@vsite.hr>
2020-05-27 21:01:04 +0200

    Add ShootingTactics Factory

--- Model/Gunner.cs
+++ Model/Gunner.cs
@@ -22,7 +22,8 @@ namespace Vsite.Oom.Battleship.Model
             shipsToShoot = new List<int>(shipLengths.OrderByDescending(l => l));
             ShootingTactics = ShootingTactics.Random;
             squareTerminator = new SquareTerminator(rows, columns);
-            targetSelect = new RandomShooting(evidenceGrid, shipsToShoot);
+            shootingTacticsFactory = new ShootingTacticsFactory(evidenceGrid, squaresHit, shipsToShoot);
+            targetSelect = shootingTacticsFactory.GetTactics(ShootingTactics.Random);
         }
 
         public Square NextTarget()
@@ -59,7 +60,6 @@ namespace Vsite.Oom.Battleship.Model
             if (hitResult == HitResult.Sunken)
             {
                 ShootingTactics = ShootingTactics.Random;
-                targetSelect = new RandomShooting(evidenceGrid, shipsToShoot);
                 return;
             }
             if (hitResult == HitResult.Hit)
@@ -68,16 +68,15 @@ namespace Vsite.Oom.Battleship.Model
                 {
                     case ShootingTactics.Random:
                         ShootingTactics = ShootingTactics.Surrounding;
-                        targetSelect = new SurroundShooting(evidenceGrid, squaresHit, shipsToShoot);
-                        return;
+                        break;
                     case ShootingTactics.Surrounding:
                         ShootingTactics = ShootingTactics.Inline;
-                        targetSelect = new InlineShooting(evidenceGrid, squaresHit, shipsToShoot);
-                        return;
+                        break;
                     case ShootingTactics.Inline:
                         return;
                 }
             }
+            targetSelect = shootingTacticsFactory.GetTactics(ShootingTactics);
         }
         private Square lastTarget;
         private Grid evidenceGrid;
@@ -87,5 +86,6 @@ namespace Vsite.Oom.Battleship.Model
         private ISquareTerminator squareTerminator;
         private ITargetSelect targetSelect;
         public ShootingTactics ShootingTactics { get; private set; }
+        private ShootingTacticsFactory shootingTacticsFactory;
     }
 }
