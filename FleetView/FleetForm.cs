5facad2875529c2da5a5ee61e2387aa7b6ca3498
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-06-10 01:37:27 +0200

    added onclick events

--- FleetView/FleetForm.cs
+++ FleetView/FleetForm.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Windows.Forms;
 using Vsite.Oom.Battleship.Model;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FleetView
 {
     public partial class FleetForm : Form
     {
 
-        int numOfRows = 10;
-        int numOfCols = 10;
-        Button[,] userPanel = new Button[10, 10];
-        Button[,] pcPanel = new Button[10, 10];
+        private int numOfRows = 10;
+        private int numOfCols = 10;
+        private GridButton[,] userPanel = new GridButton[10, 10];
+        private GridButton[,] pcPanel = new GridButton[10, 10];
+        private Fleet userFleet;
+        private Fleet pcFleet;
+        private Gunner gunner;
+        private int[] sizeOfShip = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
         public FleetForm()
         {
             InitializeComponent();
@@ -18,38 +25,109 @@ namespace FleetView
             DrawPanel(pcPanel, 600);
 
         }
-        private void DrawPanel(Button[,] panel, int left)
+        private void DrawPanel(GridButton[,] panel, int left)
         {
             for (int i = 0; i < numOfRows; i++)
             {
                 for (int j = 0; j < numOfCols; j++)
                 {
-                    panel[i, j] = new Button();
-                    panel[i, j].BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+                    panel[i, j] = new GridButton();
+                    panel[i, j].i = i;
+                    panel[i, j].j = j;
+                    panel[i, j].BackColor = System.Drawing.SystemColors.ControlDark;
                     panel[i, j].Location = new System.Drawing.Point(left + i * 40, 60 + j * 40);
                     panel[i, j].Size = new System.Drawing.Size(40, 40);
                     panel[i, j].TabStop = false;
+                    panel[i, j].Click += ClickSquareEvent;
                     this.Controls.Add(panel[i, j]);
                 }
             }
         }
-        private void buttonQuitOnClick(object sender, EventArgs e)
+        private void ClickSquareEvent(object sender, EventArgs e)
+        {
+            GridButton button = sender as GridButton;
+            Square squareClicked = new Square(button.i, button.j);
+            HitResult result = pcFleet.Hit(squareClicked);
+            switch (result)
+            {
+                case HitResult.Hit:
+                    {
+                        button.BackColor = Color.Red;
+                        break;
+                    }
+                case HitResult.Missed:
+                    {
+                        button.BackColor = Color.Black;
+                        PcPlay();
+                        break;
+                    }
+                case HitResult.Sunken:
+                    {
+                        foreach (var sunkenSquare in pcFleet.Ships.Where(s => s.Squares.Contains(squareClicked)).SelectMany(s => s.Squares))
+                            pcPanel[sunkenSquare.Row, sunkenSquare.Col].BackColor = Color.DarkMagenta;
+                        break;
+                    }
+            }
+        }
+
+        private async Task PcPlay()
+        {
+            await Task.Delay(1000);
+            Square square = gunner.NextTarget();
+            HitResult result = userFleet.Hit(square);
+            gunner.ProcessHitResult(result);
+            switch (result)
+            {
+                case HitResult.Hit:
+                    {
+                        userPanel[square.Row, square.Col].BackColor = Color.Red;
+                        await PcPlay();
+                        break;
+                    }
+                case HitResult.Missed:
+                    {
+                        userPanel[square.Row, square.Col].BackColor = Color.Black;
+                        break;
+                    }
+                case HitResult.Sunken:
+                    {
+                        foreach (var sunkenSquare in userFleet.Ships.Where(s => s.Squares.Contains(square)).SelectMany(s => s.Squares))
+                            userPanel[sunkenSquare.Row, sunkenSquare.Col].BackColor = Color.DarkMagenta;
+                        await PcPlay();
+                        break;
+                    }
+            }
+        }
+
+        private void ButtonQuitOnClick(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        private void drawButton(object sender, EventArgs e)
+        private void DrawButton(object sender, EventArgs e)
         {
+            playButton.Enabled = true;
             ResetButtons(userPanel);
-            int[] sizeOfShip = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+            ResetButtons(pcPanel);
 
             Shipwright ship = new Shipwright(numOfRows, numOfCols);
             var fleet = ship.CreateFleet(sizeOfShip);
+            var fleetPc = ship.CreateFleet(sizeOfShip);
+            userFleet = fleet;
+            pcFleet = fleetPc;
+            gunner = new Gunner(numOfRows, numOfCols, sizeOfShip);
             foreach (Ship ships in fleet.Ships)
             {
                 foreach (Square field in ships.Squares)
                 {
                     userPanel[field.Row, field.Col].BackColor = System.Drawing.SystemColors.ControlDark;
+                }
+            }
+            foreach (Ship ships in fleetPc.Ships)
+            {
+                foreach (Square field in ships.Squares)
+                {
+                    pcPanel[field.Row, field.Col].BackColor = System.Drawing.SystemColors.ControlDark;
                 }
             }
         }
@@ -64,9 +142,10 @@ namespace FleetView
                 }
             }
         }
-        private void fleetgrid_Click(object sender, EventArgs e)
+        private void playButton_Click(object sender, EventArgs e)
         {
-
+            buttonDraw.Enabled = false;
+            playButton.Enabled = false;
         }
     }
 }
---
+++ FleetView/GridButton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FleetView
+{
+    class GridButton : Button
+    {
+        public int i;
+        public int j;
+    }
+}
