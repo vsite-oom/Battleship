a65709a0aa3e3f51fa777c5189f931d0d67cbc95
Marko Boroš <Marko.Boros@vsite.hr>
2020-06-14 22:13:04 +0200

    Add player input shooting and drawing on computer grid

--- BattleshipGUI/GridLayout.cs
+++ BattleshipGUI/GridLayout.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vsite.Oom.Battleship.Model;
@@ -20,10 +21,12 @@ namespace BattleshipGUI
     {
         Player currentPlayer = Player.Player;
         Gunner gunner;
+        static int size = 10;
         Fleet playerFleet;
         Fleet computerFleet;
-        Grid playerEvidenceGrid=new Grid(10,10);
-        List<Square> hitList=new List<Square>();
+        Grid playerEvidenceGrid=new Grid(size,size);
+        Grid playerShootingGrid=new Grid(size,size);
+        
         readonly int[] ships = new int[] { 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
         public GridLayout()
         {
@@ -44,6 +47,7 @@ namespace BattleshipGUI
             textBox1.Text = "";
             textBox2.Text = "";
             playerFleet = null;
+            computerFleet = null;
             Invalidate();
         }
         private void StartNewGame()
@@ -56,6 +60,8 @@ namespace BattleshipGUI
             button6.Enabled = true;
             textBox1.Enabled = true;
             textBox2.Enabled = true;
+            
+            
             WhoFirst();
         }
 
@@ -67,6 +73,13 @@ namespace BattleshipGUI
                 return;
             }
             currentPlayer = Player.Computer;
+            FireAtPlayer();
+        }
+
+        private void FireAtPlayer()
+        {
+            Thread.Sleep(1000);
+            Shoot();
         }
 
         private void DisplayDefeat()
@@ -96,7 +109,7 @@ namespace BattleshipGUI
         private void button1_Click(object sender, EventArgs e)
         {
             //draw player playerFleet
-            var shipBuilder = new Shipwright(10, 10);
+            var shipBuilder = new Shipwright(size, size);
             try
             {
                 playerFleet = shipBuilder.CreateFleet(ships);
@@ -105,7 +118,14 @@ namespace BattleshipGUI
             {
                 playerFleet = shipBuilder.CreateFleet(ships);
             }
-
+            try
+            {
+                computerFleet = shipBuilder.CreateFleet(ships);
+            }
+            catch
+            {
+                computerFleet = shipBuilder.CreateFleet(ships);
+            }
             Invalidate();
         }
         private void button2_Click(object sender, EventArgs e)
@@ -122,7 +142,7 @@ namespace BattleshipGUI
                 MessageBox.Show("No fleet!");
                 return;
             }
-            gunner = new Gunner(10, 10, ships);
+            gunner = new Gunner(size, size, ships);
             StartNewGame();
         }
 
@@ -137,31 +157,15 @@ namespace BattleshipGUI
         }
         private void button5_Click(object sender, EventArgs e)
         {
-            //From textbox1 mark a specific cube as hit/sunken
-            try
-            {
-                //Translator(textBox1.Text);
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                MessageBox.Show("Wrong input please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            textBox1.Text = "";
-            Invalidate();
+            //Testing purpouses
+            //Shoots With computer
+            Shoot();
+
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            Square square = new Square(0, 0);
-            try
-            {
-               // square=Translator(textBox2.Text);
-            }
-            catch(ArgumentOutOfRangeException)
-            {
-                MessageBox.Show("Wrong input please try again","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
-            }
-            Shoot(square);
+            Shoot();
         }
 
         private bool FleetIsSunken(Fleet fleet)
@@ -186,63 +190,125 @@ namespace BattleshipGUI
 
         private Square Translator(string target)
         {
+            if (target.Equals(""))
+            {
+                return new Square(-1, -1);
+            }
             int column = char.ToUpper(target[0]) - 'A';
 
             target.Trim();
-            if (column < 0 || column >= 10)
+            if (column < 0 || column >= size)
                 throw new ArgumentOutOfRangeException();
             int i = 1;
             while (!char.IsDigit(target[i]))
                 ++i;
             int row = int.Parse(target.Substring(i)) - 1;
-            if (row >= 10)
+            if (row >= size)
                 throw new ArgumentOutOfRangeException();
-            return new Square(row, column);
+            return new Square(column, row);
 
         }
-        private void Shoot(Square square)
+        private void Shoot()
         {
-            //Notify grid/square/fleet that the field is shot
-            //call paint to paint it
-
-            square = gunner.NextTarget();
-            HitResult hitResult = playerFleet.Hit(square);
-            gunner.ProcessHitResult(hitResult);
-            playerEvidenceGrid.MarkHitResult(square, hitResult);
-            textBox2.Text = "";
-            textBox1.Text = square.Column.ToString() + square.Row.ToString();
+            Square square;
+            if (currentPlayer == Player.Computer)
+            {
+                square = gunner.NextTarget();
+                HitResult hitResult = playerFleet.Hit(square);
+                gunner.ProcessHitResult(hitResult);
+                playerEvidenceGrid.MarkHitResult(square, hitResult);
+            }
+            else
+            {
+                //square = Translator(textBox2.Text);
+                //if (square.Row == -1 || square.Column == -1)
+                //    return;
+                square = gunner.NextTarget();
+                HitResult hitResult = computerFleet.Hit(square);
+                gunner.ProcessHitResult(hitResult);
+                playerShootingGrid.MarkHitResult(square,computerFleet.Hit(square));
+                textBox2.Text = "";
+                textBox1.Text = square.Column.ToString() + square.Row.ToString();
+            }
+            Invalidate();
             if (FleetIsSunken(playerFleet))
             {
                 DisplayDefeat();
-            }   
+            }
+            else if (FleetIsSunken(computerFleet))
+            {
+                DisplayVictory();
+            }
         }
         //Drawing
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            DrawPlayerGrid(e.Graphics, e);
+            PaintPlayerGrid(e.Graphics, e);
+            PaintComputerGrid(e.Graphics, e);
+
             if (!GameIsStarted())
             {
                 DrawGrids(e.Graphics);
             }
+        
             
         }
 
-        private void DrawPlayerGrid(Graphics graphics,PaintEventArgs e)
+        private void PaintComputerGrid(Graphics graphics, PaintEventArgs e)
         {
-            graphics = panel1.CreateGraphics();
+            graphics = panel2.CreateGraphics();
             Pen myPen = new Pen(Brushes.Black, 1);
-            Font myFont = new Font("Arial", 10);
-            int lines = 10;
+            Font myFont = new Font("Arial", size);
+            int lines = size;
             float xspace = panel1.Height / lines;
             float yspace = panel1.Width / lines - 1;
             if (playerEvidenceGrid == null)
             {
                 throw new NullReferenceException();
             }
+            foreach (var square in playerShootingGrid.GetSquares())
+            {
+                var rect = new Rectangle((int)(square.Row * xspace) + 1, (int)(square.Column * yspace) + 1, (int)xspace - 1, (int)yspace - 1);
+
+                if (square.SquareState == SquareState.Missed)
+                {
+                    graphics.FillRectangle(Brushes.LightSlateGray, rect);
+
+                }
+                if (square.SquareState == SquareState.Hit && square.SquareState != SquareState.Sunken)
+                {
+                    graphics.FillRectangle(Brushes.Red, rect);
+                }
+                if (square.SquareState == SquareState.Sunken)
+                {
+                    ColorShipWithSquare(square, xspace, yspace, graphics, computerFleet);
+                    DrawGrids(e.Graphics);
+                }
+            }
+            if (!GameIsStarted())
+            {
+                playerShootingGrid = new Grid(size, size);
+                DrawEmptyField(graphics, xspace, yspace, playerShootingGrid);
+
+            }
+        }
+
+        private void PaintPlayerGrid(Graphics graphics,PaintEventArgs e)
+        {
+            graphics = panel1.CreateGraphics();
+            Pen myPen = new Pen(Brushes.Black, 1);
+            Font myFont = new Font("Arial", size);
+            int lines = size;
+            float xspace = panel1.Height / lines;
+            float yspace = panel1.Width / lines - 1;
+            if (playerShootingGrid == null)
+            {
+                throw new NullReferenceException();
+            }
             if (playerFleet == null)
             {
-                DrawEmptyField(graphics, xspace, yspace);
+                DrawEmptyField(graphics, xspace, yspace,playerEvidenceGrid);
                 return;
             }
             foreach (var square in playerEvidenceGrid.GetSquares())
@@ -261,20 +327,23 @@ namespace BattleshipGUI
                 if (square.SquareState == SquareState.Sunken)
                 {
                     ColorShipWithSquare(square, xspace, yspace, graphics,playerFleet);
+                    DrawSurrounding(square, xspace, yspace, graphics);
                     DrawGrids(e.Graphics);
                 }
             }
             if (!GameIsStarted())
             {
-                playerEvidenceGrid = new Grid(10, 10);
-                foreach (var square in playerEvidenceGrid.GetSquares())
-                {
-                    var rect = new Rectangle((int)(square.Row * xspace) + 1, (int)(square.Column * yspace) + 1, (int)xspace - 1, (int)yspace - 1);
-                    graphics.FillRectangle(Brushes.LightSteelBlue, rect);
-                }
+                playerEvidenceGrid = new Grid(size, size);
+                DrawEmptyField(graphics, xspace, yspace, playerEvidenceGrid);
                     DrawShips(graphics, xspace, yspace);
 
             }
+        }
+
+        private void DrawSurrounding(Square square, float xspace, float yspace, Graphics graphics)
+        {
+            //TODO: implement this
+            return;
         }
 
         private void DrawShips(Graphics graphics, float xspace, float yspace)
@@ -285,9 +354,9 @@ namespace BattleshipGUI
             }
         }
 
-        private void DrawEmptyField(Graphics graphics, float xspace, float yspace)
+        private void DrawEmptyField(Graphics graphics, float xspace, float yspace,Grid grid)
         {
-            foreach (var square in playerEvidenceGrid.GetSquares())
+            foreach (var square in grid.GetSquares())
             {
                 var rect = new Rectangle((int)(square.Row * xspace) + 1, (int)(square.Column * yspace) + 1, (int)xspace - 1, (int)yspace - 1);
                 graphics.FillRectangle(Brushes.LightSteelBlue, rect);
@@ -308,8 +377,7 @@ namespace BattleshipGUI
             {
 
                 var rect = new RectangleF((int)(squares.Row * xspace) + 1, (int)(squares.Column * yspace) + 1, (int)xspace - 1, (int)yspace - 1);
-                System.Drawing.RectangleF.Union(rect, rectf);
-                //graphics.FillRectangle(Brushes.Yellow, rect);
+                RectangleF.Union(rect, rectf);
                 rectf = RectangleF.Union(rectf, rect);
             }
             Rectangle unionRect = Rectangle.Truncate(rectf);
@@ -340,8 +408,8 @@ namespace BattleshipGUI
         private void DrawGrid(Graphics graphics)
         {
             Pen mypen = new Pen(Brushes.Black, 1);
-            Font myfont = new Font("Arial", 10);
-            int lines = 10;
+            Font myfont = new Font("Arial", size);
+            int lines = size;
             float x = 0;
             float y = 0;
             float xspace = panel1.Height / lines;
