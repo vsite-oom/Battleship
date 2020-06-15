ed6a58ca505e0b436880b78ae088dcde674b7b2e
Darijb <darij.b@gmail.com>
2020-06-15 09:14:32 +0200

    Reworked , add table

--- BrojGadjanjaDoPrvogPogodjenog/Form1.Designer.cs
+++ BrojGadjanjaDoPrvogPogodjenog/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BrojGadjanjaDoPrvogPogodjenog
+﻿namespace NasumicnaTaktikaGadjanja
 {
     partial class Form1
     {
@@ -28,67 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.prosjekPokusaja1 = new System.Windows.Forms.Label();
-            this.prosjekPokusaja2 = new System.Windows.Forms.Label();
             this.preostaloPonavljanja = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.working = new System.Windows.Forms.Label();
             this.StopButton = new System.Windows.Forms.Button();
+            this.resultGridView = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.repetitionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(83, 69);
+            this.button1.Location = new System.Drawing.Point(40, 613);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 49);
             this.button1.TabIndex = 0;
-            this.button1.Text = "1000";
+            this.button1.Text = "x1000";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(443, 78);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "LargestShip";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(624, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "OneSquare";
-            // 
-            // prosjekPokusaja1
-            // 
-            this.prosjekPokusaja1.AutoSize = true;
-            this.prosjekPokusaja1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prosjekPokusaja1.Location = new System.Drawing.Point(472, 116);
-            this.prosjekPokusaja1.Name = "prosjekPokusaja1";
-            this.prosjekPokusaja1.Size = new System.Drawing.Size(0, 25);
-            this.prosjekPokusaja1.TabIndex = 3;
-            // 
-            // prosjekPokusaja2
-            // 
-            this.prosjekPokusaja2.AutoSize = true;
-            this.prosjekPokusaja2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prosjekPokusaja2.Location = new System.Drawing.Point(654, 116);
-            this.prosjekPokusaja2.Name = "prosjekPokusaja2";
-            this.prosjekPokusaja2.Size = new System.Drawing.Size(0, 25);
-            this.prosjekPokusaja2.TabIndex = 4;
             // 
             // preostaloPonavljanja
             // 
@@ -101,43 +66,43 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(83, 135);
+            this.button2.Location = new System.Drawing.Point(190, 614);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 48);
             this.button2.TabIndex = 6;
-            this.button2.Text = "10000";
+            this.button2.Text = "x10000";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(83, 198);
+            this.button3.Location = new System.Drawing.Point(338, 613);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(124, 49);
             this.button3.TabIndex = 7;
-            this.button3.Text = "100000";
+            this.button3.Text = "x100000";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(83, 304);
+            this.progressBar1.Location = new System.Drawing.Point(12, 578);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(250, 23);
+            this.progressBar1.Size = new System.Drawing.Size(935, 10);
             this.progressBar1.TabIndex = 8;
             // 
             // working
             // 
             this.working.AutoSize = true;
-            this.working.Location = new System.Drawing.Point(163, 288);
+            this.working.Location = new System.Drawing.Point(429, 591);
             this.working.Name = "working";
             this.working.Size = new System.Drawing.Size(0, 13);
             this.working.TabIndex = 9;
             // 
             // StopButton
             // 
-            this.StopButton.Location = new System.Drawing.Point(83, 362);
+            this.StopButton.Location = new System.Drawing.Point(717, 613);
             this.StopButton.Name = "StopButton";
             this.StopButton.Size = new System.Drawing.Size(109, 38);
             this.StopButton.TabIndex = 10;
@@ -145,24 +110,77 @@
             this.StopButton.UseVisualStyleBackColor = true;
             this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
             // 
+            // resultGridView
+            // 
+            this.resultGridView.AllowUserToDeleteRows = false;
+            this.resultGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.repetitionColumn,
+            this.tip});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.resultGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this.resultGridView.Location = new System.Drawing.Point(12, 14);
+            this.resultGridView.Name = "resultGridView";
+            this.resultGridView.ReadOnly = true;
+            this.resultGridView.Size = new System.Drawing.Size(935, 558);
+            this.resultGridView.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(497, 613);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 49);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "x500000";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // index
+            // 
+            this.index.HeaderText = "id";
+            this.index.Name = "index";
+            this.index.ReadOnly = true;
+            this.index.Width = 30;
+            // 
+            // repetitionColumn
+            // 
+            this.repetitionColumn.HeaderText = "Num Of Rep";
+            this.repetitionColumn.Name = "repetitionColumn";
+            this.repetitionColumn.ReadOnly = true;
+            this.repetitionColumn.Width = 90;
+            // 
+            // tip
+            // 
+            this.tip.HeaderText = "Type";
+            this.tip.Name = "tip";
+            this.tip.ReadOnly = true;
+            this.tip.Width = 70;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(959, 674);
+            this.Controls.Add(this.resultGridView);
             this.Controls.Add(this.StopButton);
             this.Controls.Add(this.working);
             this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.preostaloPonavljanja);
-            this.Controls.Add(this.prosjekPokusaja2);
-            this.Controls.Add(this.prosjekPokusaja1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.resultGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,16 +189,17 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label prosjekPokusaja1;
-        private System.Windows.Forms.Label prosjekPokusaja2;
         private System.Windows.Forms.Label preostaloPonavljanja;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label working;
         private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.DataGridView resultGridView;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn repetitionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tip;
     }
 }
 
--- BrojGadjanjaDoPrvogPogodjenog/Form1.cs
+++ BrojGadjanjaDoPrvogPogodjenog/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Vsite.Oom.Battleship.Model;
 
-namespace BrojGadjanjaDoPrvogPogodjenog
+namespace NasumicnaTaktikaGadjanja
 {
     public partial class Form1 : Form
     {
@@ -20,10 +21,13 @@ namespace BrojGadjanjaDoPrvogPogodjenog
         }
 
         BackgroundWorker worker;
-        decimal result1;
-        decimal result2;
         String workingString = "";
         int numOfRep=0;
+        List<decimal> res;
+        List<decimal> res2;
+        int divider=1;
+
+
 
         public Form1()
         {
@@ -35,6 +39,13 @@ namespace BrojGadjanjaDoPrvogPogodjenog
             worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
             worker.DoWork += new DoWorkEventHandler(worker_DoWork);
             worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+
+            for (int i = 0; i < 10; i++) {
+                DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                col.Width = 70;
+                col.HeaderText = "Step: " + (i + 1);              
+                resultGridView.Columns.Add(col);
+            }
         }
 
         void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -50,23 +61,29 @@ namespace BrojGadjanjaDoPrvogPogodjenog
             }
             else
             {          
-                prosjekPokusaja1.Text = result1.ToString();
-                prosjekPokusaja2.Text = result2.ToString();
                 working.Text = "Done";
                 progressBar1.Value = 0;
+                
+                resultGridView.Rows.Add(divider,numOfRep,"Largest Ship", res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9]);
+                divider++;
+                resultGridView.Rows.Add(divider,numOfRep, "One Square", res2[0], res2[1], res2[2], res2[3], res2[4], res2[5], res2[6], res2[7], res2[8], res2[9]);
+                resultGridView.Rows[divider-1].DividerHeight = 2;
+                divider++;
+                
             }
             button1.Enabled = true;
             button2.Enabled = true;
             button3.Enabled = true;
+            button4.Enabled = true;
 
         }
 
         void worker_DoWork(object sender, DoWorkEventArgs e)
         {
             workingString = "Working : LargestShip";
-            result1 = Calculate( ShootingType.LargestShip,e);
+            Calculate( ShootingType.LargestShip,e);
             workingString = "Working : OneSquare";
-            result2 = Calculate( ShootingType.OneSquare,e);
+            Calculate( ShootingType.OneSquare,e);
  
         }
 
@@ -79,13 +96,9 @@ namespace BrojGadjanjaDoPrvogPogodjenog
 
        
 
-        private decimal Calculate( ShootingType type, DoWorkEventArgs e) {
-            decimal prosjek1;
+        private void Calculate( ShootingType type, DoWorkEventArgs e) {
             int i;
-            decimal count = 0;
-            
-            
-            
+            List<decimal> results = new List<decimal>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             for (i = 0; i < numOfRep; ++i)
             {
                 if (worker.CancellationPending == true)
@@ -99,25 +112,49 @@ namespace BrojGadjanjaDoPrvogPogodjenog
                 Shipwright sw = new Shipwright(10, 10);
                 Fleet fleet = sw.CreateFleet(new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
                 Gunner gunner = new Gunner(10, 10, new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 });
+                
                 if (type == ShootingType.OneSquare) {
                     gunner.shipLenght = 1;
                 }
                 HitResult hitResult = HitResult.Missed;
-
-                while (hitResult != HitResult.Hit)
+                int index=0;
+                while (fleet.AllShipsSunken() != true)
                 {
+                    
+                    while (hitResult != HitResult.Hit)
+                    {
+                        
+                        Square target = gunner.NextTarget();
+                        hitResult = fleet.Hit(target);
+                        gunner.ProcessHitResult(hitResult);
+                        results[index]++;
+                        
 
-                    Square target = gunner.NextTarget();
-                    hitResult = fleet.Hit(target);
-                    gunner.ProcessHitResult(hitResult);
-                    count++;
 
+                    }
+                    while (hitResult != HitResult.Sunken)
+                    {
 
+                        Square target = gunner.NextTarget();
+                        hitResult = fleet.Hit(target);
+                        gunner.ProcessHitResult(hitResult);
+                    }
+                    index++;
                 }
+                
 
             }
+            if (type == ShootingType.OneSquare)
+            {
+                res2 = results.Select(x => x / numOfRep).ToList();
+               
+            }
+            else
+            {
+                res = results.Select(x => x / numOfRep).ToList();
+                
+            }
            
-            return prosjek1 = count / numOfRep;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -147,6 +184,15 @@ namespace BrojGadjanjaDoPrvogPogodjenog
 
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DisableButtons();
+            numOfRep = 500000;
+            progressBar1.Maximum = 500000;
+            worker.RunWorkerAsync();
+
+        }
+
         private void StopButton_Click(object sender, EventArgs e)
         {
 
@@ -158,6 +204,9 @@ namespace BrojGadjanjaDoPrvogPogodjenog
             button1.Enabled = false;
             button2.Enabled = false;
             button3.Enabled = false;
+            button4.Enabled = false;
         }
+
+        
     }
 }
