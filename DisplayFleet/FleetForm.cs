c5ff43e7f8e28057d3ac797bce32b1256d279e03
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-06-17 19:01:03 +0200

    fixed rules

--- DisplayFleet/FleetForm.cs
+++ DisplayFleet/FleetForm.cs
@@ -175,7 +175,6 @@ namespace DisplayFleet
                             break;
 
                         }
-                        EnemyTurn();
                         break;
                     }
             }
