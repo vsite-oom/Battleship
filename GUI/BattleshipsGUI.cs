1ac655e8115340ba32a4832b92d5f914fdc8a4a6
Domagoj Salkić <dosalkic@vsite.hr>
2020-06-20 21:20:31 +0200

    Clean Form

---
+++ GUI/BattleshipsGUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.GUI
+{
+    public partial class BattleshipsGUI : Form
+    {
+        public BattleshipsGUI()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
