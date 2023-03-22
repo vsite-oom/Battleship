0ab9e5e85971d2571a50391620c055303a207cdd
FranKalinger0998 <f.kalinger@gmail.com>
2023-03-22 21:23:19 +0100

    Implemented Grid and test for it

---
+++ Model/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+
+    public class Grid
+    {
+        readonly public int rows;
+        readonly public int columns;
+        private Square[,] squares;
+        public Grid(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            squares = new Square[rows, columns];
+            for (int r = 0; r < rows; r++)
+            {
+                for(int c = 0; c < columns; c++)
+                {
+                    squares[r, c]=new Square(r, c);
+                    
+                }
+            }
+        }
+        public IEnumerable<Square> AvailableSquares()
+        {
+            return squares.Cast<Square>();
+        }
+    }
+}
---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Vsite.Oom.Battleship.Model;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridWithGivenNumerOfSquares()
+        {
+            int rows = 10; int columns = 10;
+            Grid grid = new Grid(rows,columns);
+            Assert.AreEqual(columns * rows, grid.AvailableSquares().Count());
+        }
+    }
+}
