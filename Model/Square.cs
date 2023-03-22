05d49d9154cc8a600939f4b04be73018690c50a7
Ivor Novosel <ivnovosel@vsite.hr>
2023-03-22 20:59:35 +0100

    Add Square class

---
+++ Model/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Square
+    {
+        public Square(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
