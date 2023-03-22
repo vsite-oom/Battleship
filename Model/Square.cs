5558d6a05528b1b213bacc4fbd9389a895593381
FranKalinger0998 <f.kalinger@gmail.com>
2023-03-22 20:59:43 +0100

    Added Square class and unit test for it

---
+++ Model/Square.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Square
+    {
+        readonly public int row;
+        readonly public int column;
+        public Square(int row,int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
+}
---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,18 @@
+using Vsite.Oom.Battleship.Model;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void ConstructorCreaatesSquareWithRowAndColumnProvided()
+        {
+            var square = new Square(3, 4);
+            Assert.AreEqual(3, square.row);
+            Assert.AreEqual(4, square.column);
+            
+
+        }
+    }
+}
