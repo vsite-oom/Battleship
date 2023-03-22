661797dc7df60944d45909861a5f4f87de56e4b6
Jovana Milovanović <jomilova@vsite.hr>
2023-03-22 21:23:25 +0100

    Add Grid Class

---
+++ Model/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsie.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            rows = rows;
+            columns = columns;
+            squares = new Square[Rows, Columns];
+            for(int r = 0; r< rows; r++)
+            {
+                for (int c = 0; c < Columns; ++c){
+                    squares[r,c]= new Square(r,c); 
+            }
+            }
+        }
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private readonly Square[,] squares;
+
+        public IEnumerable<Square> AvaliableSquares()
+        {
+            return squares.Cast<Square>();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsie.Oom.Battleship.Model;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridWithGivenNumberOfSquares()
+        {
+            int rows = 10;
+            int columns = 10;
+            var grid = new Grid(rows, columns);
+            Assert.AreEqual(rows * columns, grid.AvaliableSquares().Count());
+        }
+    }
+}
