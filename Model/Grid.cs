d09187437db2b7524e25689d16cebb1ad1892a04
Dragana Kovacevic <dragana.kovacevic@blc.edu.ba>
2023-03-22 21:23:58 +0100

    Add Grid class

---
+++ Model/Grid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            squares = new Square[Rows, Columns];
+            for (int r = Rows - 1; r >= 0; ++r)
+            {
+                for (int c = 0; c < Columns; ++c)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+        
+
+        private readonly Square[,] squares;
+
+
+        public IEnumerable<Square> AvaiableSquares()
+        {
+             return squares.Cast<Square>();
+            throw new NotImplementedException();
+        }
+    }
+}
