1a1efa09e65164cdd490ac4120acee6ff4005fe1
Vlado Tonković <vltonkov@vsite.hr>
2023-03-29 20:13:19 +0200

    Implement GetAvailableSequences method

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,5 +1,8 @@
 ﻿namespace Vsite.Oom.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+    using Sequences = IEnumerable<IEnumerable<Square>>;
+
     public class Grid
     {
         public Grid(int rows, int columns)
@@ -21,10 +24,50 @@
 
         private readonly Square[,] squares;
 
-        public IEnumerable<Square> AvailableSquares()
+        public SquareSequence AvailableSquares()
         {
             return squares.Cast<Square>();
             //throw new NotImplementedException();
+        }
+
+        public Sequences GetAvailableSequences(int length)
+        {
+            return GetAvailableHorizontalSequences(length).Concat(GetAvailableVerticalSequences(length));
+        }
+
+        private Sequences GetAvailableHorizontalSequences(int length) 
+        { 
+            var result = new List<SquareSequence>();
+            for (int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            var toAdd = new List<Square>();
+                            for (int cc = c - length + 1; cc <= c; ++cc)
+                            {
+                                toAdd.Add(new Square(r, cc));
+                            }
+                            result.Add(toAdd);
+                        }
+                    }
+                    else
+                        counter = 0;
+                }
+            }
+
+            return result;
+        }
+        
+        private Sequences GetAvailableVerticalSequences(int length) 
+        {
+            var result = new List<SquareSequence>();
+            return result;
         }
     }
 }
