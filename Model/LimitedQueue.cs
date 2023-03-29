8d4e8c714bd8e231cee55a9d3459c877eb5ab400
Vlado Tonković <vltonkov@vsite.hr>
2023-03-29 21:01:15 +0200

    Add LimitedQueue class

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,21 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class LimitedQueue<T> : Queue<T>
+    {
+        public LimitedQueue(int maxLength)
+        {
+            this.maxLength = maxLength;
+        }
+
+        public new void Enqueue(T item)
+        {
+            base.Enqueue(item);
+            if (Count > maxLength)
+            {
+                Dequeue();
+            }
+        }
+
+        private readonly int maxLength;
+    }
+}
