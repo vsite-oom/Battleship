b9c811443c800402ba6c558f7833a8ede3b5d729
Nedim Omerović <neomerov@vsite.hr>
2023-03-29 20:13:34 +0200

    Implement GetAvailableSequences

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Vsite.Oom.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+    using Sequences = IEnumerable<Square>;
     public class Grid
     {
         public Grid(int rows, int columns)
         {
-            Rows = rows;    
+            Rows = rows;
             Columns = columns;
             squares = new Square[Rows, Columns];
             for (int r = 0; r < Rows; ++r)
@@ -31,7 +34,46 @@ namespace Vsite.Oom.Battleship.Model
         public IEnumerable<Square> AvailableSquares()
         {
             return squares.Cast<Square>();
-            throw new NotImplementedException();
         }
+        public Sequences GetAvailableSequences(int length)
+        {
+            return GetAvailableHorizontalSequences(length);
+        }
+
+        private Sequences GetAvailableHorizontalSequences(int length)
+        {
+            var result = new List<SquareSequence>();
+            for (int r =0; r< Rows; ++r)
+            {
+                int counter = 0;
+                for (int c =0; c < Columns; ++c)
+                {
+                    if (squares[r,c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            var toAdd = new List<Square>();
+                            for (int cc = c-length +1; cc<=c; ++cc)
+                            {
+                                toAdd.Add(new Square(r, cc));
+                            }
+                            result.Add(toAdd);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return (Sequences)result;
+        }
+        private Sequences GetAvailableVerticalSequences(int length)
+        {
+            var result = new List<SquareSequence>();
+            return (Sequences)result;
+        }
+
     }
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -15,5 +15,26 @@ namespace ModelTests
             var grid = new Grid(rows, columns);
             Assert.AreEqual(rows * columns, grid.AvailableSquares().Count());
         }
+
+
+        [TestMethod]
+        public void GetAvailableSequencesesReturnsTwoSequencesOfLength3ForGrid1Row4Columns()
+        {
+            int rows = 1;
+            int columns = 4;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSequences(3);
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailableSequencesesReturnsThreeSequencesOfLength3ForGrid5Row5Columns()
+        {
+            int rows = 1;
+            int columns = 4;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSequences(3);
+            Assert.AreEqual(2, result.Count());
+        }
     }
 }
