5c8160640c0e6079691ddc8d1e03b6c1f571d65a
louis20th <lujomirs@gmail.com>
2023-03-29 20:36:18 +0200

    Add Horizontal Sequences getter and Square remover

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+    using Sequences = IEnumerable<IEnumerable<Square>>;
+
     public class Grid
     {
+
         public Grid(int rows, int columns)
         {
             Rows = rows;
@@ -27,10 +32,55 @@ namespace Vsite.Oom.Battleship.Model
 
         private readonly Square[,] squares;
 
-        public IEnumerable<Square> AvailableSquares()
+        public SquareSequence AvailableSquares()
         {
             return squares.Cast<Square>();
         }
 
+        public Sequences GetAvailableSquences(int lenght)
+        {
+            return GetAvailableHorizontalSequences(lenght).Concat(GetAvailableVerticalSequences(lenght));
+        }
+
+        private Sequences GetAvailableHorizontalSequences(int lenght)
+        {
+            List<SquareSequence> result = new();
+            for (int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= lenght)
+                        {
+                            List<Square> toAdd = new();
+                            for (int cc = c - lenght + 1; cc <= c; ++cc)
+                            {
+                                toAdd.Add(new Square(r, cc));
+                            }
+                            result.Add(toAdd);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private Sequences GetAvailableVerticalSequences(int lenght)
+        {
+            List<SquareSequence> result = new();
+            return result;
+        }
+
+        public void RemoveSquare(int row, int column)
+        {
+            squares[row, column] = null;
+        }
     }
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Vsite.Oom.Battleship.Model;
@@ -19,5 +20,47 @@ namespace ModelTests
             var grid = new Grid(rows, columns);
             Assert.AreEqual(rows * columns, grid.AvailableSquares().Count());
         }
+
+        [TestMethod]
+        public void GetAvailableSquencesReturnsTwoSquencesOfLenght3ForGrid1Row4Columns()
+        {
+            int rows = 1;
+            int columns = 4;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSquences(3);
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailableSquencesReturnsThreeSquencesOfLenght3ForGrid5Row1Columns()
+        {
+            int rows = 5;
+            int columns = 1;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSquences(3);
+            Assert.AreEqual(3, result.Count());
+        }
+        
+        [TestMethod]
+        public void GetAvailableSquencesReturnsThreeSquencesOfLenght2ForGrid1Row6Columns0_2IsRemoved()
+        {
+            int rows = 1;
+            int columns = 6;
+            var grid = new Grid(rows, columns);
+            grid.RemoveSquare(0, 2);
+            var result = grid.GetAvailableSquences(2);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailableSquencesReturnsThreeSquencesOfLenght3ForGrid5Row1Columns2()
+        {
+            int rows = 5;
+            int columns = 1;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSquences(3);
+            Assert.AreEqual(3, result.Count());
+        }
+
     }
 }
