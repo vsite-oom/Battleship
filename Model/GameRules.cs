cc67fb8e7ce76843f6bbf7a650797254f65777f6
Šime Šprljan <sisprlja@vsite.hr>
2023-04-05 21:18:43 +0200

    Create GameRules class

---
+++ Model/GameRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class GameRules
+    {
+        public readonly int GridRows = 10;
+        public readonly int GridColumns = 10;
+        public readonly IEnumerable<int> ShipLengths = new List<int> { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+        public readonly ISquaresTerminator Terminator;
+    }
+}
