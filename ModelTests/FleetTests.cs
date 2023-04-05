13bd059418f14b2380d680f790f4df125af350db
Vlado Tonković <vltonkov@vsite.hr>
2023-04-05 20:47:16 +0200

    Add Ship and Fleet classes

---
+++ ModelTests/FleetTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class FleetTests
+    {
+        [TestMethod]
+        public void CreateShipAddsNewShipToFleet()
+        {
+            var fleet = new Fleet();
+            Square[] sqaures = { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            
+            fleet.CreateShip(sqaures);
+
+            Assert.AreEqual(1, fleet.Ships.Count());
+
+            var ship = fleet.Ships.ElementAt(0);
+            Assert.IsTrue(ship.Squares.Contains(sqaures[0]));
+            Assert.IsTrue(ship.Squares.Contains(sqaures[1]));
+            Assert.IsTrue(ship.Squares.Contains(sqaures[2]));
+        }
+    }
+}
