619245e6d9f900047343d94db93848565f84b307
louis20th <lujomirs@gmail.com>
2023-04-05 20:47:47 +0200

    Add fleet and fleet tests

---
+++ Model/Fleet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Fleet
+    {
+        private readonly List<Ship> ships = new();
+
+        public void createShip(IEnumerable<Square> shipSquares)
+        {
+            ships.Add(new Ship(shipSquares));
+        }
+
+        public IEnumerable<Ship> Ships { get { return ships; } }
+
+    }
+
+}
---
+++ ModelTests/FleetTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class FleetTest
+    {
+        [TestMethod]
+        public void CreateShipAddsNewShipToFleet()
+        {
+            Fleet fleet = new();
+            Square[] squares = { new Square(1, 2), new Square(1, 2), new Square(1, 4) };
+            fleet.createShip(squares);
+
+            Assert.AreEqual(1, fleet.Ships.Count());
+            var ship = fleet.Ships.ElementAt(0);
+            Assert.IsTrue(ship.Squares.Contains(squares[0]));
+            Assert.IsTrue(ship.Squares.Contains(squares[1]));
+            Assert.IsTrue(ship.Squares.Contains(squares[2]));
+
+        }
+    }
+}
