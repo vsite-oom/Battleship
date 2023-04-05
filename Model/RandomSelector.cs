43da06c7e0ab6e252bb457af31bf9bb1eb315c09
louis20th <lujomirs@gmail.com>
2023-04-05 21:26:10 +0200

    Add RandomSelector

---
+++ Model/RandomSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class RandomSelector : ISequenceSelector
+    {
+        public IEnumerable<Square> Select(IEnumerable<IEnumerable<Square>> sequences)
+        {
+            var randIndex = random.Next(0, sequences.Count());
+            return sequences.ElementAt(randIndex);
+        }
+
+        private readonly Random random = new();
+    }
+}
