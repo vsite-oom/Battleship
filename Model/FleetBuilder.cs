1f06ab2e90429c4b78816990f8dc3c0b77710415
Jovana Milovanović <jomilova@vsite.hr>
2023-04-05 21:19:05 +0200

    Create GameRules and FleetBuilder clases

---
+++ Model/FleetBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsie.Oom.Battleship.Model
+{
+    public class FleetBuilder
+    {
+        public FleetBuilder(GameRules rules, ISequenceSelector selector)
+        {
+            this.rules = rules;
+            this.selector = selector;
+        }
+        private readonly GameRules rules;
+        private readonly ISequenceSelector selector;
+
+        public IEnumerable<Square> ToEliminat { get; private set; }
+
+        public Fleet CreateFleet()
+        {
+            var grid = new Grid(rules.GridRows, rules.GridColumns);
+            var fleet =new Fleet();
+            foreach(var shipLength in rules.ShipLengths){
+                var candidates = grid.GetAvailableSequences(shipLength);
+                var selected = selector.Select(candidates);
+                fleet.CreateShip(selected);
+                rules.Terminator.ToEliminat(selected);
+                grid.RemoveSquareSequence(ToEliminat);
+            }
+            return fleet;
+
+        }
+    }
+}
