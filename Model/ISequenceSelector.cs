4f4fb8e9051328986ef605b30937d7d2e6c0475f
Vlado Tonković <vltonkov@vsite.hr>
2023-04-05 21:33:35 +0200

    Add IsquareTerminator and ISequenceSelector interfaces

---
+++ Model/ISequenceSelector.cs
@@ -0,0 +1,10 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    using Sequences = IEnumerable<IEnumerable<Square>>;
+    using SquareSequence = IEnumerable<Square>;
+
+    public interface ISequenceSelector
+    {
+        SquareSequence Select(Sequences sequences);
+    }
+}
---
+++ Model/ISequenceTerminator.cs
@@ -0,0 +1,9 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+
+    public interface ISquaresTerminator
+    {
+        SquareSequence ToEliminate(SquareSequence shipSquares);
+    }
+}
