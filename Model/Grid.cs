1cedf6c335271078db284467c0f42eaad40c0e9d
Vlado Tonković <vltonkov@vsite.hr>
2023-04-05 13:43:27 +0200

    Implement GetAvailableVerticalSequences

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -64,6 +64,21 @@
         private Sequences GetAvailableVerticalSequences(int length) 
         {
             var result = new List<SquareSequence>();
+            for (int c = 0; c < Columns; ++c)
+            {
+                var queue = new LimitedQueue<Square>(length);
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        queue.Enqueue(squares[r, c]);
+                        if (queue.Count == length)
+                            result.Add(queue.ToArray());
+                    }
+                    else
+                        queue.Clear();
+                }
+            }
             return result;
         }
     }
