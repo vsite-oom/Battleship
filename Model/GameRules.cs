8eab364513e69ef0dd7e22b98db58268e2db23fe
Vlado Tonković <vltonkov@vsite.hr>
2023-04-05 21:18:44 +0200

    Create GameRules class

---
+++ Model/GameRules.cs
@@ -0,0 +1,9 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class GameRules
+    {
+        public readonly int GridRows = 10;
+        public readonly int GridColumns = 10;
+        public readonly IEnumerable<int> ShipLengths = new List<int> { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+    }
+}
