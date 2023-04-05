707c2bd18e081bc38fdd1be2310b6a327cdff2ae
Julijan Sribar <julijan.sribar@gmail.com>
2023-04-05 20:47:14 +0200

    Add Ship and Fleet classes

---
+++ Model/Fleet.cs
@@ -0,0 +1,14 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> shipSquares)
+        {
+            ships.Add(new Ship(shipSquares));
+        }
+
+        private readonly List<Ship> ships = new List<Ship>();
+
+        public IEnumerable<Ship> Ships => ships;
+    }
+}
---
+++ ModelTests/FleetTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class FleetTests
+    {
+        [TestMethod]
+        public void CreateShipAddsNewShipToFleet()
+        {
+            var fleet = new Fleet();
+            Square[] squares = { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            fleet.CreateShip(squares);
+
+            Assert.AreEqual(1, fleet.Ships.Count());
+
+            var ship = fleet.Ships.ElementAt(0);
+            Assert.IsTrue(ship.Squares.Contains(squares[0]));
+            Assert.IsTrue(ship.Squares.Contains(squares[1]));
+            Assert.IsTrue(ship.Squares.Contains(squares[2]));
+
+        }
+    }
+}
