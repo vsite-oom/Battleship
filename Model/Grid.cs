7adaa7205cb0a54c591f4eeeafd306f0cabf0743
FranKalinger0998 <f.kalinger@gmail.com>
2023-04-05 20:09:08 +0200

    get avaliable sequance methodes

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 
 
 namespace Vsite.Oom.Battleship.Model
-    
+
 {
     using SquareSequance = IEnumerable<Square>;
     using Sequances = IEnumerable<IEnumerable<Square>>;
+    using squareAccess = Func<int, int, Square>;
     public class Grid
     {
         readonly public int rows;
@@ -24,63 +26,39 @@ namespace Vsite.Oom.Battleship.Model
             squares = new Square[rows, columns];
             for (int r = 0; r < rows; r++)
             {
-                for(int c = 0; c < columns; c++)
+                for (int c = 0; c < columns; c++)
                 {
-                    squares[r, c]=new Square(r, c);
-                     
+                    squares[r, c] = new Square(r, c);
+
                 }
             }
         }
-        public IEnumerable<Square> AvailableSquares()
+        public SquareSequance AvailableSquares()
         {
-            return squares.Cast<Square>();
+            return squares.Cast<Square>().Where(sq => sq != null);
         }
 
-        public Sequances GetAvaliableSequences(int length) {
-
+        public Sequances GetAvaliableSequences(int length)
+        {
+            if (length == 1) return GetAvaliableHorizontalSequances(length);
             return GetAvaliableHorizontalSequances(length).Concat(GetAvaliableVerticalSequances(length));
 
 
 
         }
-        private Sequances  GetAvaliableHorizontalSequances(int length)
-        {
-            var result=new List<SquareSequance>();
-            for(int r=0; r < rows; r++)
-            {
-               
-                var queue = new LimitedQueue<Square>(length);
-                for(int c=0; c < columns; c++)
-                {
-                    if (squares[r, c] != null)
-                    {
-                        
-                        queue.Enqueue(squares[r, c]);
-                        if(queue.Count == length)
-                        {
-                            result.Add(queue.ToArray());
-                            
-                        }
-                    }
-                    else
-                    {
-                        queue.Clear();
-                    }
-                }
-            }
-            return result;
-        }
-        private Sequances GetAvaliableVerticalSequances(int length)
+        private Sequances GetAvalableSequences(int outerloopLimitint, int innerLoopLimit, squareAccess sq, int length)
         {
             var result = new List<SquareSequance>();
-            for (int c = 0; c < columns; c++) 
+            for (int o = 0; o < outerloopLimitint; o++)
             {
+
                 var queue = new LimitedQueue<Square>(length);
-                for (int r = 0; r < rows; r++)
+                for (int i = 0; i < innerLoopLimit; i++)
                 {
-                    if (squares[r, c] != null)
+                    if (sq(o, i) != null)
                     {
-                        queue.Enqueue(squares[r, c]);
+
+                        queue.Enqueue(sq(o, i));
                         if (queue.Count == length)
                         {
                             result.Add(queue.ToArray());
@@ -89,12 +67,25 @@ namespace Vsite.Oom.Battleship.Model
                     }
                     else
                     {
+
                         queue.Clear();
+                        if (innerLoopLimit - i <= length) break;
+
                     }
                 }
             }
             return result;
         }
-        public void RemoveSquare(int row,int column) { squares[row, column] = null; }
+        private Sequances GetAvaliableHorizontalSequances(int length)
+        {
+            return GetAvalableSequences(rows, columns, (a, b) => squares[a, b], length);
+
+        }
+        private Sequances GetAvaliableVerticalSequances(int length)
+        {
+            return GetAvalableSequences(columns, rows, (a, b) => squares[b, a], length);
+
+        }
+        public void RemoveSquare(int row, int column) { squares[row, column] = null; }
     }
 }
