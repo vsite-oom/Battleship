66c8e9564eb80c9e0b0e9883928294f269fb95c7
louis20th <lujomirs@gmail.com>
2023-04-05 21:18:48 +0200

    Add FleetBuilder

---
+++ Model/FleetBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class FleetBuilder
+    {
+        public FleetBuilder(GameRules rules, ISequenceSelector selector)
+        {
+            this.rules = rules;
+            this.selector = selector;
+
+        }
+
+        private readonly GameRules rules;
+        private readonly ISequenceSelector selector;
+        public Fleet CreateFleet()
+        {
+            var grid = new Grid(rules.gridRows, rules.gridColumns);
+            var fleet = new Fleet();
+            foreach (var shipLenght in rules.shipLenghts)
+            {
+                var candidates = grid.GetAvailableSquences(shipLenght);
+                var selected = selector.Select(candidates);
+
+                fleet.createShip(selected);
+                var toEliminate = rules.terminator.ToEliminate(selected);
+                grid.RemoveSquares(toEliminate);
+            }
+
+            return fleet;
+        }
+    }
+}
