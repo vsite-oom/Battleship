3f68d93adf8bf2dfd2f7c9d03bf3de1d1a66f70c
Vlado Tonković <vltonkov@vsite.hr>
2023-04-05 21:34:16 +0200

    Fix existing classes

---
+++ Model/SurroundingSquaresTerminator.cs
@@ -0,0 +1,19 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class SurroundingSquaresTerminator : ISquaresTerminator
+    {
+        public SurroundingSquaresTerminator(int gridRows, int gridColumns)
+        {
+            this.gridRows = gridRows;
+            this.gridColumns = gridColumns;
+        }
+
+        private readonly int gridRows;
+        private readonly int gridColumns;
+
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> ShipSquare) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
