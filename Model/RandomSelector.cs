26ab2439af6cde47577727d103c119439bd60e69
Šime Šprljan <sisprlja@vsite.hr>
2023-04-05 21:31:46 +0200

    Create SurroundingSquaresTerminator class

--- Model/RandomSelector.cs
+++ Model/RandomSelector.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
-    internal class RandomSelector
+    public class RandomSelector : ISequenceSelector
     {
+        public IEnumerable<Square> Select(IEnumerable<IEnumerable<Square>> sequences)
+        {
+            var index = random.Next(0, sequences.Count());
+            return sequences.ElementAt(index);
+        }
+
+        private readonly Random random = new Random();
     }
 }
