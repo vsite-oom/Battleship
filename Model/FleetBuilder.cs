d2aa67c95814430ef9eed312892c911134374128
Julijan Sribar <julijan.sribar@gmail.com>
2023-04-05 21:18:50 +0200

    Create FleetBuilder class

---
+++ Model/FleetBuilder.cs
@@ -0,0 +1,29 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class FleetBuilder
+    {
+        public FleetBuilder(GameRules rules, ISequenceSelector selector)
+        {
+            this.rules = rules;
+            this.selector = selector;
+        }
+
+        private readonly GameRules rules;
+        private readonly ISequenceSelector selector;
+
+        public Fleet CreateFleet()
+        {
+            var grid = new Grid(rules.GridRows, rules.GridColumns);
+            var fleet = new Fleet();
+            foreach (var shipLength in rules.ShipLengths)
+            {
+                var candidates = grid.GetAvailableSequences(shipLength);
+                var selected = selector.Select(candidates);
+                fleet.CreateShip(selected);
+                var toEliminate = rules.Terminator.ToEliminate(selected);
+                grid.RemoveSquares(toEliminate);
+            }
+            return fleet;
+        }
+    }
+}
