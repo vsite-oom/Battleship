883bfbc200bc721c4d6b1bf9453ccd6c23c2670d
louis20th <lujomirs@gmail.com>
2023-04-05 21:30:48 +0200

    Add SquareTerminator

---
+++ Model/SuroundingSquaresTerminator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class SuroundingSquaresTerminator : ISquareTerminator
+    {
+        public SuroundingSquaresTerminator(int gridRows, int gridColumns)
+        {
+            this.gridRows = gridRows;
+            this.gridColumns = gridColumns;
+        }
+
+        public readonly int gridRows;
+        public readonly int gridColumns;
+
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
