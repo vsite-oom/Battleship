925471da33993907d7339976a995b4d921ab94a5
louis20th <lujomirs@gmail.com>
2023-04-05 20:23:29 +0200

    Implement equals method

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,6 +1,6 @@
 ﻿namespace Vsite.Oom.Battleship.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
@@ -10,5 +10,21 @@
 
         public readonly int Row;
         public readonly int Column;
+
+        public bool Equals(Square other)
+        {
+            if (GetType() != other.GetType())
+                return false;
+            return this.Row == other.Row && this.Column == other.Column;
+        }
+
+        public override bool Equals(object obj) {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
     }
 }
