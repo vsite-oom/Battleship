8933eaf9924624affd0b0d5a5eaade23f67c7554
canerozdenkaya27 <canerozdenkaya27@gmail.com>
2023-04-06 19:51:50 +0200

    COzdenkaya

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,5 +1,7 @@
 ﻿namespace Vsite.Oom.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+    using Sequences = IEnumerable<IEnumerable<Square>>;
     public class Grid
     {
         public readonly int Rows;
@@ -17,17 +19,83 @@
 
                 for (int c = 0; c < Columns; ++c)
                 {
-                    squares[r,c] = new Square(r, c);
+                    squares[r, c] = new Square(r, c);
                 }
             }
         }
 
-        public IEnumerable<Square> AvalibleSquares 
-        {     
-            get 
+        public IEnumerable<Square> AvalibleSquares
+        {
+            get
             {
                 return squares.Cast<Square>();
-            } 
+            }
         }
+        public IEnumerable<IEnumerable<Square>> GetAvailableSequences(int length)
+        {
+            return GetAvailableHorizontalSequences(length).Concat(GetAvailableVerticalSequences(length));
+        }
+        private Sequences GetAvailableHorizontalSequences(int length)
+        {
+            var result = new List<SquareSequence>();
+            for (int r = 0; r < Rows; ++r)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; ++c)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            // Add previous squares to result
+                            var squaresToAdd = new List<Square>();
+                            for (int cc = c - length + 1; cc <= c; ++cc)
+                            {
+                                squaresToAdd.Add(squares[r, cc]);
+                            }
+                            result.Add(squaresToAdd);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+        private Sequences GetAvailableVerticalSequences(int length)
+        {
+            var result = new List<SquareSequence>();
+            for (int c = 0; c < Columns; ++c)
+            {
+                int counter = 0;
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            // Add previous squares to result
+                            var squaresToAdd = new List<Square>();
+                            for (int rr = r - length + 1; rr <= r; ++rr)
+                            {
+                                squaresToAdd.Add(squares[rr, c]);
+                            }
+                            result.Add(squaresToAdd);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
     }
 }
+
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -10,9 +10,34 @@ namespace ModelTests
         {
             int rows = 10;
             int columns = 8;
-            var grid = new Grid(rows,columns);
-            Assert.AreEqual(rows * columns,grid.AvalibleSquares.Count());
+            var grid = new Grid(rows, columns);
+            Assert.AreEqual(rows * columns, grid.AvalibleSquares.Count());
+        }
+        [TestMethod]
+        public void GetAvalibleSequancesReturns2SequancesOfLenght3ForGridWith1Row4Colums()
+        {
+            int rows = 1;
+            int columns = 4;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSequences(3);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(3, result.ElementAt(0).Count());
+            Assert.AreEqual(3, result.ElementAt(1).Count());
+
+        } [TestMethod]
+        public void GetAvalibleSequancesReturns3SequancesOfLenght3ForGridWith5Row1Colums()
+        {
+            int rows = 5;
+            int columns = 1;
+            var grid = new Grid(rows, columns);
+            var result = grid.GetAvailableSequences(3);
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(3, result.ElementAt(0).Count());
+            Assert.AreEqual(3, result.ElementAt(1).Count());
+            Assert.AreEqual(3, result.ElementAt(2).Count());
 
         }
+
     }
 }
+
