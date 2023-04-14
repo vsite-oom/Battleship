dcf89b205d6bc77f05d4c922dfe63257d1f76f91
Nedim Omerović <neomerov@vsite.hr>
2023-04-14 16:39:19 +0200

    Add Fleet Test

--- ModelTests/FleetTests.cs
+++ ModelTests/FleetTests.cs
@@ -9,7 +9,17 @@ namespace ModelTests
         [TestMethod]
         public void CreateShipAddsNewShipToFleet()
         {
-            
+            var fleet = new Fleet();
+            Square[] squares = { new Square(1, 2), new Square(1, 3), new Square(1, 4) };
+            fleet.CreateShip(squares);
+
+            Assert.AreEqual(1, fleet.Ships.Count());
+
+            var ship = fleet.Ships.ElementAt(0);
+            Assert.IsNotNull(ship);
+            Assert.IsTrue(ship.Squares.Contains(squares[0]));
+            Assert.IsTrue(ship.Squares.Contains(squares[1]));
+            Assert.IsTrue(ship.Squares.Contains(squares[2]));
         }
     }
 }
