3485345519dacf17727cac64600ded758480ec2f
louis20th <lujomirs@gmail.com>
2023-04-19 20:05:51 +0200

    Implement SuroundingSquaresTerminator and UT.

--- Model/SuroundingSquaresTerminator.cs
+++ Model/SuroundingSquaresTerminator.cs
@@ -20,7 +20,21 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
-            throw new NotImplementedException();
+            int topmostRow = Math.Max(0, shipSquares.Min(s => s.Row) - 1);
+            int bottommostRow = Math.Min(gridRows, shipSquares.Max(s => s.Row) + 2);
+
+            int leftmostColumn = Math.Max(0, shipSquares.Min(s => s.Column) - 1);
+            int rightmostColumn = Math.Min(gridColumns, shipSquares.Max(s => s.Column) + 2);
+
+            var toEliminate = new List<Square>();
+
+            for (int r = topmostRow; r < bottommostRow; ++r) {
+                for (int c = leftmostColumn; c < rightmostColumn; ++c) {
+                    toEliminate.Add(new Square(r, c));
+                }
+            }
+
+            return toEliminate;
         }
     }
 }
