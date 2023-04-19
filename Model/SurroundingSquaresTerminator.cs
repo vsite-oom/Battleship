285600d6aa5a1d860254052bb7912314ce16e5bb
Ivor Novosel <ivnovosel@vsite.hr>
2023-04-19 20:06:22 +0200

    Implement SurroundingSquaresTerminator

--- Model/SurroundingSquaresTerminator.cs
+++ Model/SurroundingSquaresTerminator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,20 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
-            throw new NotImplementedException();
+            int topmostRow = Math.Max(0, shipSquares.Min(x => x.Row) - 1);
+            int bottommostRow = Math.Min(gridRows, shipSquares.Max(x => x.Row) + 2);
+            int leftmostColumn = Math.Max(0, shipSquares.Min(x => x.Column) - 1);
+            int rightmostColumn = Math.Min(gridColumns, shipSquares.Max(x => x.Column) + 2);
+
+            var toEliminate = new List<Square>();
+            for(int r = topmostRow; r < bottommostRow; ++r)
+            {
+                for (int c = leftmostColumn; c < rightmostColumn; ++c)
+                {
+                    toEliminate.Add(new Square(r, c));
+                }
+            }
+            return toEliminate;
         }
     }
 }
