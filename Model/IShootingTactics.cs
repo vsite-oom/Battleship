ad1e54fa9d6340a76c4778e2dcbba89d81cb2038
Julijan Sribar <julijan.sribar@gmail.com>
2023-04-19 21:12:20 +0200

    Add Gunnery and shooting tactics classes

---
+++ Model/IShootingTactics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public interface IShootingTactics
+    {
+        Square NextTarget();
+    }
+}
---
+++ Model/ZoneShooting.cs
@@ -0,0 +1,19 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class ZoneShooting : IShootingTactics
+    {
+        public ZoneShooting(Grid grid, Square firstSquareHit)
+        {
+            this.grid = grid;
+            firstHit = firstSquareHit;
+        }
+
+        private readonly Grid grid;
+        private readonly Square firstHit;
+
+        public Square NextTarget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
