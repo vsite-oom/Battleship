753b58391edbb58e11456e4cc9df873a28218814
Ivor Novosel <ivnovosel@vsite.hr>
2023-04-19 20:36:02 +0200

    Add FleetBuilderTests

--- Model/GameRules.cs
+++ Model/GameRules.cs
@@ -2,9 +2,13 @@
 {
     public class GameRules
     {
+        public GameRules()
+        {
+            Terminator = new SurroundingSquaresTerminator(GridRows, GridColumns);
+        }
         public readonly int GridRows = 10;
         public readonly int GridColumns = 10;
         public readonly IEnumerable<int> ShipLengts = new List<int> { 5, 4, 4, 3, 3, 3, 2, 2, 2 };
-        public readonly ISquaresTerminator Terminator = null;
+        public readonly ISquaresTerminator Terminator;
     }
 }
