f5ae49263d136c939ba4076e9fd74c3f870fa0ba
Nedim Omerović <neomerov@vsite.hr>
2023-04-26 15:28:20 +0200

    Implement LineShooting

---
+++ Model/LineShooting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    internal class LineShooting : IShootingTactics
+    {
+        public LineShooting(Grid grid, IEnumerable<Square> squaresHit)
+        {
+            this.grid = grid;
+            this.squaresHit = new List<Square>(squaresHit);
+        }
+
+        private readonly Grid grid;
+        private List<Square> squaresHit;
+
+        public Square NextTarget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
