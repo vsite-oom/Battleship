ae11899345dd869e1ebc0451088dd25952f9c46c
Nedim Omerović <neomerov@vsite.hr>
2023-04-26 15:31:34 +0200

    Implement ZoneShooting

---
+++ Model/ZoneShooting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    internal class ZoneShooting
+    {
+        public ZoneShooting(Grid grid, Square firstHit)
+        {
+            this.grid = grid;
+            this.firstHit = firstHit;
+        }
+
+        private readonly Grid grid;
+        private readonly Square firstHit;
+
+        public Square NextTarget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
