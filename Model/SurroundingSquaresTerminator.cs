f270d4ede0086dc754a2056a292d7b6257937971
Nedim Omerović <neomerov@vsite.hr>
2023-04-26 15:23:38 +0200

    Update SurroundingSquaresTerminator

--- Model/SurroundingSquaresTerminator.cs
+++ Model/SurroundingSquaresTerminator.cs
@@ -11,15 +11,31 @@ namespace Vsite.Oom.Battleship.Model
     {
         public SurroundingSquaresTerminator(int gridRows, int gridColumns)
         {
-            
+            this.gridRows = gridRows;
+            this.gridColumns = gridColumns;
         }
 
-        
         private readonly int gridRows;
+        private readonly int gridColumns;
+
+      
 
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
-            throw new NotImplementedException();
+            int topmostRow = Math.Max(0, shipSquares.Min(x => x.Row) - 1);
+            int bottommostRow = Math.Min(gridRows, shipSquares.Max(x => x.Row) + 2);
+            int leftmostColumn = Math.Max(0, shipSquares.Min(x => x.Column) - 1);
+            int rightmostColumn = Math.Min(gridColumns, shipSquares.Max(x => x.Column) + 2);
+
+            var toEliminate = new List<Square>();
+            for (int r = topmostRow; r < bottommostRow; ++r)
+            {
+                for (int c = leftmostColumn; c < rightmostColumn; ++c)
+                {
+                    toEliminate.Add(new Square(r, c));
+                }
+            }
+            return toEliminate;
         }
     }
 }
