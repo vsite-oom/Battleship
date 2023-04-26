aba82cb1b3c62f307a5c01edde34e9440c09ddf3
Nedim Omerović <neomerov@vsite.hr>
2023-04-26 17:00:44 +0200

    Implement Gunnery

---
+++ Model/Gunnery.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public class Gunnery
+    {
+        public enum CurrentShootingTactics
+        {
+            Random,
+            Zone,
+            Line
+        }
+
+        public Gunnery(GameRules gameRules, Fleet fleet)
+        {
+            grid = new Grid(gameRules.GridRows, gameRules.GridColumns);
+            this.fleet = fleet;
+            shootingTactics = new RandomShooting(grid);
+            currentShootingTactics = CurrentShootingTactics.Random;
+        }
+
+        public Square nextTarget()
+        {
+            return shootingTactics.NextTarget();
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        {
+
+            if (hitResult == HitResult.Sunk)
+            {
+                currentShootingTactics = CurrentShootingTactics.Random;
+            }
+
+            if (hitResult == HitResult.Hit && currentShootingTactics == CurrentShootingTactics.Zone)
+            {
+                currentShootingTactics = CurrentShootingTactics.Line;
+            }
+
+            if (hitResult == HitResult.Hit && currentShootingTactics == CurrentShootingTactics.Random)
+            {
+                currentShootingTactics = CurrentShootingTactics.Zone;
+            }
+
+            if (hitResult == HitResult.Hit && currentShootingTactics == CurrentShootingTactics.Line)
+            {
+                currentShootingTactics = CurrentShootingTactics.Line;
+            }
+
+            if (hitResult == HitResult.Missed && currentShootingTactics == CurrentShootingTactics.Random)
+            {
+                currentShootingTactics = CurrentShootingTactics.Random;
+            }
+
+            if (hitResult == HitResult.Missed && currentShootingTactics == CurrentShootingTactics.Zone)
+            {
+                currentShootingTactics = CurrentShootingTactics.Zone;
+            }
+
+            if (hitResult == HitResult.Missed && currentShootingTactics == CurrentShootingTactics.Line)
+            {
+                currentShootingTactics = CurrentShootingTactics.Zone;
+            }
+
+        }
+
+        private readonly Grid grid;
+        private readonly Fleet fleet;
+        private IShootingTactics shootingTactics;
+
+        public CurrentShootingTactics currentShootingTactics { get; private set; }
+    }
+}
---
+++ ModelTests/GunneryTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class GunneryTests
+    {
+        [TestMethod]
+        public void CurrentShootingTacticsIsInitiallyRandom()
+        {
+            var gunnery = new Gunnery(new GameRules(), new Fleet());
+            Assert.AreEqual(CurrentShootingTactics.Random, gunnery.currentShootingTactics);
+        }
+    }
+}
