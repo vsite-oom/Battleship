12906725ee8434e60e20edf7ca0db54455ce389e
Julijan Sribar <julijan.sribar@gmail.com>
2023-05-10 21:17:07 +0200

    Implement Fleet shooting

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -7,6 +7,19 @@
             ships.Add(new Ship(shipSquares));
         }
 
+        public HitResult Fire(Square target)
+        {
+            foreach (var ship in ships)
+            {
+                HitResult result = ship.Fire(target);
+                if (result != HitResult.Missed)
+                {
+                    return result;
+                }
+            }
+            return HitResult.Missed;
+        }
+
         private readonly List<Ship> ships = new List<Ship>();
 
         public IEnumerable<Ship> Ships => ships;
