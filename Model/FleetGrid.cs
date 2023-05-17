175286139004cd08e6ed83eba5a5a277a391c0d0
Julijan Sribar <julijan.sribar@gmail.com>
2023-05-17 20:46:34 +0200

    Separate classes into files

---
+++ Model/FleetGrid.cs
@@ -0,0 +1,28 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+    public class FleetGrid : Grid
+    {
+        public FleetGrid(int rows, int columns) : base(rows, columns)
+        {
+        }
+
+        public void RemoveSquare(int row, int column)
+        {
+            squares[row, column] = null;
+        }
+
+        public void RemoveSquares(SquareSequence squaresToRemove)
+        {
+            foreach (var square in squaresToRemove)
+            {
+                RemoveSquare(square.Row, square.Column);
+            }
+        }
+
+        protected override bool IsAvailable(Square square)
+        {
+            return square != null;
+        }
+    }
+}
