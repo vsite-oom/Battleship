32776c4962455b3f05bf9c4dc1f4bb13b2aa67f0
Vlado Tonković <vltonkov@vsite.hr>
2023-05-17 18:23:31 +0200

    Add elimination of surrounding squares

--- Model/Square.cs
+++ Model/Square.cs
@@ -5,6 +5,7 @@ namespace Vsite.Oom.Battleship.Model
     public enum SquareState
     {
         Initial,
+        Eliminated,
         Missed,
         Hit,
         Sunk
@@ -41,6 +42,11 @@ namespace Vsite.Oom.Battleship.Model
                     Debug.Assert(false, "Unsupported hit result");
                     break;
             }
+        }
+
+        public void Eliminate()
+        {
+            SquareState = SquareState.Eliminated;
         }
 
         public bool Equals(Square other)
