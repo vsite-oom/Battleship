f9be437f16eb3a08533a1b87a0dc75bc006b0aad
Ivor Novosel <ivnovosel@vsite.hr>
2023-05-17 20:46:43 +0200

    Divide classes into separate files

---
+++ Model/FleetGrid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+
+    public class FleetGrid : Grid
+    {
+        public FleetGrid(int rows, int columns) : base(rows, columns)
+        {
+        }
+
+        protected override bool IsAvailable(Square square)
+        {
+            return square != null;
+        }
+
+        public void RemoveSquare(int row, int column)
+        {
+            squares[row, column] = null;
+        }
+
+        public void RemoveSquares(SquareSequence squaresToRemove)
+        {
+            foreach (var square in squaresToRemove)
+            {
+                RemoveSquare(square.Row, square.Column);
+            }
+        }
+    }
+}
--- Model/Grid.cs
+++ Model/Grid.cs
@@ -39,10 +39,10 @@ namespace Vsite.Oom.Battleship.Model
 
         protected readonly Square[,] squares;
 
-        //public SquareSequence AvailableSquares()
-        //{
-        //    return squares.Cast<Square>().Where(sq => sq != null);
-        //}
+        public SquareSequence AvailableSquares()
+        {
+            return squares.Cast<Square>().Where(sq => IsAvailable(sq));
+        }
 
         public Sequences GetAvailableSequences(int length)
         {
@@ -63,9 +63,6 @@ namespace Vsite.Oom.Battleship.Model
         }
 
         protected abstract bool IsAvailable(Square square);
-        //{
-        //    return square != null && square.SquareState == SquareState.Initial;
-        //}
 
         private Sequences GetAvailableSequences(int outerLoopLimit, int innerLoopLimit, SquareAccess squareAccess, int length)
         {
@@ -91,176 +88,6 @@ namespace Vsite.Oom.Battleship.Model
                 }
             }
             return result;
-        }
-
-        //public void RemoveSquare(int row, int column)
-        //{
-        //    squares[row, column] = null;
-        //}
-
-        //public void RemoveSquares(SquareSequence squaresToRemove)
-        //{
-        //    foreach (var square in squaresToRemove)
-        //    {
-        //        RemoveSquare(square.Row, square.Column);
-        //    }
-        //}
-
-        //public void MarkSquare(int row, int column, HitResult hitResult)
-        //{
-        //    squares[row, column].Mark(HitResult.Hit);
-        //}
-
-        //    public SquareSequence GetAvailableSequence(Square from, Direction direction)
-        //    {
-        //        int row = from.Row;
-        //        int endRow = row;
-        //        int deltaRow = 0;
-        //        if (direction == Direction.Upwards)
-        //        {
-        //            --row;
-        //            deltaRow = -1;
-        //            endRow = -1;
-        //        }
-        //        else if (direction == Direction.Downwards)
-        //        {
-        //            ++row;
-        //            deltaRow = +1;
-        //            endRow = Rows;
-        //        }
-        //        int column = from.Column;
-        //        int endColumn = column;
-        //        int deltaColumn = 0;
-        //        if (direction == Direction.Leftwards)
-        //        {
-        //            --column;
-        //            deltaColumn = -1;
-        //            endColumn = -1;
-        //        }
-        //        else if (direction == Direction.Rightwards)
-        //        {
-        //            ++column;
-        //            deltaColumn = +1;
-        //            endColumn = Columns;
-        //        }
-        //        var result = new List<Square>();
-        //        while (row != endRow || column != endColumn)
-        //        {
-        //            if (squares[row, column].SquareState != SquareState.Initial)
-        //            {
-        //                break;
-        //            }
-        //            result.Add(squares[row, column]);
-        //            row += deltaRow;
-        //            column += deltaColumn;
-        //        }
-        //        return result;
-        //    }
-
-        //    public void Eliminate(int row, int column)
-        //    {
-        //        squares[row, column].Eliminate();
-        //    }
-        //}
-
-    }
-    public class FleetGrid : Grid
-    {
-        public FleetGrid(int rows, int columns) : base(rows, columns)
-        {
-        }
-
-        public SquareSequence AvailableSquares()
-        {
-            return squares.Cast<Square>().Where(sq => sq != null);
-        }
-
-        protected override bool IsAvailable(Square square)
-        {
-            return square != null;
-        }
-
-        public void RemoveSquare(int row, int column)
-        {
-            squares[row, column] = null;
-        }
-
-        public void RemoveSquares(SquareSequence squaresToRemove)
-        {
-            foreach (var square in squaresToRemove)
-            {
-                RemoveSquare(square.Row, square.Column);
-            }
-        }
-
-
-
-    }
-    public class RecordGrid : Grid
-    {
-        public RecordGrid(int rows, int columns) : base (rows, columns)
-        {
-        }
-
-        protected override bool IsAvailable(Square square)
-        {
-            return square.SquareState == SquareState.Initial;
-        }
-
-        public void MarkSquare(int row, int column, HitResult hitResult)
-        {
-            squares[row, column].Mark(HitResult.Hit);
-        }
-
-        public SquareSequence GetAvailableSequence(Square from, Direction direction)
-        {
-            int row = from.Row;
-            int endRow = row;
-            int deltaRow = 0;
-            if (direction == Direction.Upwards)
-            {
-                --row;
-                deltaRow = -1;
-                endRow = -1;
-            }
-            else if (direction == Direction.Downwards)
-            {
-                ++row;
-                deltaRow = +1;
-                endRow = Rows;
-            }
-            int column = from.Column;
-            int endColumn = column;
-            int deltaColumn = 0;
-            if (direction == Direction.Leftwards)
-            {
-                --column;
-                deltaColumn = -1;
-                endColumn = -1;
-            }
-            else if (direction == Direction.Rightwards)
-            {
-                ++column;
-                deltaColumn = +1;
-                endColumn = Columns;
-            }
-            var result = new List<Square>();
-            while (row != endRow || column != endColumn)
-            {
-                if (squares[row, column].SquareState != SquareState.Initial)
-                {
-                    break;
-                }
-                result.Add(squares[row, column]);
-                row += deltaRow;
-                column += deltaColumn;
-            }
-            return result;
-        }
-
-        public void Eliminate(int row, int column)
-        {
-            squares[row, column].Eliminate();
         }
     }
 }
