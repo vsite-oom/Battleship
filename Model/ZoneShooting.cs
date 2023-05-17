8b9cac89a6e9cd7c49a23ffaef50125388ebbc6c
Dragana Kovacevic <dragana.kovacevic@blc.edu.ba>
2023-05-17 10:16:27 +0200

    Implement NextTarget

--- Model/ZoneShooting.cs
+++ Model/ZoneShooting.cs
@@ -8,18 +8,32 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class ZoneShooting : IShootingTactics
     {
-        public ZoneShooting(Grid grid, Square firstSquareHit, IEnumerable<int> shipLenghts)
+        public ZoneShooting(Grid grid, Square firstSquareHit, IEnumerable<int> shipLengths)
         {
             this.grid = grid;
-            this.firstHit = firstSquareHit;
-            this.shipLenghts = shipLenghts;
+            firstHit = firstSquareHit;
+            this.shipLengths = shipLengths;
         }
+
         private readonly Grid grid;
-        private readonly Square firstHit; 
-        private readonly IEnumerable<int> shipLenghts;
+        private readonly Square firstHit;
+        private readonly IEnumerable<int> shipLengths;
+
+        private readonly Random random = new Random();
+
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            var sequences = new List<IEnumerable<Square>>();
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var seq = grid.GetAvailableSequence(firstHit, direction);
+                if (seq.Any())
+                {
+                    sequences.Add(seq);
+                }
+            }
+            int index = random.Next(sequences.Count);
+            return sequences[index].First();
         }
     }
 }
