eec66a89ed003b3a2bc6e1f113fe7b1821441a80
Dragana Kovacevic <dragana.kovacevic@blc.edu.ba>
2023-05-17 10:11:03 +0200

    Implement NextTarget

--- Model/LineShooting.cs
+++ Model/LineShooting.cs
@@ -9,20 +9,51 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class LineShooting : IShootingTactics
     {
-        public LineShooting(Grid grid, IEnumerable<Square> squaresHit, IEnumerable<int> shipLenghts)
+        public LineShooting(Grid grid, IEnumerable<Square> squaresHit, IEnumerable<int> shipLengths)
         {
             this.grid = grid;
-            squares = new List<Square>(squaresHit);
-            this.shipLenghts = shipLenghts; 
-
+            squares = squaresHit;
+            this.shipLengths = shipLengths;
         }
 
         private readonly Grid grid;
-        private List<Square> squares;
-        private readonly IEnumerable<int> shipLenghts; 
+        private IEnumerable<Square> squares;
+        private readonly IEnumerable<int> shipLengths;
+
+        private readonly Random random = new Random();
+
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            squares = squares.OrderBy(s => s.Row + s.Column);
+            var sequences = new List<IEnumerable<Square>>();
+            if (squares.First().Column == squares.Last().Column)
+            {
+                var s1 = grid.GetAvailableSequence(squares.First(), Direction.Upwards);
+                if (s1.Any())
+                {
+                    sequences.Add(s1);
+                }
+                var s2 = grid.GetAvailableSequence(squares.Last(), Direction.Downwards);
+                if (s2.Any())
+                {
+                    sequences.Add(s2);
+                }
+            }
+            else
+            {
+                var s1 = grid.GetAvailableSequence(squares.First(), Direction.Leftwards);
+                if (s1.Any())
+                {
+                    sequences.Add(s1);
+                }
+                var s2 = grid.GetAvailableSequence(squares.Last(), Direction.Rightwards);
+                if (s2.Any())
+                {
+                    sequences.Add(s2);
+                }
+            }
+            int index = random.Next(sequences.Count);
+            return sequences[index].First();
         }
     }
 }
