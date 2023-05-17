801bc51f0833420e5c87818babfe2b8224361088
Dragana Kovacevic <dragana.kovacevic@blc.edu.ba>
2023-05-17 10:14:57 +0200

    Add elimination of surrounding squares

--- Model/Square.cs
+++ Model/Square.cs
@@ -2,14 +2,15 @@
 
 namespace Vsite.Oom.Battleship.Model
 {
-
     public enum SquareState
     {
-        Initial, 
-        Missed, 
-        Hit, 
-        Sunk 
+        Initial,
+        Eliminated,
+        Missed,
+        Hit,
+        Sunk
     }
+
     public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
@@ -22,25 +23,30 @@ namespace Vsite.Oom.Battleship.Model
         public readonly int Row;
         public readonly int Column;
 
-        public SquareState SquareState { get; private set; } 
+        public SquareState SquareState { get; private set; }
 
         public void Mark(HitResult hitResult)
         {
             switch (hitResult)
             {
-                case HitResult.Missed: 
-                    SquareState = SquareState.Missed; 
+                case HitResult.Missed:
+                    SquareState = SquareState.Missed;
                     break;
                 case HitResult.Hit:
                     SquareState = SquareState.Hit;
                     break;
-                case HitResult.Sunk: 
+                case HitResult.Sunk:
                     SquareState = SquareState.Sunk;
                     break;
-                 default:
+                default:
                     Debug.Assert(false, "Unsupported hit result");
                     break;
             }
+        }
+
+        public void Eliminate()
+        {
+            SquareState = SquareState.Eliminated;
         }
 
         public bool Equals(Square other)
@@ -51,9 +57,9 @@ namespace Vsite.Oom.Battleship.Model
             }
             return Row == other.Row && Column == other.Column;
         }
+
         public override bool Equals(object obj) => Equals(obj as Square);
 
         public override int GetHashCode() => HashCode.Combine(Row, Column);
-     
     }
 }
