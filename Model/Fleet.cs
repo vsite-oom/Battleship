687e55ed962afcf73afe5cfb4646f6a77e66acb1
Sonja Perković <soperkov@vsite.hr>
2024-03-27 20:22:55 +0100

    Define square constructor

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Mail;
+using System.Threading.Channels;
 
 namespace Vsite.Oom.Battleship.Model
 {
     public class Fleet
     {
+        
     }
 }
--- Model/Ship.cs
+++ Model/Ship.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vsite.Oom.Battleship.Model
+﻿namespace Vsite.Oom.Battleship.Model
 {
     public class Ship
     {
