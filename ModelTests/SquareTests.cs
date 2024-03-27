b239d7c16fb023355ea7fdd909c4467e30ccf9ea
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-03-27 20:41:58 +0100

    - create test class for Square

---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWithRowAndColumnProvided()
+        {
+            int row = 4;
+            int col = 8;
+            var square = new Square(row, col);
+
+            Assert.AreEqual(row, square.Row);
+            Assert.AreEqual(col, square.Column);
+            
+
+            
+        }
+    }
+}
