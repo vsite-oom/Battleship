2b2c0ce6e7f671ae1f1b50694dadaa0722606d62
Julijan Sribar <julijan.sribar@gmail.com>
2024-03-27 21:25:07 +0100

    Implement GetAvailablePlacements for horizontal ships

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,4 +1,7 @@
-﻿namespace Vsite.Oom.Battleship.Model
+﻿
+using System.Linq;
+
+namespace Vsite.Oom.Battleship.Model
 {
     public class Grid
     {
@@ -18,14 +21,50 @@
             }
         }
 
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private readonly Square?[,] squares;
+
         public IEnumerable<Square> Squares
         {
             get { return squares.Cast<Square>().Where(s => s != null); }
         }
 
-        public readonly int Rows;
-        public readonly int Columns;
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            return GetHorizontalAvailablePlacements(length);
+        }
 
-        private readonly Square?[,] squares;
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int c1 = c - length + 1; c1 <= c; ++c1)
+                            {
+                                temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
