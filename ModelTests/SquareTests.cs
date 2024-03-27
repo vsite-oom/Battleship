6bf140708a5dc807cfef70bfa84d6b44ae881c31
Mihael Krpes <mikrpes@vsite.hr>
2024-03-27 20:41:58 +0100

    Create test class for square

---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.OOM.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            int row = 4;
+            int col = 8;
+            var square = new Square(4, 8);
+            Assert.AreEqual(row, square.Row);
+            Assert.AreEqual(col,square.Column);
+        }
+    }
+}
