9dba83bae2d8007a606da51d4ba9443d75e682f1
Marko Pavić <mapavic@vsite.hr>
2024-03-27 20:57:01 +0100

    Implement grid contructor and tests for grid

---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vste.oom.battleship.model;
+
+namespace vsite.oom.battleship.model.tests
+{
+	[TestClass]
+	public class GridTests
+	{
+		[TestMethod]
+		public void ContructorCreatesGridWith50SquaresOr5RowsAnd10Columns()
+		{
+			int rows = 5;
+			int cols = 10;
+			var grid = new Grid(rows, cols);
+
+			Assert.AreEqual(50, grid.Squares.Count());
+		}
+	}
+}
