de1c1ecdbafbea223d87c8049c6ee96c2bb312cb
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-03-27 20:23:21 +0100

    - add Square constructor and properties

--- Model/Square.cs
+++ Model/Square.cs
@@ -2,5 +2,16 @@
 {
     public class Square
     {
+
+        public Square(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+
+
     }
 }
