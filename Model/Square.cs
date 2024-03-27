2cbbadba10094fcc529100b25e66f8e58215389d
Đuro Rukavina <djrukavina@vsite.hr>
2024-03-27 20:23:03 +0100

    Define Square constructor

--- Model/Square.cs
+++ Model/Square.cs
@@ -8,6 +8,13 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class Square
     {
+        public readonly int Row;
+        public readonly int Column;
 
+        public Square(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
