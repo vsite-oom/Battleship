e9cfa87f4e545abf69a40a15ea6a9de5e8a85177
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-03-27 20:57:18 +0100

    - add test for Grid Constructor

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -2,7 +2,28 @@
 {
     public class Grid
     {
+        public readonly int rows;
+        public readonly int columns;
 
+        private readonly Square?[,] _squares;
+        public IEnumerable<Square> Squares
+        {
+            get { return _squares.Cast<Square>().Where(x => x != null); }
+        }
+
+
+        public Grid(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+
+            _squares = new Square[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < columns; c++)
+                    _squares[r, c] = new Square(r, c);
+
+        }
 
     }
 }
