18538d4ba369e112f03a2267f53ce55125d15c7c
Kristian Bolarić <krbolaric@vsite.hr>
2024-03-27 20:22:53 +0100

    Define square constructor

--- Model/Square.cs
+++ Model/Square.cs
@@ -8,5 +8,13 @@ namespace vsite.oom.battleship.model
 {
     public class Square
     {
+        public Square(int row, int column)
+        {
+            Row = row;
+            Column=column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
     }
 }
---
+++ ModelTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ModelTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
