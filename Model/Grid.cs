58c48e404939d264b776589f83ea2330788e14ed
Julijan Sribar <julijan.sribar@gmail.com>
2024-03-27 20:56:44 +0100

    Implement Grid constructor and tests for it

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -2,5 +2,30 @@
 {
     public class Grid
     {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows, Columns];
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private readonly Square?[,] squares;
     }
 }
