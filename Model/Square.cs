2b77a27ecfde7f9c22e5684ce940252d43b28bc0
Julijan Sribar <julijan.sribar@gmail.com>
2024-03-27 20:22:53 +0100

    Define Square constructor

--- Model/Square.cs
+++ Model/Square.cs
@@ -2,5 +2,13 @@
 {
     public class Square
     {
+        public Square(int row, int column)
+        {
+            Row = row; 
+            Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
     }
 }
