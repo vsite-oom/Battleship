7087f7951e55fbc5fcf8a74351a34e682ac2bbe8
Julijan Sribar <julijan.sribar@gmail.com>
2024-03-27 20:56:44 +0100

    Implement Grid constructor and tests for it

---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+        {
+            int rows = 5;
+            int cols = 10;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(50, grid.Squares.Count());
+        }
+    }
+}
