3239519237afba35021a8c5540baa83357548c49
AnMal <junkie_@msn.com>
2024-03-27 20:56:55 +0100

    Implement Grid constructor and tests for it

---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+        {
+            int rows = 5;
+            int columns = 10;
+            Grid grid = new Grid(rows, columns);
+            Assert.AreEqual(50, grid.Squares.Count());
+
+
+        }
+    }
+}
