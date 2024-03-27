d014767c585cd6b08d4d5fe3ac836918f6cb0ad7
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-03-27 20:56:52 +0100

    commit - implement grid constructor and tests for grid

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -8,5 +8,32 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class Grid
     {
+        public Grid(int rows, int columns)  
+        { 
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows,Columns];
+
+            for (int r=0; r<Rows; r++)
+            {
+                for(int c=0; c<Columns; c++)
+                {
+                    squares[r,c] = new Square(r,c);
+                }
+            }
+        
+        }
+
+        public IEnumerable<Square> Squares // sakrivamo implementaciju
+        {     
+
+            get {  return squares.Cast<Square>().Where(s=>s !=null); }  // dobivamo squareove u mrezi bez Null!
+
+        }
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private readonly Square[,] squares;
     }
 }
