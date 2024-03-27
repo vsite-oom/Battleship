e1518acb934f54b5986c8f088aca044bc24c368c
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-03-27 21:20:47 +0100

    commit - add tests for GetHorizontalAvaliablePlacements

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -25,15 +25,53 @@ namespace Vsite.Oom.Battleship.Model
         
         }
 
-        public IEnumerable<Square> Squares // sakrivamo implementaciju
-        {     
 
-            get {  return squares.Cast<Square>().Where(s=>s !=null); }  // dobivamo squareove u mrezi bez Null!
-
-        }
         public readonly int Rows;
         public readonly int Columns;
 
         private readonly Square[,] squares;
+
+        public IEnumerable<Square> Squares // sakrivamo implementaciju
+        {
+
+            get { return squares.Cast<Square>().Where(s => s != null); }  // dobivamo squareove u mrezi bez Null!
+
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAvaliablePlacements(int length)
+        {
+            return GetHorizontalAvaliablePlacements(length);
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvaliablePlacements(int length)
+        {
+            List<IEnumerable<Square>>result=new List<IEnumerable<Square>>();
+
+            for (int r=0; r<Rows; r++)
+            {
+                int counter = 0;
+                for (int c=0; c<Columns; c++)
+                {
+                    if (squares[r,c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int c1=c - length + 1; c1<=c; ++c1)
+                            {
+                                temp.Add(squares[r,c1]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -15,5 +15,16 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.AreEqual(50, grid.Squares.Count());
         }
+
+        [TestMethod]
+        public void GetAvaliablePlacementsForGrid1x5Returns3PlacementsForShipWith3Squares()
+        {
+            int rows = 1;
+            int cols = 5;
+            int shipLength = 3;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(3, grid.GetAvaliablePlacements(shipLength).Count());
+        }
     }
 }
