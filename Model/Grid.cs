8736fba4a0c316a5b1a72dc992828693700dd38b
Josip Grabušić <jograbus@vsite.hr>
2024-03-27 21:25:08 +0100

    Implement GetAvailablePlacement for  horizontal ships

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -21,14 +21,54 @@ namespace Vsite.Oom.Battleship.Model
 
         }
 
-        public IEnumerable<Square> Squares
-        {
-            get { return squares.Cast<Square>().Where(s => s != null); }
-        }
+       
 
         public readonly int Rows;
         public readonly int Columns;
 
         private readonly Square?[,] squares;
+
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+
+        public IEnumerable<Square> GetAvailablePlacements(int length)
+        {
+            return GetHorizontalAvailablePlacements(length);
+
+        }
+
+        public IEnumerable<Square> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        counter++;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int c1=c-length+1; c1<=c; ++c1) {
+                                temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);
+
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }   
+            }
+            return result;
+
+        }
     } 
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -15,5 +15,30 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.AreEqual(50, grid.Squares.Count());
         }
+
+        [TestMethod]
+        public void GetAvailablePlacementsForGridx5Returns3PlacementsForShipWith3Squares()
+        {
+            int rows = 1;
+            int cols = 5;
+            int shipLength= 3;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+
+        }
+
+
+        [TestMethod]
+        public void GetAvailablePlacementsForGrid7xReturns6PlacementsForShipWith2Squares()
+        {
+            int rows = 7;
+            int cols = 1;
+            int shipLength = 2;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(6, grid.GetAvailablePlacements(shipLength).Count());
+
+        }
     }
 }
