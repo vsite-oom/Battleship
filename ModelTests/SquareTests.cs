51854a4b5e9065d142009e67037725e6beb2a12f
AnMal <junkie_@msn.com>
2024-03-27 20:42:37 +0100

    Add test method for square

---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWithRowAndColumnProvided()
+        {
+            int row = 4;
+            int column = 8;
+            var square = new Square(row, column);
+        
+            Assert.AreEqual(row, square.Row);
+            Assert.AreEqual(column, square.Column);
+        }
+    }
+}
