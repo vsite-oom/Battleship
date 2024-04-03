c44f4e6a50ca6a1837a3b8dea8b0d0b26fa5e271
Filip Les <ketvok@gmail.com>
2024-04-03 15:39:16 +0200

    Implement GetAvailablePlacements for horizontal ships

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,8 +15,10 @@ namespace Vsite.Oom.Battleship.Model
             Rows = rows;
             Columns = columns;
 
+            // Rezerviranje prostora.
             squares = new Square[Rows, Columns];
 
+            // Inicijalizacija pojedinih polja.
             for (int r = 0; r < Rows; r++)
             {
                 for (int c = 0; c < Columns; c++)
@@ -25,14 +28,61 @@ namespace Vsite.Oom.Battleship.Model
             }
         }
 
+        public readonly int Rows;
+        public readonly int Columns;
+
+        // Dvodimenzionalno polje. Squarevi mogu biti null, to će nam
+        // trebati kasnije da ih možemo eliminirati.
+        public readonly Square?[,] squares;
+
+        // Prema testu moramo imati svojstvo Squares koje će vratiti
+        // jednodimenzionalnu kolekciju Squareova.
+        // Budući da ne želimo otkrivati interne detalje implementacije
+        // iskoristiti ćemo IEnumerable sučelje u kojem će biti
+        // pojedinačni Squareovi i taj property će biti Squares.
+        // To će nam biti getter.
+        // Castamo dvodimenzionalni niz squares u jednodimenzionalni niz
+        // Square, ali želimo dobiti samo squareove koji su trenutno u
+        // mreži, ne želimo one koje su null reference.
         public IEnumerable<Square> Squares
         {
             get { return squares.Cast<Square>().Where(s => s != null); }
         }
 
-        public readonly int Rows;
-        public readonly int Columns;
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            return GetHorizontalAvailablePlacements(length);
+        }
 
-        public readonly Square?[,] squares;
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)  // Idemo po redovima.
+            {
+                int counter = 0;  // Na pocetku reda imamo brojac kojeg postavljamo na 0.
+                for (int c = 0; c < Columns; c++)  // U svakom redu idemo polje po polje (stupac po stupac).
+                {
+                    if (squares[r, c] != null)  // Ako je polje slobodno...
+                    {
+                        ++counter;  // ...povećamo brojač.
+                        if (counter >= length)  // Ako je brojač veći ili jednak traženoj duljini...
+                        {
+                            List<Square> temp = new List<Square>();  // Lista koju popunimo poljima u koja stane brod.
+                            for (int c1 = c - length + 1; c1 <= c; ++c1)
+                            {
+                                temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);  // Dodamo listu u rezultat.
+                        }
+                    }
+                    else  // Ako smo naletili na eliminirano polje, brojač se vraća na 0.
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -15,5 +15,25 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.AreEqual(50, grid.Squares.Count());
         }
+        [TestMethod]
+        public void GetAvailablePlacementsForGrid1x5Returns3PlacementsForShipWith3Squares()
+        {
+            int rows = 1;
+            int cols = 5;
+            int shipLength = 3;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+        }
+        [TestMethod]
+        public void GetAvailablePlacementsForGrid7x1Returns6PlacementsForShipWith2Squares()
+        {
+            int rows = 7;
+            int cols = 1;
+            int shipLength = 2;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(6, grid.GetAvailablePlacements(shipLength).Count());
+        }
     }
 }
