e6d5816b1d15d7d73761eaf3073047d61c4392c1
Filip Les <ketvok@gmail.com>
2024-04-03 16:12:56 +0200

    Implement GetAvailablePlacements for vertical ships

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -51,7 +51,7 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return GetHorizontalAvailablePlacements(length);
+            return GetHorizontalAvailablePlacements(length).Concat<IEnumerable<Square>>(GetVerticalAvailablePlacements(length));
         }
 
         private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
@@ -68,10 +68,41 @@ namespace Vsite.Oom.Battleship.Model
                         ++counter;  // ...povećamo brojač.
                         if (counter >= length)  // Ako je brojač veći ili jednak traženoj duljini...
                         {
-                            List<Square> temp = new List<Square>();  // Lista koju popunimo poljima u koja stane brod.
+                            List<Square> temp = new List<Square>();  // ...napravimo listu polja u koja stane brod.
                             for (int c1 = c - length + 1; c1 <= c; ++c1)
                             {
                                 temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);  // Dodamo listu u rezultat.
+                        }
+                    }
+                    else  // Ako smo naletili na eliminirano polje, brojač se vraća na 0.
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)  // Idemo po stupcima.
+            {
+                int counter = 0;  // Na pocetku stupca imamo brojac kojeg postavljamo na 0.
+                for (int r = 0; r < Rows; r++)  // U svakom stupcu idemo polje po polje (red po red).
+                {
+                    if (squares[r, c] != null)  // Ako je polje slobodno...
+                    {
+                        ++counter;  // ...povećamo brojač.
+                        if (counter >= length)  // Ako je brojač veći ili jednak traženoj duljini...
+                        {
+                            List<Square> temp = new List<Square>();  // ...napravimo listu polja u koja stane brod.
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
+                            {
+                                temp.Add(squares[r1, c]!);
                             }
                             result.Add(temp);  // Dodamo listu u rezultat.
                         }
