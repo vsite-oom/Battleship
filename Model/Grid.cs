fcaff6b65021b9a9008b5b0441591b2306552878
Hrvoje Janđel <hrjandje@vsite.hr>
2024-04-08 22:22:37 +0200

    Implement grid class and test for it

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -15,6 +15,7 @@ namespace Vsite.Oom.Battleship.Model
             Columns = columns;
 
             squares = new Square[Rows, Columns];
+
             for (int r = 0; r < Rows; r++)
             {
                 for (int c = 0; c < Columns; c++)
@@ -22,54 +23,87 @@ namespace Vsite.Oom.Battleship.Model
                     squares[r, c] = new Square(r, c);
                 }
             }
+        }
 
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            //return GetHorizontalAvailablePlacements(length);
+            return GetVerticalAvailablePlacements(length);
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                int counter = 0;
+
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int c1 = c - length + 1; c1 <= c; ++c1)
+                            {
+                                temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)
+            {
+                int counter = 0;
+
+                for (int r = 0; r < Rows; r++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
+                            {
+                                temp.Add(squares[r1, c]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
         }
 
         public readonly int Rows;
         public readonly int Columns;
 
         private readonly Square?[,] squares;
-        public IEnumerable<Square> Squares
-        {
-            get { return squares.Cast<Square>().Where(s => s != null); }
-        }
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
-        {
-            return GetHorizontalAvialablePlacements(length);
-        }
-        private IEnumerable<IEnumerable<Square>> GetHorizontalAvialablePlacements(int length)
-        {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-            for (int r = 0; r < Rows; r++)
-            {
-                for (int c = 0; c <= Columns; c++)
-                {
-                    int counter = 0;
-
-                    {
-                        List <Square> temp = new List<Square>();
-                        if (squares[r, c] != null)
-                        {
-                            ++counter;
-
-                            if (counter >= length)
-                            {
-                                for (int c1 = c - length + 1; c1 <= c; ++c1) 
-                                {
-                                    temp.Add(squares[r, c1]!);
-                                }
-                            }
-                            else
-                            {
-                                counter = 0;
-
-                            }
-                        }
-                        result.Add(temp);
-                    }
-                }
-            }
-            return result;
-        }
     }
 }
