add6e248f69e125c8ecb130535e0a7e02a536baf
Julijan Sribar <julijan.sribar@gmail.com>
2024-04-09 07:57:17 +0200

    Implement GetVerticalAvailablePlacements method

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -33,17 +33,17 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return GetHorizontalAvailablePlacements(length);
+            return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
         }
 
         private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
         {
             List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
 
-            for (int r = 0; r < Rows; r++)
+            for (int r = 0; r < Rows; ++r)
             {
                 int counter = 0;
-                for (int c = 0; c < Columns; c++)
+                for (int c = 0; c < Columns; ++c)
                 {
                     if (squares[r, c] != null)
                     {
@@ -54,6 +54,37 @@ namespace Vsite.Oom.Battleship.Model
                             for (int c1 = c - length + 1; c1 <= c; ++c1)
                             {
                                 temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; ++c)
+            {
+                int counter = 0;
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
+                            {
+                                temp.Add(squares[r1, c]!);
                             }
                             result.Add(temp);
                         }
