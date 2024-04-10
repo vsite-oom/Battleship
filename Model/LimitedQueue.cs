2e0f03376708bcdf7cc0d8a11fd34c190e9cf75d
Josip Grabušić <jograbus@vsite.hr>
2024-04-10 20:43:19 +0200

    Implement limited queue

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Batelship.Model
+{
+   public class LimitedQueue<T> : Queue<T>
+    {
+        public LimitedQueue(int maxItems)
+        {
+            this.maxItems = maxItems;     
+        }
+        private readonly int maxItems;
+
+        public new void Enqueue(T item)
+        {
+           while (Count >= maxItems)
+            {
+                Dequeue();
+            }
+            base.Enqueue(item);
+        }
+
+       
+    }
+}
---
+++ Model/LimitedQueueTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Batelship.Model
+{
+    [TestClass]
+    public class LimitedQueueTests
+    {
+        [TestMethod]
+        public void EnqueueAddsItemToQueue()
+        {
+            var queue = new LimitedQueue<int>(3);
+            Assert.AreEqual(0, queue.Count);
+            
+            queue.Enqueue(1);
+            Assert.AreEqual(1, queue.Count);
+            queue.Enqueue(2);
+            Assert.AreEqual(2, queue.Count);
+
+        }
+
+        [TestMethod]
+        public void QueueRemovesExtraItemsAfterItIsFilled()
+        {
+            var queue = new LimitedQueue<int>(3);
+            Assert.AreEqual(0, queue.Count);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Assert.AreEqual(3, queue.Count);
+
+            queue.Enqueue(4);
+           
+            Assert.AreEqual(3, queue.Count);
+            Assert.IsFalse(queue.Contains(1));
+
+            queue.Enqueue(5);
+            Assert.AreEqual(3, queue.Count);
+            Assert.IsFalse(queue.Contains(1));
+            Assert.IsFalse(queue.Contains(2));
+
+        }
+
+    }
+
+
+   
+
+    }
+}
