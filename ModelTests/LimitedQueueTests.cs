2b499629609e0eea8aff9050b6c018b2d3bdaf51
Filip Les <ketvok@gmail.com>
2024-04-10 20:43:29 +0200

    Implement LimitedQueue

---
+++ ModelTests/LimitedQueueTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class LimitedQueueTests
+    {
+        [TestMethod]
+        public void EnqueueInsertsItemToQueue()
+        {
+            var queue = new LimitedQueue<int>(3);
+
+            Assert.AreEqual(0, queue.Count);
+
+            queue.Enqueue(1);
+            Assert.AreEqual(1, queue.Count);
+
+            queue.Enqueue(2);
+            Assert.AreEqual(2, queue.Count);
+        }
+
+        [TestMethod]
+        public void QueueRemovesExtraItemsAfterItIsFilled()
+        {
+            var queue = new LimitedQueue<int>(3);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Assert.AreEqual(3, queue.Count);
+
+            queue.Enqueue(4);
+            Assert.AreEqual(3, queue.Count);
+            Assert.IsFalse(queue.Contains(1));
+
+            queue.Enqueue(5);
+            Assert.AreEqual(3, queue.Count);
+            Assert.IsFalse(queue.Contains(1));
+            Assert.IsFalse(queue.Contains(2));
+        }
+    }
+
+
+}
