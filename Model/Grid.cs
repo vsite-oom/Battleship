6720d966ef6b8e0a22948417255156c10056532f
Šime Šprljan <sisprlja@vsite.hr>
2024-04-10 16:54:06 +0200

    Fixed errors from ssprljan and implemented GetVerticalAvalilablePlacements

---
+++ Model/Grid.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Vsite.Oom.battleship.Model
+{
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows, Columns];
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+
+        }
+
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private readonly Square?[,] squares;
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+        public IEnumerable<IEnumerable<Square>> GetAvailableHorizontalPlacements(int length)
+        {
+            return GetHorizontalAvailablePlacements(length);
+        }
+        public IEnumerable<IEnumerable<Square>> GetAvailableVerticalPlacements(int length)
+        {
+            return GetVerticalAvailablePlacements(length);
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                int counter = 0;
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int c1 = c - length + 1; c1 <= c; c1++)
+                            {
+                                temp.Add(squares[r, c1]);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int height)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)
+            {
+                int counter = 0;
+                for (int r = 0; r < Rows; r++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= height)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int r1 = r - height + 1; r1 <= r; r1++)
+                            {
+                                temp.Add(squares[r1, c]);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ModelTest/SquareTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vsite.Oom.battleship.Model;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquarewithRowColumn()
+        {
+            int row = 4;
+            int col = 8;
+            var square = new Square(row, col);
+            Assert.AreEqual(row, square.Row);
+            Assert.AreEqual(col, square.Column);
+        }
+    }
+}
