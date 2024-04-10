e9a9c6ae1227afd95acd51fe6c492e6b5034d5e6
Julijan Sribar <julijan.sribar@gmail.com>
2024-04-10 20:43:10 +0200

    Implement LimitedQueue

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,21 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class LimitedQueue<T> : Queue<T>
+    {
+        public LimitedQueue(int maxItems)
+        {
+            this.maxItems = maxItems;
+        }
+
+        private readonly int maxItems;
+
+        public new void Enqueue(T item)
+        {
+            while (Count >= maxItems)
+            {
+                Dequeue();
+            }
+            base.Enqueue(item);
+        }
+    }
+}
