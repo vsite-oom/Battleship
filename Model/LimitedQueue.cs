8f8251f2f89e6ccbb9b707dd3b4b50df2cdf69f0
Đuro Rukavina <djrukavina@vsite.hr>
2024-04-10 20:43:29 +0200

    Implement Limited Queue

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,20 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class LimitedQueue<T> : Queue<T>
+    {
+        private readonly int maxItems;
+        public LimitedQueue(int maxItems)
+        {
+            this.maxItems = maxItems;
+        }
+
+        public new void Enqueue(T item)
+        {
+            while (Count >=  maxItems)
+            {
+                Dequeue();
+            }
+            base.Enqueue(item);
+        }
+    }
+}
