8223f6d4f6569dba7a8251037cbec05472259cdf
Filip Les <ketvok@gmail.com>
2024-04-10 20:27:21 +0200

    Implement GetAvailablePlacements for grid 5x5

--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -35,5 +35,15 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.AreEqual(6, grid.GetAvailablePlacements(shipLength).Count());
         }
+        [TestMethod]
+        public void GetAvailablePlacementsForGrid5x5Returns20PlacementsForShipWith4Squares()
+        {
+            int rows = 5;
+            int cols = 5;
+            int shipLength = 4;
+            var grid = new Grid(rows, cols);
+
+            Assert.AreEqual(20, grid.GetAvailablePlacements(shipLength).Count());
+        }
     }
 }
