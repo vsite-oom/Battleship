f8ebfc2be52ae15364e989504319ec42029cc637
Hrvoje Janđel <hrjandje@vsite.hr>
2024-04-11 18:59:30 +0200

    Add tests after squares are eliminated

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -9,6 +9,10 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class Grid
     {
+        public readonly int Rows;
+        public readonly int Columns;
+        private readonly Square?[,] squares;
+
         public Grid(int rows, int columns)
         {
             Rows = rows;
@@ -27,35 +31,61 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<Square> Squares
         {
-            get { return squares.Cast<Square>().Where(s => s != null); }
+            get
+            {
+                return squares.Cast<Square>().Where(s => s != null);
+            }
         }
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            
             return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
         }
 
         private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
         {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-            var queue= new LimitedQueue<Square>(length);
+            List<IEnumerable<Square>> result = new();
+
             for (int r = 0; r < Rows; r++)
             {
-                int counter = 0;
+                var queue = new LimitedQueue<Square>(length);
 
                 for (int c = 0; c < Columns; c++)
                 {
                     if (squares[r, c] != null)
                     {
-                        queue.Enqueue(squares[r,c]!);
-                        if (queue.Count() == length)
+                        queue.Enqueue(squares[r, c]!);
+
+                        if (queue.Count >= length)
                         {
-                            List<Square> temp = new List<Square>();
-                            for (int c1 = c - length + 1; c1 <= c; ++c1)
-                            {
-                                temp.Add(squares[r, c1]!);
-                            }
+                            result.Add(queue.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; ++c)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int r = 0; r < Rows; ++r)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        queue.Enqueue(squares[r, c]!);
+                        if (queue.Count >= length)
+                        {
                             result.Add(queue.ToArray());
                         }
                     }
@@ -68,43 +98,9 @@ namespace Vsite.Oom.Battleship.Model
             return result;
         }
 
-
-        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        public void EliminateSquare(int row, int column)
         {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-
-            for (int c = 0; c < Columns; c++)
-            {
-                var queue = new LimitedQueue<Square>(length);
-                
-
-                for (int r = 0; r < Rows; r++)
-                {
-                    if (squares[r, c] != null)
-                    {
-                        queue.Enqueue(squares[r,c]!);
-                        if (queue.Count()== length)
-                        {
-                            List<Square> temp = new List<Square>();
-                            for (int r1 = r - length + 1; r1 <= r; ++r1)
-                            {
-                                result.Add(queue.ToArray());
-                            }
-                           
-                        }
-                    }
-                    else
-                    {
-                        queue.Clear();
-                    }
-                }
-            }
-            return result;
+            squares[row, column] = null;
         }
-
-        public readonly int Rows;
-        public readonly int Columns;
-
-        private readonly Square?[,] squares;
     }
 }
