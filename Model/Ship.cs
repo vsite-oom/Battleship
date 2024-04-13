068606d8900464c33ed2eb8e3ffa7c24028b7b13
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-04-13 10:56:43 +0200

    Implement Ship constructor

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -8,5 +8,16 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class Ship
     {
+        public Ship(IEnumerable<Square> squares) 
+        { 
+            this.squares = squares;
+        }
+
+        private readonly IEnumerable<Square> squares;
+
+        public bool Contains(int row, int column)
+        {
+            return squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column)!=null;
+        }
     }
 }
