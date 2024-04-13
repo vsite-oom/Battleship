0d4d5f03eb5b4895eb402f3f58985a7b1db5a4e4
Sonja Perković <soperkov@vsite.hr>
2024-04-13 11:09:06 +0200

    implement createShip method in fleet class

--- ModelTests/FleetTests.cs
+++ ModelTests/FleetTests.cs
@@ -10,5 +10,18 @@
 
             Assert.AreEqual(0, fleet.Ships.Count());
         }
+
+        [TestMethod]
+        public void CreateShipAddsNewShipToFleet()
+        {
+            var fleet = new Fleet();
+
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+
+
+            fleet.CreateShip(squares);
+
+            Assert.AreEqual(1, fleet.Ships.Count());
+        }
     }
 }
