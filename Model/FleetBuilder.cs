41e940c95b6c7dad98965314ea16bbe2b3d03f1f
Sonja Perković <soperkov@vsite.hr>
2024-04-13 11:35:09 +0200

    start implementation of fleetBuilder

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -5,12 +5,30 @@
         public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths) 
         {
             fleetGrid = new Grid(gridRows, gridColumns);
-            this.shipLengths = new List<int> (shipLengths);
+            this.shipLengths = new List<int> (shipLengths.OrderByDescending(length => length));
         }
 
         private readonly Grid fleetGrid;
 
         private readonly List<int> shipLengths;
+
+        public Fleet CreateFleet()
+        {
+            var fleet = new Fleet();
+
+            for (int i = 0; i < shipLengths.Count; ++i)
+            {
+                var candidates = fleetGrid.GetAvailablePlacements(shipLengths[i]);
+                var selectedIndex = random.Next(candidates.Count());
+                var selected = candidates.ElementAt(selectedIndex);
+
+                fleet.CreateShip(selected);
+            }
+
+            return fleet;
+        }
+
+        private readonly Random random;
     }
 
 }
