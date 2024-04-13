e906d2cfe5dda361b68520379d028e3df5671ed4
Josip Grabušić <jograbus@vsite.hr>
2024-04-13 11:35:14 +0200

    Start implementation of Fleetbuilder

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -11,12 +11,31 @@ namespace Vsite.Oom.Battleship.Model
         public FleetBuilder(int gridRows, int gridColumns, int[] shiplengths)
         {
             fleetGrid = new Grid(gridRows, gridColumns);
-            this.shiplengths = new List<int>(shiplengths);
+            this.shiplengths = new List<int>(shiplengths.OrderByDescending(length => length));
         }
 
         private readonly Grid fleetGrid;
 
         private readonly List<int> shiplengths;
+
+        public Fleet CreateFleet()
+        {
+            var fleet = new Fleet();
+            for (int i = 0; i < shiplengths.Count; i++)
+            {
+                var candidates = fleetGrid.GetVerticalAvailablePlacements(shiplengths[i]);
+                var selectedIndex = random.Next(candidates.Count());
+                var selected = candidates.ElementAt(selectedIndex);
+
+                fleet.CreateShip(selected);
+
+
+            }
+
+            return fleet;
+        }
+
+        private readonly Random random;
        
     }
 }
