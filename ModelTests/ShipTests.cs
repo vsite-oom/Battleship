5aef8f7d691784c41f253e2575799e91acba6440
Sonja Perković <soperkov@vsite.hr>
2024-04-13 10:56:42 +0200

    implement ship constructor

---
+++ ModelTests/ShipTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class ShipTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesShipWithSquaresProvided()
+        {
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+            var ship = new Ship(squares);
+
+            Assert.IsTrue(ship.Contains(1, 4));
+
+        }
+    }
+}
