4725f5c4d723676d45ef8bbc7623a1a4a8e4f198
Marko Pavić <mapavic@vsite.hr>
2024-04-13 12:18:22 +0200

    Add SquareEliminator class

---
+++ ModelTests/SquareEliminatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vste.oom.battleship.model;
+
+namespace vsite.oom.battleship.model.tests
+{
+	[TestClass]
+	public class SquareEliminatorTests
+	{
+		[TestMethod]
+		public void ForSquares4x3To4x6Returns18SquaresIncludingSurroundingSquares()
+		{
+			var eliminator = new SquareEliminator();
+			var shipSquares=new List<Square> { new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6) };
+
+			Assert.AreEqual(18, eliminator.ToEliminate(shipSquares, 10, 10).Count());
+		}
+		
+		[TestMethod]
+		public void ForSquares3x9To4x9Returns8SquaresIncludingSurroundingSquares()
+		{
+			var eliminator = new SquareEliminator();
+			var shipSquares=new List<Square> { new Square(3,9), new Square(4, 9) };
+			var toEliminate = eliminator.ToEliminate(shipSquares, 10, 10);
+
+			Assert.AreEqual(8, toEliminate.Count());
+
+			//TODO: check if boundary coordiantes are included
+		}
+	}
+}
