6f0962889f88f9873f677be2b4881b7e32810910
Marko Pavić <mapavic@vsite.hr>
2024-04-13 11:19:17 +0200

    Implement FleetBuilder constructor

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -8,5 +8,13 @@ namespace vste.oom.battleship.model
 {
 	public class FleetBuilder
 	{
+		public FleetBuilder(int gridRows, int gridColums, int[] shipLengths) 
+		{
+			fleetGrid = new Grid(gridRows, gridColums);
+			this.shipLengths=new List<int>(shipLengths);
+		}
+
+		private readonly Grid fleetGrid;
+		private readonly List<int> shipLengths;
 	}
 }
