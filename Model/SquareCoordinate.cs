896c4b3d3e8286270d48c9e430aae63b7bd74c5f
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-04-13 12:18:48 +0200

    Implement SquareEliminator

---
+++ Model/SquareCoordinate.cs
@@ -0,0 +1,13 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public struct SquareCoordinate
+    {
+        public SquareCoordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
---
+++ ModelTests/SquareEliminatorTests.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareEliminatorTests
+    {
+        [TestMethod]
+        public void ForSquares4x3To4x6Returns18SquaresIncludingSurroundingSquares()
+        {
+            var eliminator = new SquareEliminator();
+            var shipSquares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+
+            Assert.AreEqual(18, eliminator.ToEliminate(shipSquares, 10,10).Count());
+        }
+
+        public void ForSquares4x9To4x9Returns8SquaresIncludingSurroundingSquares()
+        {
+            var eliminator = new SquareEliminator();
+            var shipSquares = new List<Square> { new Square(3, 9), new Square(4, 9), new Square(4, 9) };
+
+            var toEliminate = eliminator.ToEliminate(shipSquares, 10, 10);
+            Assert.AreEqual(8, toEliminate.Count());
+
+            //TODO
+        }
+    }
+}
