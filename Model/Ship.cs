2c4dab37f6dfb51a57682060eee02928045450ea
Marko Pavić <mapavic@vsite.hr>
2024-04-13 10:56:42 +0200

    Implement Ship constructor

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -2,5 +2,14 @@
 {
 	public class Ship
 	{
+		public Ship(IEnumerable<Square> squares)
+		{
+			this.squares=squares;
+		}
+		private readonly IEnumerable<Square> squares;
+		public bool Contains(int row, int column)
+		{
+			return squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column) !=null;
+		}
 	}
 }
---
+++ ModelTests/ShipTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vste.oom.battleship.model;
+
+namespace vsite.oom.battleship.model.tests
+{
+	[TestClass]
+	public class ShipTests
+	{
+		[TestMethod]
+		public void ContructorCreatesShipWithSquaresProvided()
+		{
+			var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+			var ship = new Ship(squares);
+
+			Assert.IsTrue(ship.Contains(1, 4));
+
+		}
+	}
+}
