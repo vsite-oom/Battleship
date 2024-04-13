8e51199b46f85b90048a2abb3940e6619ce61f73
Julijan Sribar <julijan.sribar@gmail.com>
2024-04-13 11:19:07 +0200

    Implement FleetBuilder constructor

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -2,5 +2,14 @@
 {
     public class FleetBuilder
     {
+        public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths)
+        {
+            fleetGrid = new Grid(gridRows, gridColumns);
+            this.shipLengths = new List<int>(shipLengths);
+        }
+
+        private readonly Grid fleetGrid;
+
+        private readonly List<int> shipLengths;
     }
 }
