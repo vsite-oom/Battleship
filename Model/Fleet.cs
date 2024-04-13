3b4d1f11dfe9c9ecb3ee3dcb9fec859246145993
Marko Pavić <mapavic@vsite.hr>
2024-04-13 11:09:24 +0200

    Implement CreateShip method in Fleet class

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -11,5 +11,11 @@ namespace vste.oom.battleship.model
 		private List<Ship> ships = new List<Ship>();
 
 		public IEnumerable<Ship> Ships { get {  return ships; } }
+
+		public void CreateShip(List<Square> squares)
+		{
+			var ship=new Ship(squares);
+			ships.Add(ship);
+		}
 	}
 }
--- ModelTests/FleetTests.cs
+++ ModelTests/FleetTests.cs
@@ -13,5 +13,16 @@ namespace vste.oom.battleship.model.tests
 
 			Assert.AreEqual(0, fleet.Ships.Count());
 		}
+		
+		[TestMethod]
+		public void CreateShipAddsNewShipToFleet()
+		{
+			var fleet = new Fleet();
+			var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+
+			fleet.CreateShip(squares);
+
+			Assert.AreEqual(1, fleet.Ships.Count());
+		}
 	}
 }
