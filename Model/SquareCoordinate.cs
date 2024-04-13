27b9affa28a35766c55c3d2e84e975370a9778dd
Sonja Perković <soperkov@vsite.hr>
2024-04-13 12:20:07 +0200

    Add squareEliminator

---
+++ Model/SquareCoordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public struct SquareCoordinate
+    {
+        public SquareCoordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
