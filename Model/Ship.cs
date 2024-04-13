094743d19e2a94c0c246fe7bd7f582999eb4cc08
AnMal <junkie_@msn.com>
2024-04-13 10:56:45 +0200

    Implement Ship constructor

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -2,5 +2,16 @@
 {
     public class Ship
     {
+        private readonly IEnumerable<Square> squares;
+
+        public Ship(IEnumerable<Square> squares)
+        {
+            this.squares = squares;
+        }
+
+        public bool Contains(int row, int column)
+        {
+            return squares.FirstOrDefault(s => s.Row == row && s.Column == column) is not null;
+        }
     }
 }
