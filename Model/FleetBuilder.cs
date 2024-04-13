b985bee99c222b6fed2ea9947e71953e922f7853
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-04-13 11:35:06 +0200

    Start Implementation of FleetBuilder

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -5,11 +5,31 @@
         public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths)
         {
             fleetGrid = new Grid(gridRows, gridColumns);
-            this.shipLengths = new List<int>(shipLengths);
+            this.shipLengths = new List<int>(shipLengths.OrderByDescending(length => length));
         }
 
         private readonly Grid fleetGrid;
 
         private readonly List<int> shipLengths;
+
+        public Fleet CreateFleet()
+        {
+            var fleet = new Fleet();
+
+            for (int i = 0; i<shipLengths.Count; ++i)
+            {
+                var candidates = fleetGrid.GetAvailablePlacements(shipLengths[1]);
+                var selectedIndex = random.Next(candidates.Count());
+                var selected = candidates.ElementAt(selectedIndex);
+
+                fleet.CreateShip(selected);
+
+
+            }
+
+            return fleet;
+        }
+
+        private Random random;
     }
 }
