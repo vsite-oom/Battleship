bbb11d9faac8214fbc940ad170499a875de4b71b
Julijan Sribar <julijan.sribar@gmail.com>
2024-04-13 10:56:39 +0200

    Implement Ship constructor

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -2,5 +2,16 @@
 {
     public class Ship
     {
+        public Ship(IEnumerable<Square> squares) 
+        {
+            this.squares = squares;
+        }
+
+        private readonly IEnumerable<Square> squares;
+
+        public bool Contains(int row, int column)
+        {
+            return squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column) != null;
+        }
     }
 }
