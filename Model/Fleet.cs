c5f3735ea3463668ee0235179b748c0b541d95ea
Julijan Sribar <julijan.sribar@gmail.com>
2024-04-13 11:09:14 +0200

    Implement CreateShip method in Fleet class

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -1,10 +1,16 @@
-﻿namespace Vsite.Oom.Battleship.Model
+﻿
+namespace Vsite.Oom.Battleship.Model
 {
     public class Fleet
     {
-
         private List<Ship> ships = new List<Ship>();
 
         public IEnumerable<Ship> Ships { get {  return ships; } }
+
+        public void CreateShip(List<Square> squares)
+        {
+            var ship = new Ship(squares);
+            ships.Add(ship);
+        }
     }
 }
