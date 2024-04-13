efb9c97d06db7401d0cf5d196ea6e02e95d96c32
Đuro Rukavina <djrukavina@vsite.hr>
2024-04-13 11:19:11 +0200

    Implement FleetBuilder constructor

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -8,6 +8,12 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class FleetBuilder
     {
-
+        private readonly Grid fleetGrid;
+        private readonly List<int> shipLengths;
+        public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths)
+        {
+            fleetGrid = new Grid(gridRows, gridColumns);
+            this.shipLengths = new List<int>(shipLengths);
+        }
     }
 }
