653c4ab4c4decded241758e47d26cb6ab2dbf1d5
Kristian Mesic <kristian.mesic303@gmail.com>
2024-04-17 17:13:17 +0200

    - implementation of SquareEliminator class - add tests

---
+++ Model/SquareCoordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public struct SquareCoordinate
+    {
+        public readonly int Row;
+        public readonly int Column;
+
+        public SquareCoordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
---
+++ ModelTests/SquareEliminatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SquareEliminatorTests
+    {
+        [TestMethod]
+        public void ForSquares4x3To4x6Returns18SquaresIncludingSurroundingSquares()
+        {
+            var eliminator = new SquareEliminator();
+            var shipSquares = new List<Square> 
+            { 
+                new Square(4, 3),
+                new Square(4, 4),
+                new Square(4, 5),
+                new Square(4, 6)
+            };
+
+            Assert.AreEqual(18, eliminator.ToEliminate(shipSquares, 10, 10).Count());
+        }
+        [TestMethod]
+        public void ForSquares3x9To4x9Returns8SquaresIncludingSurroundingSquares()
+        {
+            var eliminator = new SquareEliminator();
+            var shipSquares = new List<Square>
+            {
+                new Square(3, 9), 
+                new Square(4, 9)
+            };
+
+            var toEliminate = eliminator.ToEliminate(shipSquares, 10, 10);
+            Assert.AreEqual(8, toEliminate.Count());
+        }
+    }
+}
