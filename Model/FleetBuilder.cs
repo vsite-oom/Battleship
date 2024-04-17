e46de5604a82b79f3c0fc75a3119919fc101c49f
Kristian Mesic <kristian.mesic303@gmail.com>
2024-04-17 16:38:57 +0200

    - add FleetBuilder constructor

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -3,5 +3,13 @@
     public class FleetBuilder
     {
         //FirstPart -> Initialization of game; 
+
+        private readonly Grid fleetGrid;
+        private readonly List<int> shipLengths;
+        public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths)
+        {
+            fleetGrid = new Grid(gridRows, gridColumns);
+            this.shipLengths = new List<int>(shipLengths);
+        }
     }
 }
