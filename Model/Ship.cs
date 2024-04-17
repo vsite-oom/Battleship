a109265828081bb2ff1e22d299431b725293f6e6
Kristian Mesic <kristian.mesic303@gmail.com>
2024-04-17 16:28:39 +0200

    - add Ship constructor and Test class

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -1,12 +1,20 @@
-﻿namespace Vsite.Oom.Battleship.Model
+﻿using System.Collections.Immutable;
+
+namespace Vsite.Oom.Battleship.Model
 {
     public class Ship
     {
-        
-
-        public Ship() 
+        public Ship(IEnumerable<Square> squares)
         {
-        
+            this.squares = squares;
+        }
+
+        private readonly IEnumerable<Square> squares;
+
+        public bool Contains(int row, int column)
+        {
+            return squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column) != null;
         }
     }
+
 }
---
+++ ModelTests/ShipTests.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class ShipTests
+    {
+        [TestMethod]
+        public void ConstructorCreatesShipWithSquaresProvided()
+        {
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+            var ship = new Ship(squares);
+
+            Assert.IsTrue(ship.Contains(1, 4));
+
+        }
+    }
+}
