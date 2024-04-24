226c083d875f799ce693826bb2458cbb4ee1e2f3
Filip Les <ketvok@gmail.com>
2024-04-24 18:43:00 +0200

    Add SquareEliminator class

---
+++ Model/SquareCoordinate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public struct SquareCoordinate
+    {
+        public SquareCoordinate(int row, int columns)
+        {
+            Row = row;
+            Column = Column;
+        }
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
---
+++ Model/SquareEliminator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    // "SquareEliminator ne zna za Squareove koji su okolo. Umjesto da te okolne stvaramo preko konstruktora, mi ćemo umjesto Squareova vraćati parove redak, stupac."
+    // Za to u Model moramo dodati strukturu SquareCoordinate.
+    // Prijašnjih godina su se koristili Squareovi, ali to je kompliciralo kod."
+    public class SquareEliminator
+    {
+        public  IEnumerable<SquareCoordinate> ToEliminate(List<Square> shipSquares, int rows, int columns)
+        {
+            // Grid vraća već sortirane Squareove od najlijevijeg, najgornjeg, prema dolje, pa ovdje možemo pretpostaviti da će nam prvi imati najmanje koordinate.
+            var first = shipSquares.First();
+            int firstRow = first.Row;
+            int firstColumn = first.Column;
+            if (firstRow > 0)
+            {
+                --firstRow;
+            }
+            if (firstColumn > 0)
+            {
+                --firstColumn;
+            }
+
+            var last = shipSquares.Last();
+            int lastRow = last.Row;
+            int lastColumn = last.Column;
+            if (lastRow < rows - 1)
+            {
+                ++lastRow;
+            }            
+            if (lastColumn < columns - 1)
+            {
+                ++lastColumn;
+            }
+
+            var result = new List<SquareCoordinate>();
+            for (int r = firstRow; r <= lastRow; ++r)
+            {
+                for (int c = firstColumn; c <= lastColumn; ++c)
+                {
+                    result.Add(new SquareCoordinate(r, c));
+                }
+            }
+            return result;
+
+        }
+    }
+}
