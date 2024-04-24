d212237dfa64254405e2b2a83dee97724b6d4563
Filip Les <ketvok@gmail.com>
2024-04-24 17:42:50 +0200

    Start implementation of FleetBuilder

--- Model/FleetBuilder.cs
+++ Model/FleetBuilder.cs
@@ -11,12 +11,30 @@ namespace Vsite.Oom.Battleship.Model
         public FleetBuilder(int gridRows, int gridColumns, int[] shipLengths)
         {
             fleetGrid = new Grid(gridRows, gridColumns);
-            this.shipLengths = new List<int>(shipLengths);
+            this.shipLengths = new List<int>(shipLengths.OrderByDescending(length => length));
         }
 
 
         private readonly Grid fleetGrid;
 
         private readonly List<int> shipLengths; // "readonly ne znači da se njen sadržaj ne može mijenjati. Jedino ne možemo pozvati konstruktor s novom listom."
+
+        public Fleet CreateFleet()
+        {
+            var fleet = new Fleet();
+
+            for (int i = 0; i < shipLengths.Count; ++i)
+            {
+                var candidates = fleetGrid.GetAvailablePlacements(shipLengths[i]);
+                var selectedIndex = random.Next(candidates.Count());
+                var selected = candidates.ElementAt(selectedIndex);
+
+                fleet.CreateShip(selected);
+            }
+
+            return fleet;
+        }
+
+        private readonly Random random;  // "Da ne inicijalizira svaki puta daj generator slučajnih brojeva unutar metode staviti ćemo ga kao member klase."
     }
 }
