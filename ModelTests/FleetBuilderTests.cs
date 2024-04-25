eaabb7cf38f3b905bfcb79d48f3383706754cc48
Đuro Rukavina <djrukavina@vsite.hr>
2024-04-25 16:31:03 +0200

    Added additional FleetBuilder Tests

--- ModelTests/FleetBuilderTests.cs
+++ ModelTests/FleetBuilderTests.cs
@@ -19,7 +19,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             Assert.AreEqual(10, fleet.Ships.Count());
         }
         [TestMethod]
-        public void CreateFleetBuildsFleetWithSHipsOfLengthProvided()
+        public void CreateFleetBuildsFleetWithSHipsOfLength2Provided()
         {
             int rows = 10;
             int columns = 10;
@@ -30,7 +30,41 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.AreEqual(4, fleet.Ships.Count(s => s.Squares.Count() == 2));
         }
+        [TestMethod]
+        public void CreateFleetBuildsFleetWithSHipsOfLength3Provided()
+        {
+            int rows = 10;
+            int columns = 10;
+            int[] shipLengths = { 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
 
-        //napraviti za duljine 3, 4 i 5
+            var builder = new FleetBuilder(rows, columns, shipLengths);
+            var fleet = builder.CreateFleet();
+
+            Assert.AreEqual(3, fleet.Ships.Count(s => s.Squares.Count() == 3));
+        }
+        [TestMethod]
+        public void CreateFleetBuildsFleetWithSHipsOfLength4Provided()
+        {
+            int rows = 10;
+            int columns = 10;
+            int[] shipLengths = { 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+
+            var builder = new FleetBuilder(rows, columns, shipLengths);
+            var fleet = builder.CreateFleet();
+
+            Assert.AreEqual(2, fleet.Ships.Count(s => s.Squares.Count() == 4));
+        }
+        [TestMethod]
+        public void CreateFleetBuildsFleetWithSHipsOfLength5Provided()
+        {
+            int rows = 10;
+            int columns = 10;
+            int[] shipLengths = { 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
+
+            var builder = new FleetBuilder(rows, columns, shipLengths);
+            var fleet = builder.CreateFleet();
+
+            Assert.AreEqual(1, fleet.Ships.Count(s => s.Squares.Count() == 5));
+        }
     }
 }
