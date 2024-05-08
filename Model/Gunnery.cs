ea136e9297a1f3d454b7798ba48cc090f48c584b
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-08 21:04:16 +0200

    Start Gunnery implementation

---
+++ Model/Gunnery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random,
+        Surrounding,
+        Inline
+    }
+
+    public class Gunnery
+    {
+        private readonly Grid recordGrid;
+        private ITargetSelector targetSelector = new RandomTargetSelector();
+
+        public ShootingTactics ShootingTactics { get; private set; } = ShootingTactics.Random;
+
+        public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+        {
+            recordGrid = new Grid(rows, columns);
+        }
+
+        public SquareCoordinate Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessHit(HitResult hitResult)
+        {
+
+        }
+    }
+}
