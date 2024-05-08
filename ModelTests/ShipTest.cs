e12846275304cf8e6dff469842bea48829442699
Josip Grabušić <jograbus@vsite.hr>
2024-05-08 19:32:32 +0200

    Add Hit tests for ship

--- ModelTests/ShipTest.cs
+++ ModelTests/ShipTest.cs
@@ -14,5 +14,39 @@ namespace Vsite.Oom.Battleship.Model.Tests
 
             Assert.IsTrue(ship.Contains(1, 4));
         }
+
+        [TestMethod]
+        public void HitMethodReturnsMissedIfSquaereIsNotPartOfShip()
+        {
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+            var ship = new Ship(squares);
+
+            Assert.AreEqual(HitResult.Missed, ship.Hit(2, 4));
+        }
+
+        [TestMethod]
+        public void HitMethodReturnsMissedIfSquaereIsPartOfShipHit()
+        {
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+            var ship = new Ship(squares);
+
+            Assert.AreEqual(HitResult.Hit, ship.Hit(1, 3));
+            Assert.AreEqual(HitResult.Hit, ship.Hit(1, 5));
+
+            Assert.AreEqual(HitResult.Sunken, ship.Hit(1, 4));
+        }
+
+        [TestMethod]
+        public void HitMethodReturnsSunkenIfSquaereIsPartSquareIsHitAgain()
+        {
+            var squares = new List<Square> { new Square(1, 3), new Square(1, 4), new Square(1, 5) };
+            var ship = new Ship(squares);
+
+            Assert.AreEqual(HitResult.Hit, ship.Hit(1, 3));
+            Assert.AreEqual(HitResult.Hit, ship.Hit(1, 5));
+
+            Assert.AreEqual(HitResult.Sunken, ship.Hit(1, 4));
+            Assert.AreEqual(HitResult.Sunken, ship.Hit(1, 5));
+        }
     }
 }
