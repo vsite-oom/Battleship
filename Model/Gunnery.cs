fb69d3af2e961cd1a09c8c69371e1d70ee9c3495
Julijan Sribar <julijan.sribar@gmail.com>
2024-05-14 19:33:49 +0200

    Implement shooting tactics change

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -1,4 +1,6 @@
-﻿namespace Vsite.Oom.Battleship.Model
+﻿using System.Diagnostics;
+
+namespace Vsite.Oom.Battleship.Model
 {
     public enum ShootingTactics
     {
@@ -21,7 +23,44 @@
 
         public void ProcessHitResult(HitResult hitResult)
         {
+            switch (hitResult)
+            {
+                case HitResult.Missed:
+                    return;
+                case HitResult.Hit:
+                    switch (ShootingTactics)
+                    {
+                        case ShootingTactics.Random:
+                            ChangeTacticsToSurrounding();
+                            return;
+                        case ShootingTactics.Surrounding:
+                            ChangeTacticsToInline(); break;
+                        case ShootingTactics.Inline:
+                            return;
+                        default:
+                            Debug.Assert(false);
+                            return;
+                    }
+                    return;
+                case HitResult.Sunken:
+                    ChangeTacticsToRandom();
+                    return;
+            }
+        }
 
+        private void ChangeTacticsToRandom()
+        {
+            ShootingTactics = ShootingTactics.Random;
+        }
+
+        private void ChangeTacticsToSurrounding()
+        {
+            ShootingTactics = ShootingTactics.Surrounding;
+        }
+
+        private void ChangeTacticsToInline()
+        {
+            ShootingTactics = ShootingTactics.Inline;
         }
 
         public ShootingTactics ShootingTactics { get; private set; } = ShootingTactics.Random;
