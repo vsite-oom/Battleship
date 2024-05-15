099216fd508d275862c74846e44278bd479c3703
DrHrast <petar.huljek@gmail.com>
2024-05-15 20:48:48 +0200

    Add SquareState.

---
+++ Model/ShotsGrid.cs
@@ -0,0 +1,15 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public class ShotsGrid : Grid
+    {
+        public ShotsGrid(int rows, int columns) : base(rows, columns) 
+        { 
+        
+        }
+
+        protected override bool IsSqaureAvailable(int row, int column)
+        {
+            
+        }
+    }
+}
