470e22a2eb068d75106c0e2c42f4527db82cd6c6
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-15 20:34:19 +0200

    Refactor Grid class into hierarchy

--- Model/FleetGrid.cs
+++ Model/FleetGrid.cs
@@ -2,77 +2,28 @@
 
 namespace Vsite.Oom.Battleship.Model
 {
-    public class FleetGrid
+    public class FleetGrid : Grid
     {
-        public readonly int Rows;
-        public readonly int Columns;
-        private readonly Square?[,] squares;
-
-        public FleetGrid(int rows, int columns)
+        public FleetGrid(int rows, int columns) : base(rows, columns)
         {
-            Rows = rows;
-            Columns = columns;
-
-            squares = new Square[Rows, Columns];
-
-            for (int r = 0; r < Rows; r++)
-            {
-                for (int c = 0; c < Columns; c++)
-                {
-                    squares[r, c] = new Square(r, c);
-                }
-            }
         }
 
-        public IEnumerable<Square> Squares
+        public override IEnumerable<Square> Squares
         {
             get
             {
                 return squares.Cast<Square>().Where(s => s != null);
             }
         }
-
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
-        {
-
-            return GetAvailablePlacements(length, true).Concat(GetAvailablePlacements(length, false));
-        }
-
-        private IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length, bool isHorizontal)
-        {
-            List<IEnumerable<Square>> result = new();
-
-            for (int i = 0; i < (isHorizontal ? Rows : Columns); i++)
-            {
-                var queue = new LimitedQueue<Square>(length);
-
-                for (int j = 0; j < (isHorizontal ? Columns : Rows); j++)
-                {
-                    int rowId = isHorizontal ? i : j;
-                    int colId = isHorizontal ? j : i;
-
-                    if (squares[rowId, colId] != null)
-                    {
-                        queue.Enqueue(squares[rowId, colId]!);
-
-                        if (queue.Count >= length)
-                        {
-                            result.Add(queue.ToArray());
-                        }
-                    }
-                    else
-                    {
-                        queue.Clear();
-                    }
-                }
-            }
-
-            return result;
-        }
         
         public void EliminateSquare(int row, int column)
         {
             squares[row, column] = null;
+        }
+
+        protected override bool IsSquareAvailable(int row, int column)
+        {
+            return squares[row, column] != null;
         }
     }
 }
---
+++ Model/Grid.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Vsite.Oom.Battleship.Model
+{
+    public abstract class Grid
+    {
+        public readonly int Rows;
+        public readonly int Columns;
+        protected readonly Square?[,] squares;
+
+        protected Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows, Columns];
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public virtual IEnumerable<Square> Squares
+        {
+            get => squares.Cast<Square>();
+        }
+
+        protected abstract bool IsSquareAvailable(int row, int column);
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+
+            return GetAvailablePlacements(length, true).Concat(GetAvailablePlacements(length, false));
+        }
+        private IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length, bool isHorizontal)
+        {
+            List<IEnumerable<Square>> result = new();
+
+            for (int i = 0; i < (isHorizontal ? Rows : Columns); i++)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int j = 0; j < (isHorizontal ? Columns : Rows); j++)
+                {
+                    int rowId = isHorizontal ? i : j;
+                    int colId = isHorizontal ? j : i;
+
+                    if (IsSquareAvailable(rowId, colId))
+                    {
+                        queue.Enqueue(squares[rowId, colId]!);
+
+                        if (queue.Count >= length)
+                        {
+                            result.Add(queue.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
