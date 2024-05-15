cba695a5b5d359d2b56cbd8eb173c13e49848f14
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-15 20:48:39 +0200

    Add Square states

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,22 +1,40 @@
 ﻿namespace Vsite.Oom.Battleship.Model
 {
+    public enum SquareState
+    {
+        Intact,
+        Eliminated,
+        Missed,
+        Hit,
+        Sunken
+    }
+
     public class Square
     {
+        public readonly int Row;
+        public readonly int Column;
+        public bool IsHit => (int)SquareState >= (int)SquareState.Hit;
+        public SquareState SquareState { get; set; }
+
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            SquareState = SquareState.Intact;
         }
-
-        public readonly int Row;
-        public readonly int Column;
 
         public void Hit()
         {
-            hit = true;
+            SquareState = SquareState.Hit;
         }
 
-        private bool hit = false;
-        public bool IsHit => hit;
+        public void ChangeState(SquareState newState)
+        {
+            if ((int)newState > (int)SquareState)
+            {
+                SquareState = newState;
+            }
+        }
+
     }
 }
