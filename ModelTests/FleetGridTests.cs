7bc8ae72dab730eb74139745cb36b3011f156baa
DrHrast <petar.huljek@gmail.com>
2024-05-15 20:17:06 +0200

    Refactor Grid to FleetGrid.

--- ModelTests/FleetGridTests.cs
+++ ModelTests/FleetGridTests.cs
@@ -3,7 +3,7 @@
 namespace Vsite.Oom.Battleship.Model.Tests
 {
     [TestClass]
-    public class GridTests
+    public class FleetGridTests
     {
         [TestMethod]
         public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
@@ -11,7 +11,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int rows = 5;
             int cols = 10;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
 
             Assert.AreEqual(50, grid.Squares.Count());
         }
@@ -23,7 +23,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int cols = 5;
             int shipLength = 3;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
 
             Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
         }
@@ -35,7 +35,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int cols = 1;
             int shipLength = 2;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
 
             Assert.AreEqual(6, grid.GetAvailablePlacements(shipLength).Count());
         }
@@ -47,7 +47,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int cols = 5;
             int shipLength = 4;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
 
             Assert.AreEqual(20, grid.GetAvailablePlacements(shipLength).Count());
         }
@@ -59,7 +59,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int cols = 6;
             int shipLength = 2;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
             grid.EliminateSquare(0, 3);
 
             Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
@@ -72,7 +72,7 @@ namespace Vsite.Oom.Battleship.Model.Tests
             int cols = 1;
             int shipLength = 2;
 
-            var grid = new Grid(rows, cols);
+            var grid = new FleetGrid(rows, cols);
             grid.EliminateSquare(3, 0);
             grid.EliminateSquare(5, 0);
 
