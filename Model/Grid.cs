6a0ca380909604c274b5ba1679adbc1de4158584
DrHrast <petar.huljek@gmail.com>
2024-05-15 20:34:26 +0200

    Refactor Grid class into hierarchy.

---
+++ Model/Grid.cs
@@ -0,0 +1,92 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public abstract class Grid
+    {
+        protected Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows, Columns];
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public virtual IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>(); }
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            return GetVerticalAvailablePlacements(length).Concat(GetHorizontalAvailablePlacements(length));
+        }
+
+        protected abstract bool IsSqaureAvailable(int row, int column);
+
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (IsSqaureAvailable(r, c))
+                    {
+                        queue.Enqueue(squares[r, c]!);
+                        if (queue.Count() >= length)
+                        {
+                            result.Add(queue.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int r = 0; r < Rows; r++)
+                {
+                    if (IsSqaureAvailable(r, c))
+                    {
+                        queue.Enqueue(squares[r, c]!);
+                        if (queue.Count() >= length)
+                        {
+                            result.Add(queue.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+            return result;
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        protected readonly Square?[,] squares;
+    }
+}
