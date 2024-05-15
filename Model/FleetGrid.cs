25ca4a86b22bacf8f24f010ad1e0d657ce171ba7
Sonja Perković <soperkov@vsite.hr>
2024-05-15 20:34:25 +0200

    Refactor Grid class into hierarchy

--- Model/FleetGrid.cs
+++ Model/FleetGrid.cs
@@ -1,88 +1,16 @@
 ﻿
 namespace Vsite.Oom.Battleship.Model
 {
-    public class FleetGrid
+    public class FleetGrid : Grid
     {
-        public FleetGrid(int rows, int columns)
+        public FleetGrid(int rows, int columns) : base(rows, columns)
         {
-            Rows = rows;
-            Columns = columns;
-
-            squares = new Square[Rows, Columns];
-
-            for (int r = 0; r < Rows; r++)
-            {
-                for (int c = 0; c < Columns; c++)
-                {
-                    squares[r, c] = new Square(r, c);
-                }
-            }
+            
         }
 
-        public IEnumerable<Square> Squares
+        public override IEnumerable<Square> Squares
         {
             get { return squares.Cast<Square>().Where(s => s != null); }
-        }
-
-        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
-        {
-            return GetVerticalAvailablePlacements(length).Concat(GetHorizontalAvailablePlacements(length));
-        }
-
-        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
-        {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-
-            for (int r = 0; r < Rows; r++)
-            {
-                var queue = new LimitedQueue<Square>(length);
-
-                for (int c = 0;c < Columns; c++)
-                {
-                    if (squares[r, c] != null)
-                    {
-                        queue.Enqueue(squares[r, c]!);
-                        if (queue.Count() >= length)
-                        {
-                            result.Add(queue.ToArray());
-                        }
-                    }
-                    else
-                    {
-                        queue.Clear();
-                    }
-                }
-            }
-            return result;
-        }
-
-
-        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
-        {
-            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-
-            for (int c = 0; c < Columns; c++)
-            {
-                var queue = new LimitedQueue<Square>(length);
-
-                for (int r = 0; r < Rows; r++)
-                {
-                    if (squares[r, c] != null)
-                    {
-                        queue.Enqueue(squares[r, c]!);
-                        if (queue.Count() >= length)
-                        {
-                            result.Add(queue.ToArray());
-                        }
-
-                    }
-                    else
-                    {
-                        queue.Clear();
-                    }
-                }
-            }
-            return result;
         }
 
         public void EliminateSquare(int row, int column)
@@ -90,9 +18,9 @@ namespace Vsite.Oom.Battleship.Model
             squares[row, column] = null;
         }
 
-        public readonly int Rows;
-        public readonly int Columns;
-
-        private readonly Square?[,] squares;
+        protected override bool IsSquareAvailable(int row, int column)
+        {
+            return squares[row, column] != null;
+        }
     }
 }
---
+++ Model/Grid.cs
@@ -0,0 +1,93 @@
+﻿namespace Vsite.Oom.Battleship.Model
+{
+    public abstract class Grid
+    {
+        protected Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            squares = new Square[Rows, Columns];
+
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        protected readonly Square?[,] squares;
+
+        public virtual IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>(); }
+        }
+
+        public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+        {
+            return GetVerticalAvailablePlacements(length).Concat(GetHorizontalAvailablePlacements(length));
+        }
+
+        protected abstract bool IsSquareAvailable(int row, int column);
+
+        private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int r = 0; r < Rows; r++)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (IsSquareAvailable(r, c))
+                    {
+                        queue.Enqueue(squares[r, c]!);
+                        if (queue.Count() >= length)
+                        {
+                            result.Add(queue.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+            return result;
+        }
+
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)
+            {
+                var queue = new LimitedQueue<Square>(length);
+
+                for (int r = 0; r < Rows; r++)
+                {
+                    if (IsSquareAvailable(r, c))
+                    {
+                        queue.Enqueue(squares[r, c]!);
+                        if (queue.Count() >= length)
+                        {
+                            result.Add(queue.ToArray());
+                        }
+
+                    }
+                    else
+                    {
+                        queue.Clear();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
