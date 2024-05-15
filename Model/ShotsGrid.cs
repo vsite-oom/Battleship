c9fe7186c43998155667415d4092f95e86244588
Mihael Krpes <mikrpes@vsite.hr>
2024-05-15 20:49:22 +0200

    Add spongebob SquareStates

---
+++ Model/ShotsGrid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.OOM.Battleship.Model
+{
+    public class ShotsGrid : Grid
+    {
+        public ShotsGrid(int rows, int columns) : base(rows, columns)
+        {
+        }
+
+        protected override bool IsSquareAvailable(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -6,22 +6,37 @@ using System.Threading.Tasks;
 
 namespace Vsite.OOM.Battleship.Model
 {
+    public enum SquareState
+    {
+        Intact,
+        Eliminated,
+        Miss,
+        Hit,
+        Sunk
+    }
     public class Square
     {
         public readonly int Row;
         public readonly int Column;
+        public SquareState State {  get; private set; }
 
         public Square(int row, int column)
         {
             Row = row;
             Column = column;
+            State = SquareState.Intact;
         }
         
-        private bool hit = false;
-        public bool IsHit => hit;
+        public bool IsHit => (int)State>=(int)SquareState.Hit;
         public void Hit()
         {
-            hit = true;
+            State=SquareState.Hit;
+        }
+        public void changeState(SquareState newState)
+        {
+            if ((int)newState>(int)State) {
+            State=newState;
+            }
         }
     }
 }
