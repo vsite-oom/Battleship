c12df0524c0c6af54cc03f29d2efdbdbb671e691
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-05-15 20:48:39 +0200

    Add square states

---
+++ Model/ShotsGrid.cs
@@ -0,0 +1,10 @@
+﻿namespace Vsite.Oom.Battleship.Model;
+
+public class ShotsGrid(int rows, int columns) : Grid(rows, columns)
+{
+    public override bool IsSquareAvailable(int row, int column)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -1,21 +1,38 @@
 ﻿namespace Vsite.Oom.Battleship.Model;
 
+public enum SquareState
+{
+    Intact,
+    Eliminated,
+    Missed,
+    Hit,
+    Sunken
+}
 public class Square
 {
     public readonly int Column;
-
     public readonly int Row;
+    
+    public SquareState SquareState { get; private set; }
+    public bool IsHit => (int)SquareState >= (int)SquareState.Hit;
 
     public Square(int row, int column)
     {
         Row = row;
         Column = column;
+        this.SquareState = SquareState.Intact;
     }
-
-    public bool IsHit { get; private set; }
-
+    
     public void Hit()
     {
-        IsHit = true;
+        SquareState = SquareState.Hit;
+    }
+
+    public void ChangeState(SquareState newState)
+    {
+        if ((int)newState > (int)SquareState)
+        {
+            SquareState = newState;
+        }
     }
 }
