50dd7eb40bfc4b85f467825ea1bc36af7061fed8
Marko Pavić <mapavic@vsite.hr>
2024-05-15 20:34:14 +0200

    Refactor Grid classes

--- Model/FleetGrid.cs
+++ Model/FleetGrid.cs
@@ -7,74 +7,26 @@ using System.Threading.Tasks;
 
 namespace vste.oom.battleship.model
 {
-	public class FleetGrid
+	public class FleetGrid : Grid
 	{
-		public FleetGrid(int rows, int columns)
+		public FleetGrid(int rows, int columns) : base(rows, columns)
 		{
-			Rows= rows;
-			Columns= columns;
 
-			squares = new Square[Rows, Columns];
-
-			for(int r=0; r<Rows; r++)
-			{
-				for(int c=0; c<Columns; c++)
-				{
-					squares[r, c] = new Square(r, c);
-				}
-			}
 		}
-		
-		public IEnumerable<Square> Squares
+
+		public override IEnumerable<Square> Squares
 		{
 			get { return squares.Cast<Square>().Where(s => s != null); }
-		}
-
-		public readonly int Rows;
-		public readonly int Columns;
-
-		private readonly Square?[,] squares;
-
-		public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
-		{
-			return GetHorizontalAvailablePlacemets(length);
-		}
-
-		private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacemets(int length)
-		{
-			List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-
-			for (int r=0; r<Rows; r++)
-			{
-				int counter = 0;
-				for(int c=0; c < Columns; c++)
-				{
-					if (squares[r, c] != null)
-					{
-						++counter;
-						if (counter >= length)
-						{
-							List<Square> temp = new List<Square>();
-							for (int c1 = c - length+1; c1 <= c; ++c1)
-							{
-								temp.Add(squares[r, c1]!);
-							}
-							result.Add(temp);
-						}
-					}
-					else
-					{
-						counter = 0;
-					}
-				}
-				
-			}
-			return result;
 		}
 
 		public void EliminateSquare(int row, int column)
 		{
 			squares[row, column] = null;
+		}
+
+		protected override bool IsSquareAvailable(int row, int column)
+		{
+			return squares[row, column] != null;
 		}
 	}
 }
---
+++ Model/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vste.oom.battleship.model
+{
+	public abstract class Grid
+	{
+		protected Grid(int rows, int columns)
+		{
+			Rows = rows;
+			Columns = columns;
+
+			squares = new Square[Rows, Columns];
+
+			for (int r = 0; r < Rows; r++)
+			{
+				for (int c = 0; c < Columns; c++)
+				{
+					squares[r, c] = new Square(r, c);
+				}
+			}
+		}
+
+		public readonly int Rows;
+		public readonly int Columns;
+
+		protected readonly Square?[,] squares;
+
+		public virtual IEnumerable<Square> Squares
+		{
+			get { return squares.Cast<Square>(); }
+		}
+
+		public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+		{
+			return GetHorizontalAvailablePlacemets(length);
+		}
+
+		protected abstract bool IsSquareAvailable(int row, int column);
+
+		private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacemets(int length)
+		{
+			List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+			for (int r = 0; r < Rows; r++)
+			{
+				int counter = 0;
+				for (int c = 0; c < Columns; c++)
+				{
+					if (IsSquareAvailable(r,c))
+					{
+						++counter;
+						if (counter >= length)
+						{
+							List<Square> temp = new List<Square>();
+							for (int c1 = c - length + 1; c1 <= c; ++c1)
+							{
+								temp.Add(squares[r, c1]!);
+							}
+							result.Add(temp);
+						}
+					}
+					else
+					{
+						counter = 0;
+					}
+				}
+
+			}
+			return result;
+		}
+
+	}
+}
