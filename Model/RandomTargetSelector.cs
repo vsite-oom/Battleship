ba14f7fd5841fb8659ea538435fa93b71c342702
DrHrast <petar.huljek@gmail.com>
2024-05-22 20:09:10 +0200

    Implemented new random target selector.

--- Model/RandomTargetSelector.cs
+++ Model/RandomTargetSelector.cs
@@ -14,10 +14,17 @@ namespace Vsite.Oom.Battleship.Model
             var placements = grid.GetAvailablePlacements(shipLength);
             var candidates = placements.SelectMany(s => s);
 
-            //TODO: Implement square target by count of how many
-
             var selectedIndex = random.Next(candidates.Count());
             return candidates.ElementAt(selectedIndex);
+
+            //TODO: Implement square target by count of how many
+            var availablePlacements = grid.GetAvailablePlacements(shipLength);
+            var availableSquares = availablePlacements.SelectMany(s => s);
+            var grouped = availableSquares.GroupBy(x => x);
+            var sorted = grouped.OrderByDescending(x => x.Count());
+            var first = sorted.Select(grp => grp.Key).First();
+
+            return first;
         }
 
         private readonly ShotsGrid grid;
