0f5082b2d42b954cc0ac985d9b1e084380551dee
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-05-22 21:13:14 +0200

    dodavanje test SurroundingTargetSelector

--- ModelTests/SurroundingTargetSelector.cs
+++ ModelTests/SurroundingTargetSelector.cs
@@ -1,19 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Linq;
 
 namespace Vsite.Oom.Battleship.Model.Tests
 {
+
+
+
     [TestClass]
-    public class SurroundingTargetSelector
+    public class SurroundingTargetSelectorTests
     {
-        [TestMethod]
-        public void MyTestMethod()
+
+        private IEnumerable<Square> CreateCandidates(ShotsGrid grid, IEnumerable<SquareCoordinate> coord)
         {
-            var grid = new ShotsGrid(10 , 10);
-            var squareHit= grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
+            List<Square> result = new List<Square>();
+            foreach (var c in coord)
+            {
+                var square = grid.Squares.FirstOrDefault(s => s.Row == c.Row && s.Column == c.Column);
+                result.Add(square);
+
+            }
+            return result;
+        }
+
+        [TestMethod]
+        public void NextReturnesOneOfSquaresArroundSquare3x4()
+        {
+            var grid = new ShotsGrid(10, 10);
+            var squareHit = grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
             squareHit.ChangeState(SquareState.Hit);
             int shipLength = 5;
             var selector = new SurroundingTargetSelector(grid, squareHit, shipLength);
+            var target = selector.Next();
+
+            var candidates = CreateCandidates(grid, [new(2, 4), new(3, 5), new(4, 4), new(3, 3)]);
+            Assert.IsTrue(candidates.Contains(target));
         }
     }
 }
