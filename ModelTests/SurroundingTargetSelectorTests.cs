3d2a7ae885565bbbf294e2f0d5dccf6f00f7115b
Hrvoje Janđel <hrjandje@vsite.hr>
2024-05-22 21:15:48 +0200

    Create test for target selector

---
+++ ModelTests/SurroundingTargetSelectorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+
+    public class SurroundingTargetSelectorTests
+
+    {
+        private IEnumerable<Square> CreateCandidates(ShotsGrid grid, IEnumerable<SquareCoordinate> coord)
+        {
+            List<Square> result = new List<Square>();
+            foreach (var c in coord)
+            {
+                var square = grid.Squares.FirstOrDefault(s => s.Row == c.Row && s.Column == c.Column);
+
+                {
+                    result.Add(square);
+                }
+            }
+                return result;
+        }
+
+            [TestMethod]
+        public void NextReturnsOneOfSquaresAroundSquare3x4()
+        {
+            
+
+            }
+            var grid = new ShotsGrid(10, 10);
+            var squareHit = grid.Squares.FirstOrDefault(s=>s.Row == 3 && s.Column == 4);
+            
+            squareHit!.ChangeState(SquareState.Hit);
+            int shipLength = 5;
+
+            var selector = new SurroundingTargetSelector(grid, squareHit, shipLength);
+            var target = selector.Next();
+
+            var candidates = CreateCandidates(grid, [new(2,4), new(4, 4), new(3, 3), new(3, 5)]); 
+            Assert.IsTrue(candidates.Contains(target));
+
+        }
+    }
+}
