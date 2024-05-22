ae389d9daf4a3fd5b0d1e4df7bdde892c2b01dc8
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-05-22 21:04:02 +0200

    izmjena SurroundingTargetSelector

--- Model/SurroundingTargetSelector.cs
+++ Model/SurroundingTargetSelector.cs
@@ -3,8 +3,40 @@ namespace Vsite.Oom.Battleship.Model
 {
     public class SurroundingTargetSelector : ITargetSelector
     {
+        public SurroundingTargetSelector(ShotsGrid grid, Square firstHit, int shipLength) {
+        this.grid = grid;
+        this.firstHit = firstHit;
+        this.shipLength = shipLength;
+    }
+
+        private readonly ShotsGrid grid;
+        private readonly Square firstHit;
+        private readonly int shipLength;
         public Square Next()
         {
+            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
+            var up = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Upwards);
+            if (up.Count() > 0)
+            {
+                squares.Add(up);
+            }
+            var right = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Rightwards);
+            if (right.Count() > 0)
+            {
+                squares.Add(right);
+            }
+            var Down = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Downards);
+            if (Down.Count() > 0)
+            {
+                squares.Add(Down);
+            }
+
+            var Left = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Leftwards);
+            if (Left.Count() > 0)
+            {
+            squares.Add(Left); 
+            }
+
             throw new NotImplementedException();
         }
     }
---
+++ ModelTests/SurroundingTargetSelector.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class SurroundingTargetSelector
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var grid = new ShotsGrid(10 , 10);
+            var squareHit= grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
+            squareHit.ChangeState(SquareState.Hit);
+            int shipLength = 5;
+            var selector = new SurroundingTargetSelector(grid, squareHit, shipLength);
+        }
+    }
+}
