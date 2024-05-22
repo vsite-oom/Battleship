ff1177afce2c70ba8fbe7e542b2e80b270fe152f
Josip Grabušić <jograbus@vsite.hr>
2024-05-22 20:42:14 +0200

    Add tests for ShootsGrid

--- Model/SootsGrid.cs
+++ Model/SootsGrid.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Vsite.Oom.Battleship.Model
 {
+    public enum Direction
+    {
+        Upwards,
+        Rightwards,
+        Downwards,
+        Leftwards
+    }
+
     public class ShotsGrid : Grid
     {
         public ShotsGrid(int rows, int columns) : base(rows, columns) { }
@@ -23,6 +31,11 @@ namespace Vsite.Oom.Battleship.Model
         public void ChangeSquareState(int row, int column, SquareState newState)
         {
             squares[row, column]!.ChangeState(newState);
+        }
+
+        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction upwards)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ ModelTests/ShootsGridTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.Battleship.Model.Tests
+{
+    [TestClass]
+    public class ShootsGridTests
+    {
+        [TestMethod]
+        public void GetSquaresInDirectionReturns3SquaresAboveSquare3x3()
+        {
+            var grid = new ShotsGrid(10, 10);
+            int row = 3;
+            int column = 3;
+            var squares = grid.GetSquaresInDirection(row, column, Direction.Upwards);
+            Assert.AreEqual(3, squares.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresInDirectionReturns4SquaresRightSquare3x5 ()
+        {
+            var grid = new ShotsGrid(10, 10);
+            int row = 3;
+            int column = 5;
+            var squares = grid.GetSquaresInDirection(row, column, Direction.Rightwards);
+            Assert.AreEqual(4, squares.Count());
+        }
+
+
+        [TestMethod]
+        public void GetSquaresInDirectionReturns2SquaresBelowSquare7x5()
+        {
+            var grid = new ShotsGrid(10, 10);
+            int row = 7;
+            int column = 5;
+            var squares = grid.GetSquaresInDirection(row, column, Direction.Downwards);
+            Assert.AreEqual(2, squares.Count());
+        }
+
+        [TestMethod]
+        public void GetSquaresInDirectionReturns1SquaresLeftSquare7x1 ()
+        {
+            var grid = new ShotsGrid(10, 10);
+            int row = 7;
+            int column = 1;
+            var squares = grid.GetSquaresInDirection(row, column, Direction.Leftwards);
+            Assert.AreEqual(1, squares.Count());
+        }
+
+    }
+}
