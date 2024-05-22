dec511523d9f6bd0a67935de3a6ced2f4ac64f0c
Josip Grabušić <jograbus@vsite.hr>
2024-05-22 20:17:23 +0200

    Fix GunneryTests for tactics change

--- ModelTests/GunneryTests.cs
+++ ModelTests/GunneryTests.cs
@@ -13,13 +13,16 @@ namespace Vsite.Oom.Battleship.Model.Tests
             var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3 });
             Assert.AreEqual(ShootingTactics.Random, gunnery.ShootingTactics);
         }
-        //[TestMethod]
-        //public void ShootingTacticsRemainsRandomIfHitResultIsMissed()
-        //{
-        //    var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3});
-        //    gunnery.ProcessHitResult(HitResult.Missed);
-        //    Assert.AreEqual(ShootingTactics.Random, gunnery.ShootingTactics);
-        //}
+        [TestMethod]
+        public void ShootingTacticsRemainsRandomIfHitResultIsMissed()
+        {
+            var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3 });
+            gunnery.Next();
+            gunnery.ProcessHitResult(HitResult.Missed);
+            Assert.AreEqual(ShootingTactics.Random, gunnery.ShootingTactics);
+        }
+
+
         [TestMethod]
         public void ShootingTacticsChangesToSurroundingAfterFirstSquareIsHit()
         {
@@ -27,15 +30,17 @@ namespace Vsite.Oom.Battleship.Model.Tests
             gunnery.ProcessHitResult(HitResult.Hit);
             Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
         }
-        //[TestMethod]
-        //public void ShootingTacticsRemainsSurroundingIfNextSquareIsMissed()
-        //{
-        //    var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3});
-        //    gunnery.ProcessHitResult(HitResult.Hit);
-        //    Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
-        //    gunnery.ProcessHitResult(HitResult.Missed);
-        //    Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
-        //}
+        [TestMethod]
+        public void ShootingTacticsRemainsSurroundingIfNextSquareIsMissed()
+        {
+            var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3 });
+            gunnery.Next();
+            gunnery.ProcessHitResult(HitResult.Hit);
+            Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
+            //gunnery.Next();
+            gunnery.ProcessHitResult(HitResult.Missed);
+            Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
+        }
         [TestMethod]
         public void ShootingTacticsChangesFromSurroundingToInlineAfterSecondSquareIsHit()
         {
@@ -57,17 +62,18 @@ namespace Vsite.Oom.Battleship.Model.Tests
             Assert.AreEqual(ShootingTactics.Inline, gunnery.ShootingTactics);
         }
 
-        //[TestMethod]
-        //public void ShootingTacticsRemainsInlineAfterThirdSquareIsMissed()
-        //{
-        //    var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3});
-        //    gunnery.ProcessHitResult(HitResult.Hit);
-        //    Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
-        //    gunnery.ProcessHitResult(HitResult.Hit);
-        //    Assert.AreEqual(ShootingTactics.Inline, gunnery.ShootingTactics);
-        //    gunnery.ProcessHitResult(HitResult.Missed);
-        //    Assert.AreEqual(ShootingTactics.Inline, gunnery.ShootingTactics);
-        //}
+        [TestMethod]
+        public void ShootingTacticsRemainsInlineAfterThirdSquareIsMissed()
+        {
+            var gunnery = new Gunnery(10, 10, new List<int> { 1, 2, 3 });
+            gunnery.Next();
+            gunnery.ProcessHitResult(HitResult.Hit);
+            Assert.AreEqual(ShootingTactics.Surrounding, gunnery.ShootingTactics);
+            gunnery.ProcessHitResult(HitResult.Hit);
+            Assert.AreEqual(ShootingTactics.Inline, gunnery.ShootingTactics);
+            gunnery.ProcessHitResult(HitResult.Missed);
+            Assert.AreEqual(ShootingTactics.Inline, gunnery.ShootingTactics);
+        }
 
         [TestMethod]
         public void ShootingTacticsChangesToRandomAfterShipIsSunken()
