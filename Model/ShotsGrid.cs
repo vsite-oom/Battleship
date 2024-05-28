de909ab340fff4cd6c45c9adab92a83f06f916c5
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-28 19:43:31 +0200

    Implement GetSquaresInDirection method

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -16,9 +16,13 @@ namespace Vsite.Oom.Battleship.Model
 
     public class ShotsGrid : Grid
     {
+        private readonly int _rows;
+        private readonly int _columns;
+
         public ShotsGrid(int rows, int columns) : base(rows, columns)
         {
-
+            _rows = rows;
+            _columns = columns;
         }
 
         protected override bool IsSquareAvailable(int row, int column)
@@ -31,10 +35,36 @@ namespace Vsite.Oom.Battleship.Model
             squares[row, column]!.ChangeState(newState);
         }
 
-        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction upwards)
+        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction dir)
         {
-            // todo: implement this method
-            throw new NotImplementedException();
+            List<Square> squares = new();
+
+            int tempRow = row;
+            int tempCol = column;
+
+            int rowDir = dir switch
+            {
+                Direction.Upwards => -1,
+                Direction.Downwards => 1,
+                _ => 0
+            };
+
+            int colDir = dir switch
+            {
+                Direction.Rightwards => 1,
+                Direction.Leftwards => -1,
+                _ => 0
+            };
+
+            while (tempRow > 0 && tempRow < _rows - 1 && tempCol > 0 && tempCol < _columns - 1)
+            {
+                tempRow += rowDir;
+                tempCol += colDir;
+                squares.Add(new Square(tempRow, tempCol));
+            }
+
+            return squares;
+
         }
     }
 }
