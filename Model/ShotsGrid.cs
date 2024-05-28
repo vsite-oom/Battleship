82968c7c8730bbe6d431df87818bf040604b168c
Julijan Šribar <julijan.sribar@gmail.com>
2024-05-28 22:49:18 +0200

    Implement GetSquaresInDirection method in ShotsGrid class

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -12,7 +12,7 @@ namespace Vsite.Oom.Battleship.Model
     public class ShotsGrid : Grid
     {
         public ShotsGrid(int rows, int columns) : base(rows, columns)
-        { 
+        {
         }
 
         protected override bool IsSquareAvailable(int row, int column)
@@ -25,9 +25,41 @@ namespace Vsite.Oom.Battleship.Model
             squares[row, column]!.ChangeState(newState);
         }
 
-        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction upwards)
+        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction direction)
         {
-            throw new NotImplementedException();
+            var result = new List<Square>();
+
+            int deltaRow = 0;
+            int deltaColumn = 0;
+            int limit = 0;
+            switch (direction)
+            {
+                case Direction.Upwards:
+                    --row;
+                    deltaRow = -1;
+                    limit = -1;
+                    break;
+                case Direction.Rightwards:
+                    ++column;
+                    deltaColumn = +1;
+                    limit = Columns;
+                    break;
+                case Direction.Downwards:
+                    ++row;
+                    deltaRow = +1;
+                    limit = Rows;
+                    break;
+                case Direction.Leftwards:
+                    --column;
+                    deltaColumn = -1;
+                    limit = -1;
+                    break;
+            }
+            for (int r = row, c = column; r != limit && c != limit && IsSquareAvailable(r, c); r += deltaRow, c += deltaColumn)
+            {
+                result.Add(squares[r, c]!);
+            }
+            return result;
         }
     }
 }
