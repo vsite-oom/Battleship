74b93f0c48c6043e9f272f06b767df28ea2d2f0c
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-05-28 19:50:21 +0200

    Implement Next method

--- Model/SurroundingTargetSelector.cs
+++ Model/SurroundingTargetSelector.cs
@@ -23,34 +23,17 @@ namespace Vsite.Oom.Battleship.Model
         {
             List<IEnumerable<Square>> squares = [];
 
-
-            var up = _grid.GetSquaresInDirection(_firstHit.Row, _firstHit.Column, Direction.Upwards);
-            if (up.Count() > 0)
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
             {
-                squares.Add(up);
+                var squaresInDirection = _grid.GetSquaresInDirection(_firstHit.Row, _firstHit.Column, direction);
+                if (squaresInDirection.Count() > 0)
+                {
+                    squares.Add(squaresInDirection);
+                }
             }
 
-            var right = _grid.GetSquaresInDirection(_firstHit.Row, _firstHit.Column, Direction.Rightwards);
-            if (right.Count() > 0)
-            {
-                squares.Add(right);
-            }
-
-            var down = _grid.GetSquaresInDirection(_firstHit.Row, _firstHit.Column, Direction.Downwards);
-            if (down.Count() > 0)
-            {
-                squares.Add(down);
-            }
-
-            var left = _grid.GetSquaresInDirection(_firstHit.Row, _firstHit.Column, Direction.Leftwards);
-            if (left.Count() > 0)
-            {
-                squares.Add(left);
-            }
-
-            // todo: implement the logic to select the next target
-
-            throw new NotImplementedException();
+            var longest = squares.OrderByDescending(s => s.Count()).First();
+            return longest.First();
         }
     }
 }
