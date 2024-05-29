fb97c869c823015ec49d132687ac1dc3fc88d5ab
Kristian Mesic <kristian.mesic303@gmail.com>
2024-05-29 09:39:10 +0200

    - implement GetSquaresInDirection method

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -27,9 +27,35 @@ public class ShotsGrid : Grid
         _squares[row, column]!.ChangeState(newState);
     }
 
-    public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction newDirection)
+    public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction direction)
     {
-        //Todo: GetSquaresInDirection <,>,|;
-        throw new NotImplementedException();
+        var result = new List<Square>();
+
+        int tempRow = row;
+        int tempCol = column;
+
+        int rowDirection = direction switch
+        {
+            Direction.Upwards => -1,
+            Direction.Downwards => 1,
+            _ => 0
+        };
+
+        int colDirection = direction switch
+        {
+            Direction.Right => 1,
+            Direction.Left => -1,
+            _ => 0
+        };
+
+        while (tempRow > 0 && tempRow < this.Rows - 1 && tempCol > 0 && tempCol < this.Columns - 1)
+        {
+            tempRow += rowDirection;
+            tempCol += colDirection;
+            result.Add(new Square(tempRow, tempCol));
+        }
+
+        return result;
+
     }
 }
