0e1d4648bb40cb68852a58f494bb243c518e6841
Sonja Perković <soperkov@vsite.hr>
2024-05-29 20:23:55 +0200

    Extend SurroundingTargetSelectorTests

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Vsite.Oom.Battleship.Model
+﻿namespace Vsite.Oom.Battleship.Model
 {
     public enum Direction
     {
@@ -8,9 +7,12 @@ namespace Vsite.Oom.Battleship.Model
         Downwards,
         Leftwards
     }
+
     public class ShotsGrid : Grid
     {
-        public ShotsGrid(int rows, int columns) : base(rows, columns) { }
+        public ShotsGrid(int rows, int columns) : base(rows, columns)
+        {
+        }
 
         protected override bool IsSquareAvailable(int row, int column)
         {
@@ -22,9 +24,41 @@ namespace Vsite.Oom.Battleship.Model
             squares[row, column]!.ChangeState(newState);
         }
 
-        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction upwards)
+        public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction direction)
         {
-            throw new NotImplementedException();
+            var result = new List<Square>();
+
+            int deltaRow = 0;
+            int deltaColumn = 0;
+            int limit = 0;
+            switch (direction)
+            {
+                case Direction.Upwards:
+                    --row;
+                    deltaRow = -1;
+                    limit = -1;
+                    break;
+                case Direction.Rightwards:
+                    ++column;
+                    deltaColumn = +1;
+                    limit = Columns;
+                    break;
+                case Direction.Downwards:
+                    ++row;
+                    deltaRow = +1;
+                    limit = Rows;
+                    break;
+                case Direction.Leftwards:
+                    --column;
+                    deltaColumn = -1;
+                    limit = -1;
+                    break;
+            }
+            for (int r = row, c = column; r != limit && c != limit && IsSquareAvailable(r, c); r += deltaRow, c += deltaColumn)
+            {
+                result.Add(squares[r, c]!);
+            }
+            return result;
         }
     }
 }
--- Model/SurroundingTargetSelector.cs
+++ Model/SurroundingTargetSelector.cs
@@ -8,39 +8,27 @@
             this.firstHit = firstHit;
             this.shipLength = shipLength;
         }
-        public Square Next()
-        {
-            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
-            var up = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Upwards);
-            if (up.Count() > 0)
-            {
-                squares.Add(up);
-            }
-
-            var right = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Rightwards);
-            if (right.Count() > 0)
-            {
-                squares.Add(right);
-            }
-
-            var down = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Downwards);
-            if (down.Count() > 0)
-            {
-                squares.Add(down);
-            }
-
-            var left = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, Direction.Leftwards);
-            if (left.Count() > 0)
-            {
-                squares.Add(left);
-            }
-
-            throw new NotImplementedException();
-
-        }
 
         private readonly ShotsGrid grid;
         private readonly Square firstHit;
         private readonly int shipLength;
+        private readonly Random random = new Random();
+
+        public Square Next()
+        {
+            List<IEnumerable<Square>> squares = new List<IEnumerable<Square>>();
+
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var inDirection = grid.GetSquaresInDirection(firstHit.Row, firstHit.Column, direction);
+                if (inDirection.Any())
+                {
+                    squares.Add(inDirection);
+                }
+            }
+
+            int index = random.Next(squares.Count);
+            return squares[index].First();
+        }
     }
 }
