2e2a35bf1f0a2f80ea0bee795431f7b3841ba5fe
Mihael Krpes <mikrpes@vsite.hr>
2024-05-29 20:24:16 +0200

    Extend SurrondingTargetSelectorTests

--- ModelTests/SurrondingTargetSelectorTests.cs
+++ ModelTests/SurrondingTargetSelectorTests.cs
@@ -7,17 +7,6 @@ namespace Vsite.OOM.Battleship.Model.Tests
     [TestClass]
     public class SurrondingTargetSelectorTests
     {
-        private IEnumerable<Square> CreateCanditates(ShotsGrid grid, IEnumerable<SquareCoordinate> coords)
-        {
-            List<Square> result = new List<Square>();
-            foreach (var c in coords)
-            {
-                var square = grid.Squares.FirstOrDefault(s => s.Row == c.row && s.Column == c.col);
-                result.Add(square!);
-            }
-            return result;
-        }
-
         [TestMethod]
         public void NextReturnsOneOfSquaresAroundSquare3x4()
         {
@@ -27,8 +16,56 @@ namespace Vsite.OOM.Battleship.Model.Tests
             int shipLen = 5;
             var selector =new SurrondingTargetSelector(grid, squareHit, shipLen);
             var target = selector.Next();
-            var candidates = CreateCanditates(grid, [new(2, 4), new(3, 5), new(4, 4), new(3, 3)]);
-            Assert.IsTrue(candidates.Contains(target));
+            var candidates = new SquareCoordinate[]{ new(2, 4), new(3, 5), new(4, 4), new(3, 3)};
+            Assert.IsTrue(candidates.Contains(new SquareCoordinate(target.Row,target.Column)));
+        }
+        [TestMethod]
+        public void NextReturnsOneOfThreeSquaresAroundSquare3x4AfterSquareIsMisseed()
+        {
+            var grid = new ShotsGrid(10, 10);
+            var squareHit = grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
+            squareHit!.changeState(SquareState.Hit);
+            int shipLen = 5;
+            var selector = new SurrondingTargetSelector(grid, squareHit, shipLen);
+            grid.ChangeSquareState(3, 5, SquareState.Miss);
+            var target = selector.Next();
+            var candidates = new SquareCoordinate[] { new(2, 4), new(4, 4), new(3, 3) };
+            Assert.IsTrue(candidates.Contains(new SquareCoordinate(target.Row, target.Column)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(3, 5)));
+        }
+        [TestMethod]
+        public void NextReturnsOneOfTwoSquaresAroundSquare3x4AfterSquaresWereMisseed()
+        {
+            var grid = new ShotsGrid(10, 10);
+            var squareHit = grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
+            squareHit!.changeState(SquareState.Hit);
+            int shipLen = 5;
+            var selector = new SurrondingTargetSelector(grid, squareHit, shipLen);
+            grid.ChangeSquareState(3, 5, SquareState.Miss);
+            grid.ChangeSquareState(3, 3, SquareState.Miss);
+            var target = selector.Next();
+            var candidates = new SquareCoordinate[] { new(2, 4), new(4, 4) };
+            Assert.IsTrue(candidates.Contains(new SquareCoordinate(target.Row, target.Column)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(3, 5)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(3, 3)));
+        }
+        [TestMethod]
+        public void NextReturnsOnlySquareAroundSquare3x4AfterOtherSquaresWereMisseed()
+        {
+            var grid = new ShotsGrid(10, 10);
+            var squareHit = grid.Squares.FirstOrDefault(s => s.Row == 3 && s.Column == 4);
+            squareHit!.changeState(SquareState.Hit);
+            int shipLen = 5;
+            var selector = new SurrondingTargetSelector(grid, squareHit, shipLen);
+            grid.ChangeSquareState(3, 5, SquareState.Miss);
+            grid.ChangeSquareState(3, 3, SquareState.Miss);
+            grid.ChangeSquareState(2, 4, SquareState.Miss);
+            var target = selector.Next();
+            var candidates = new SquareCoordinate[] {new(4, 4) };
+            Assert.IsTrue(candidates.Contains(new SquareCoordinate(target.Row, target.Column)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(3, 5)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(3, 3)));
+            Assert.IsFalse(candidates.Contains(new SquareCoordinate(2, 4)));
         }
     }
 }
