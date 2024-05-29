0d2d170bfbd24fd3e0d6b0ff219bb023e3dcc7b6
Kristian Bolarić <kristian.bolaric@gmail.com>
2024-05-29 21:05:16 +0200

    Commit

--- Model/InlineTargetSelector.cs
+++ Model/InlineTargetSelector.cs
@@ -9,9 +9,50 @@ namespace vsite.oom.battleship.model
 {
     public class InlineTargetSelector : ITargetSelector
     {
+        public InlineTargetSelector(ShotsGrid grid, IEnumerable<Square> squaresHit, int shipLength)
+        {
+            this.grid = grid;
+            this.squaresHit = squaresHit;
+            this.shipLength = shipLength;
+        }
+        private readonly ShotsGrid grid;
+        private readonly IEnumerable<Square> squaresHit;
+        private readonly int shipLength;
+        private readonly Random random = new Random();
         public Square Next()
         {
-            throw new NotImplementedException();
+            var sorted = squaresHit.OrderBy(sq => sq.Row + sq.Column);
+            var directionCandidates = new List<IEnumerable<Square>>();
+            //horizontal
+            if(sorted.First().Row == sorted.Last().Row)
+            {
+                var left = grid.GetSquaresDirection(sorted.First().Row, sorted.First().Column, Direction.Leftwards);
+                if (left.Any())
+                {
+                    directionCandidates.Add(left);
+                }       
+                var right = grid.GetSquaresDirection(sorted.Last().Row, sorted.Last().Column, Direction.Rightwards);
+                if (right.Any())
+                {
+                    directionCandidates.Add(right);
+                }
+            }
+            //vertical
+            else
+            {
+
+            }
+            var groupedByLength = directionCandidates.GroupBy(l => l.Count());
+            var sortedByLength = groupedByLength.OrderByDescending(g => g.Key);
+            var longestDirections = sortedByLength.First();
+            var candidates = longestDirections.Count();
+
+            if(candidates == 1)
+            {
+                return longestDirections.First().First();
+            }
+            int selectedIndex = random.Next(candidates);
+            return longestDirections.ElementAt(selectedIndex).First();
         }
     }
 }
