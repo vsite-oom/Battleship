59cdaeb04e56a4b113a31ce060393561b08e5340
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-03 12:52:53 +0200

    Create NewGameView

---
+++ GUI/GUI/ViewModels/NewGameViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Vsite.Oom.Battleship.GUI.Models;
+
+namespace Vsite.Oom.Battleship.GUI.ViewModels;
+
+public partial class NewGameViewModel : ViewModelBase
+{
+    [ObservableProperty] private Game _game;
+
+    public ObservableCollection<DisplayDifficulty> Difficulties { get; } =
+    [
+        new DisplayDifficulty { Difficulty = GameDifficulty.Easy },
+        new DisplayDifficulty { Difficulty = GameDifficulty.Normal },
+        new DisplayDifficulty { Difficulty = GameDifficulty.Hard }
+    ];
+
+    public NewGameViewModel() : this(null!)
+    {
+    }
+    public NewGameViewModel(Game game)
+    {
+        _game = game;
+        
+        Difficulties.First(x => x.Difficulty == Game.Difficulty).IsSelected = true;
+    }
+
+    [RelayCommand]
+    private void SetDifficulty(GameDifficulty difficulty)
+    {
+        Game.Difficulty = difficulty;
+    }
+}
---
+++ GUI/GUI/Views/NewGameView.axaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Vsite.Oom.Battleship.GUI.Views;
+
+public partial class NewGameView : UserControl
+{
+    public Thickness ControlPadding { get; set; } = new Thickness(40);
+    public double LargeFont { get; set; } = 36;
+    public double MediumFont { get; set; } = 26;
+    public double SmallFont { get; set; } = 16;
+    
+    public double ButtonWidth { get; set; } = 150;
+
+    public NewGameView()
+    {
+        if (OperatingSystem.IsAndroid() || IsVertical)
+        {
+            ControlPadding = new Thickness(10);
+            ButtonWidth = 100;
+            
+            LargeFont = 24;
+            MediumFont = 18;
+            SmallFont = 12;
+        }
+        
+        InitializeComponent();
+    }
+
+    public static readonly StyledProperty<ICommand> StartGameCommandProperty =
+        AvaloniaProperty.Register<NewGameView, ICommand>(
+            nameof(StartGameCommand));
+
+    public ICommand StartGameCommand
+    {
+        get => GetValue(StartGameCommandProperty);
+        set => SetValue(StartGameCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsVerticalProperty = AvaloniaProperty.Register<NewGameView, bool>(
+        nameof(IsVertical));
+
+    public bool IsVertical
+    {
+        get => GetValue(IsVerticalProperty);
+        set => SetValue(IsVerticalProperty, value);
+    }
+}
