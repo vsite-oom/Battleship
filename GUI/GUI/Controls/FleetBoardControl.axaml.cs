06a222253a36ecc9fbde792cc323aafb7c9eb1f6
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-03 12:47:41 +0200

    Create controls for displaying game boards

---
+++ GUI/GUI/Controls/FleetBoardControl.axaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Vsite.Oom.Battleship.GUI.Models;
+
+namespace Vsite.Oom.Battleship.GUI.Controls;
+
+public partial class FleetBoardControl : UserControl
+{
+    public FleetBoardControl()
+    {
+        InitializeComponent();
+    }
+    
+    public static readonly StyledProperty<ObservableCollection<ObservableCollection<DisplaySquare>>> SourceProperty = 
+        AvaloniaProperty.Register<FleetBoardControl, ObservableCollection<ObservableCollection<DisplaySquare>>>(
+            nameof(Source));
+
+    public ObservableCollection<ObservableCollection<DisplaySquare>> Source
+    {
+        get => GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public static readonly StyledProperty<double> CellSizeProperty = AvaloniaProperty.Register<FleetBoardControl, double>(
+        nameof(CellSize), defaultValue: 30);
+
+    public double CellSize
+    {
+        get => GetValue(CellSizeProperty);
+        set => SetValue(CellSizeProperty, value);
+    }
+}
---
+++ GUI/GUI/Controls/ShotsBoardControl.axaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Vsite.Oom.Battleship.GUI.Models;
+
+namespace Vsite.Oom.Battleship.GUI.Controls;
+
+public partial class ShotsBoardControl : UserControl
+{
+    public ShotsBoardControl()
+    {
+        InitializeComponent();
+    }
+    
+    public static readonly StyledProperty<ObservableCollection<ObservableCollection<DisplaySquare>>> SourceProperty =
+        AvaloniaProperty.Register<FleetBoardControl, ObservableCollection<ObservableCollection<DisplaySquare>>>(
+            nameof(Source));
+
+    
+    public static readonly StyledProperty<ICommand> ShootCommandProperty =
+        AvaloniaProperty.Register<ShotsBoardControl, ICommand>(
+            nameof(ShootCommand));
+    
+    public ObservableCollection<ObservableCollection<DisplaySquare>> Source
+    {
+        get => GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public ICommand ShootCommand
+    {
+        get => GetValue(ShootCommandProperty);
+        set => SetValue(ShootCommandProperty, value);
+    }
+
+    public static readonly StyledProperty<double> CellSizeProperty = AvaloniaProperty.Register<FleetBoardControl, double>(
+        nameof(CellSize), defaultValue: 30);
+
+    public double CellSize
+    {
+        get => GetValue(CellSizeProperty);
+        set => SetValue(CellSizeProperty, value);
+    }
+
+}
