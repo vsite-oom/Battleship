5e10e2707641d7eff3f96f5323ba28d27da1145d
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-03 12:48:10 +0200

    Create control for displaying messages

---
+++ GUI/GUI/Controls/MessageControl.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Vsite.Oom.Battleship.GUI.Models;
+
+namespace Vsite.Oom.Battleship.GUI.Controls;
+
+public partial class MessageControl : UserControl
+{
+    public MessageControl()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly StyledProperty<DisplayMessage> MessageProperty = AvaloniaProperty.Register<MessageControl, DisplayMessage>(
+        nameof(Message));
+
+    public DisplayMessage Message
+    {
+        get => GetValue(MessageProperty);
+        set => SetValue(MessageProperty, value);
+    }
+
+    public void CloseCommand()
+    {
+        Message.IsVisible = false;
+    }
+}
