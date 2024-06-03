1655ff8bd139c4a5fab2a4cb22aa1b2b8a4f0991
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-03 12:53:22 +0200

    Setup Dependency Injection

--- GUI/GUI/App.axaml.cs
+++ GUI/GUI/App.axaml.cs
@@ -1,15 +1,25 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Vsite.Oom.Battleship.GUI.Models;
 using Vsite.Oom.Battleship.GUI.ViewModels;
 using Vsite.Oom.Battleship.GUI.Views;
+using Vsite.Oom.Battleship.Model;
 
 namespace Vsite.Oom.Battleship.GUI;
 
 public partial class App : Application
 {
+    public App()
+    {
+        ConfigureServices();
+    }
+    
     public override void Initialize()
     {
+        
         AvaloniaXamlLoader.Load(this);
     }
 
@@ -19,17 +29,28 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = Ioc.Default.GetService<MainViewModel>()
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = Ioc.Default.GetService<MainViewModel>()
             };
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+    
+    private void ConfigureServices()
+    {
+        Ioc.Default.ConfigureServices(new ServiceCollection()
+            .AddTransient<MainViewModel>()
+            .AddTransient<GameViewModel>()
+            .AddTransient<NewGameViewModel>()
+            .AddScoped<SquareEliminator>()
+            .AddSingleton<Game>()
+            .BuildServiceProvider());
     }
 }
