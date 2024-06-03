abc9bb002e3143133ed48fb0abd7473fe461c4d3
Antonio Maletić <35150776+anmaletic@users.noreply.github.com>
2024-06-03 12:53:04 +0200

    Create GameView

---
+++ GUI/GUI/ViewModels/GameViewModel.cs
@@ -0,0 +1,68 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Vsite.Oom.Battleship.GUI.Messages;
+using Vsite.Oom.Battleship.GUI.Models;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.GUI.ViewModels;
+
+public partial class GameViewModel : ViewModelBase
+{
+    [ObservableProperty] private Game _game;
+
+    [ObservableProperty] private string _message = "";
+
+    public GameViewModel() : this(null!) { }
+    public GameViewModel(Game game)
+    {
+        _game = game;
+
+        _game.NewGame();
+    }
+
+    
+    [RelayCommand(CanExecute = nameof(CanShoot))]
+    private void Shoot(DisplaySquare target)
+    {
+        Game.PlayerAttack(target);
+
+        if (Game.IsGameOver(true))
+        {
+            Message = "You won!";
+            return;
+        }
+
+        OpponentShoot();
+    }
+    private bool CanShoot(DisplaySquare? square)
+    {
+        return square?.SquareState == SquareState.Intact;
+    }
+
+    private void OpponentShoot()
+    {
+        Game.OpponentAttack();
+
+        if (Game.IsGameOver(false))
+        {
+            Message = "You lost!";
+        }
+    }
+    
+    // Start new game with the same settings
+    [RelayCommand]
+    private void NewGame()
+    {
+        Message = "";
+        Game.NewGame();
+    }
+
+    // Change game settings
+    [RelayCommand]
+    private void ChangeSettings()
+    {
+        Message = "";
+        WeakReferenceMessenger.Default.Send(new ChangeSettingsMessage());
+    }
+}
---
+++ GUI/GUI/Views/GameView.axaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Layout;
+
+
+namespace Vsite.Oom.Battleship.GUI.Views;
+
+public partial class GameView : UserControl
+{
+    public GameView()
+    {
+        InitializeComponent();
+    }
+
+    private void Control_OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        var orientation = Bounds.Width > Bounds.Height ? Orientation.Horizontal : Orientation.Vertical;
+        
+        if(orientation == Orientation.Vertical)
+        {
+            ShotsBoard.SetValue(Grid.RowProperty, 1);
+            ShotsBoard.SetValue(Grid.ColumnProperty, 0);
+            
+            var vm = DataContext as ViewModels.GameViewModel;
+            
+            var cellSize = (Bounds.Width - 150) / vm!.Game.Columns;
+            
+            cellSize = cellSize < 15 ? 15 : cellSize;
+            cellSize = cellSize > 30 ? 30 : cellSize;
+            
+            BoardGrid.VerticalAlignment = VerticalAlignment.Top;
+            
+            FleetBoard.CellSize = cellSize;
+            ShotsBoard.CellSize = cellSize;
+        }
+        else
+        {
+            ShotsBoard.SetValue(Grid.RowProperty, 0);
+            ShotsBoard.SetValue(Grid.ColumnProperty, 1);
+            FleetBoard.CellSize = 30;
+            ShotsBoard.CellSize = 30;
+            
+            BoardGrid.VerticalAlignment = VerticalAlignment.Center;
+        }
+
+        if (OperatingSystem.IsAndroid())
+        {
+            BoardGrid.VerticalAlignment = VerticalAlignment.Center;
+        }
+    }
+}
