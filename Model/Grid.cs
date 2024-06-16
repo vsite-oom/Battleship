9d5d844d991917060ffc5f6176bc403c24da705d
Kristian Bolarić <kristian.bolaric@gmail.com>
2024-06-16 12:10:59 +0200

    Rename IsSquareAvailable

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -35,12 +35,12 @@ namespace vsite.oom.battleship.model
         {
             get { return squares.Cast<Square>().Where(s => s != null); }
         }
-        protected abstract bool isSquareAvailable(int row, int column);
+        protected abstract bool IsSquareAvailable(int row, int column);
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
 
-            return GetHorizontalAvailablePlacements(length);
+            return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
         }
 
         private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
@@ -51,7 +51,7 @@ namespace vsite.oom.battleship.model
                 int counter = 0;
                 for (int c = 0; c < Columns; c++)
                 {
-                    if (isSquareAvailable(r, c))
+                    if (IsSquareAvailable(r, c))
                     {
                         ++counter;
                         if (counter >= length)
@@ -75,20 +75,20 @@ namespace vsite.oom.battleship.model
         private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
         {
             List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
-            for (int r = 0; r < Rows; r++)
+            for (int c = 0; c < Columns; c++)
             {
                 int counter = 0;
-                for (int c = 0; c < Columns; c++)
+                for (int r = 0; r < Rows; r++)
                 {
-                    if (isSquareAvailable(r, c))
+                    if (IsSquareAvailable(r, c))
                     {
                         ++counter;
                         if (counter >= length)
                         {
                             List<Square> temp = new List<Square>();
-                            for (int c1 = c - length + 1; c1 <= c; ++c1)
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
                             {
-                                temp.Add(squares[r, c1]!);
+                                temp.Add(squares[r1, c]!);
                             }
                             result.Add(temp);
                         }
