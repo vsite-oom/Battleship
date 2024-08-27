da866a556f95e7088380b73fbbe953a8d192dca5
Hrvoje Janđel <hrjandje@vsite.hr>
2024-08-27 10:56:32 +0200

    Added welcome form

---
+++ Battleship/Welcome.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Battleship
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ToBattle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BATTLESHIP OF WAR";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ToBattle
+            // 
+            this.ToBattle.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ToBattle.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToBattle.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.ToBattle.Location = new System.Drawing.Point(312, 52);
+            this.ToBattle.Name = "ToBattle";
+            this.ToBattle.Size = new System.Drawing.Size(214, 82);
+            this.ToBattle.TabIndex = 1;
+            this.ToBattle.Text = "TO BATTLE";
+            this.ToBattle.UseVisualStyleBackColor = false;
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Battleship.Properties.Resources.battleship;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ToBattle);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Welcome";
+            this.Text = "BATTLESHIP OF WAR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ToBattle;
+    }
+}
+
