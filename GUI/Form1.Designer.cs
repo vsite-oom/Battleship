200a6b72267249e6a0efce51ae1509d3a9c58cbd
Filip Leš <files@vsite.hr>
2024-08-28 17:39:47 +0200

    Add grids and other buttons
    In Form1.Designer.cs
    - Added various buttons
    
    In Form1.cs:
    - Added CreateGrids method to generate 10x10 grids for fleet and shots
    - Added CreateButton helper method for custom button creation

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnIndicator1 = new Button();
+            btnIndicator2 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            btnPlaceFleet = new Button();
+            btnStartReset = new Button();
+            SuspendLayout();
+            // 
+            // btnIndicator1
+            // 
+            btnIndicator1.Location = new Point(325, 25);
+            btnIndicator1.Name = "btnIndicator1";
+            btnIndicator1.Size = new Size(50, 50);
+            btnIndicator1.TabIndex = 0;
+            btnIndicator1.UseVisualStyleBackColor = true;
+            // 
+            // btnIndicator2
+            // 
+            btnIndicator2.Location = new Point(925, 25);
+            btnIndicator2.Name = "btnIndicator2";
+            btnIndicator2.Size = new Size(50, 50);
+            btnIndicator2.TabIndex = 1;
+            btnIndicator2.UseVisualStyleBackColor = true;
+            // 
+            // btnPlaceFleet
+            // 
+            btnPlaceFleet.Location = new Point(200, 625);
+            btnPlaceFleet.Name = "btnPlaceFleet";
+            btnPlaceFleet.Size = new Size(100, 50);
+            btnPlaceFleet.TabIndex = 2;
+            btnPlaceFleet.Text = "Place Fleet";
+            btnPlaceFleet.UseVisualStyleBackColor = true;
+            // 
+            // btnStartReset
+            // 
+            btnStartReset.Location = new Point(350, 625);
+            btnStartReset.Name = "btnStartReset";
+            btnStartReset.Size = new Size(100, 50);
+            btnStartReset.TabIndex = 3;
+            btnStartReset.Text = "Start / Reset";
+            btnStartReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1265, 736);
+            Controls.Add(btnStartReset);
+            Controls.Add(btnPlaceFleet);
+            Controls.Add(btnIndicator2);
+            Controls.Add(btnIndicator1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnIndicator1;
+        private Button btnIndicator2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;  
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;  
+        private Button btnPlaceFleet;
+        private Button btnStartReset;
     }
 }
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -5,6 +5,33 @@ namespace GUI
         public Form1()
         {
             InitializeComponent();
+            CreateGrids();
+        }
+
+        private void CreateGrids()
+        {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++)
+                {
+                    int FleetGridLeftMargin = 100;
+                    int ShotsGridLeftMargin = 700;
+                    int topMargin = 100;
+                    Controls.Add(CreateButton(i, j, FleetGridLeftMargin, topMargin));
+                    Controls.Add(CreateButton(i, j, ShotsGridLeftMargin, topMargin));
+                }
+            }
+        }
+
+        private CustomButton CreateButton(int i, int j, int leftMargin, int topMargin)
+        {
+            CustomButton button = new CustomButton(i, j);
+            button.Location = new Point(leftMargin + i * 50, topMargin + j * 50);
+            button.Name = "button" + i + j;
+            button.Size = new Size(50, 50);
+            button.TabIndex = i * 10 + j;
+            button.Text = "";
+            button.UseVisualStyleBackColor = true;
+            return button;
         }
     }
 }
