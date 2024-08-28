9ab4dbcb1b6e0c2bbd8d205d8d8dc026681d1df4
Filip Leš <files@vsite.hr>
2024-08-28 13:43:08 +0200

    Add CustomButton class with row and column properties
    Introduce CustomButton.cs in GUI namespace, inheriting from Button.
    Includes read-only Row and Column properties set via constructor.

---
+++ GUI/CustomButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal class CustomButton : Button
+    {
+        public CustomButton(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+    }
+}
