1c1e4119a5c1c6ee9dd036c55dd58982539737ff
Hrvoje Janđel <hrjandje@vsite.hr>
2024-09-02 13:11:06 +0200

    Add all strings to one class

---
+++ BattleGame/StringFile.cs
@@ -0,0 +1,24 @@
+﻿namespace Vsite.Oom.Battleship.Game
+{
+    public static class StringFile
+    {
+        public static readonly string igrac = "Igrač";
+        public static readonly string neprijatelj = "neprijatelj";
+        public static readonly string gridColumnLetters = "ABCDEFGHIJ";
+        public static readonly string mojiBodoviText = "PLAYER BODOVI";
+        public static readonly string aiBodoviText = "AI BODOVI";
+        public static readonly string startText = "START";
+        public static readonly string restartText = "RESTART";
+        public static readonly string baseAudioPath = @"C:\oom\Battleship\music";
+        public static readonly string windowsAudioPath = @"C:\Windows\Media\";
+        public static readonly string singleGunshot = "single-gunshot.wav";
+        public static readonly string singleHit = "single-shot-hit.wav";
+        public static readonly string startReset = "chimes.wav";
+        public static readonly string win = "WIN";
+        public static readonly string poraz = "PORAZ";
+        public static readonly string gameOver = "Game Over!";
+        public static readonly string neprijateljskaFlotaMsg = "Neprijateljska flota nije postavljena!";
+        public static readonly string soundFileMsg = "Sound file not found: ";
+        public static readonly string combatBtn = "CombatButton_";
+    }
+}
