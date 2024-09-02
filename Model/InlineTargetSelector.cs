609711e6783dd250fee8f127f0ab11ec9ea36672
Hrvoje Janđel <hrjandje@vsite.hr>
2024-09-02 16:42:12 +0200

    fix the inline target selector

--- Model/InlineTargetSelector.cs
+++ Model/InlineTargetSelector.cs
@@ -39,18 +39,15 @@ namespace Vsite.Oom.Battleship.Model
             //vertical
             else
             {
-                if (sorted.First().Row == sorted.Last().Row)
+                var up = grid.GetSquaresInDirection(sorted.First().Row, sorted.First().Column, Direction.Upwards);
+                if (up.Any())
                 {
-                    var up = grid.GetSquaresInDirection(sorted.First().Row, sorted.First().Column, Direction.Upwards);
-                    if (up.Any())
-                    {
-                        directionCandidates.Add(up);
-                    }
-                    var down = grid.GetSquaresInDirection(sorted.Last().Row, sorted.Last().Column, Direction.Downwards);
-                    if (down.Any())
-                    {
-                        directionCandidates.Add(down);
-                    }
+                    directionCandidates.Add(up);
+                }
+                var down = grid.GetSquaresInDirection(sorted.Last().Row, sorted.Last().Column, Direction.Downwards);
+                if (down.Any())
+                {
+                    directionCandidates.Add(down);
                 }
             }
            var groupedByLength = directionCandidates.GroupBy(l=>l.Count());
