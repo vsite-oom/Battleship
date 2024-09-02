e33c2d7afca84e48d2107cf9c68af90eb2d707bc
Hrvoje Janđel <hrjandje@vsite.hr>
2024-09-02 16:41:10 +0200

    fix grid

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -36,7 +36,11 @@ namespace Vsite.Oom.Battleship.Model
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
+            var horizontalPlacements = GetHorizontalAvailablePlacements(length);
+            var verticalPlacements = GetVerticalAvailablePlacements(length);
+            var allPlacements = horizontalPlacements.Concat(verticalPlacements);
+
+            return allPlacements;
         }
 
         protected abstract bool IsSquareAvailable(int row, int column);
@@ -45,15 +49,15 @@ namespace Vsite.Oom.Battleship.Model
         {
             List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
 
-            for (int r = 0; r < Rows; ++r)
+            for (int r = 0; r < Rows; r++)
             {
                 var queue = new LimitedQueue<Square>(length);
-                for (int c = 0; c < Columns; ++c)
+                for (int c = 0; c < Columns; c++)
                 {
                     if (IsSquareAvailable(r, c))
                     {
                         queue.Enqueue(squares[r, c]!);
-                        if (queue.Count() == length)
+                        if (queue.Count() >= length)
                         {
                             result.Add(queue.ToArray());
                         }
@@ -71,15 +75,15 @@ namespace Vsite.Oom.Battleship.Model
         {
             List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
 
-            for (int c = 0; c < Columns; ++c)
+            for (int c = 0; c < Columns; c++)
             {
                 var queue = new LimitedQueue<Square>(length);
-                for (int r = 0; r < Rows; ++r)
+                for (int r = 0; r < Rows; r++)
                 {
                     if (IsSquareAvailable(r, c))
                     {
                         queue.Enqueue(squares[r, c]!);
-                        if (queue.Count() == length)
+                        if (queue.Count() >= length)
                         {
                             result.Add(queue.ToArray());
                         }
