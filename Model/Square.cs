16f12119443d511bc2372ee2e8d92e80cc1af635
Tomislav Prstec <toprstec@vsite.hr>
2025-04-02 20:45:20 +0200

    Square constructor implementation and first unit test.

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,5 +1,15 @@
-﻿namespace Model;
+﻿using System.Data;
+
+namespace Model;
 
 public class Square
 {
+    public readonly int Row;
+    public readonly int Column;
+
+    public Square(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
 }
---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+
+namespace ModelTests;
+
+[TestClass]
+public class SquareTests
+{
+    [TestMethod]
+    public void ConstructorCreateSquareWithRowAndColumnProvided()
+    {
+        int row = 4;
+        int column = 8;
+
+        var square = new Square(row, column);
+
+        Assert.AreEqual(row, square.Row);
+        Assert.AreEqual(column, square.Column);
+    }
+}
