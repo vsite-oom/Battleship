83a66e95f8a30bbbdcab8465f92d5039e2749fb9
KriZa96 <kriza9605@gmail.com>
2025-04-02 20:45:31 +0200

    Defined first test

--- Model/Square.cs
+++ Model/Square.cs
@@ -8,4 +8,13 @@ namespace Model;
 
 public class Square
 {
+    public readonly int Row;
+    public readonly int Column;
+
+    public Square(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
 }
---
+++ ModelTests/SquareTests.cs
@@ -0,0 +1,19 @@
+﻿
+using Model;
+
+namespace ModelTests;
+
+[TestClass]
+public class SquareTests
+{
+    public void ConstructorCreateSquareWithRowAndColumnProvided()
+    {
+        int row = 1;
+        int column = 2;
+
+        Square square = new Square(row, column);
+
+        Assert.AreEqual(row, square.Row);
+        Assert.AreEqual(column, square.Column);
+    }
+}
