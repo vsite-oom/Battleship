f9fa3b4949204149a272eb529a29196b7796e019
breski-igor <igor.breski@vsite.hr>
2025-04-02 20:45:34 +0200

    Square test and first test

---
+++ ModelTest/SquareTests.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+
+namespace ModelTest;
+
+
+[TestClass]
+public class SquareTests
+{
+    [TestMethod]
+    public void ConstructorCreateSquareWithRowAndColumnProvided()
+    {
+        int row = 1;
+        int column = 2;
+
+        var square = new Square(row, column);
+        Assert.AreEqual(row, square.Row);
+        Assert.AreEqual(column, square.Column);
+
+    }
+}
