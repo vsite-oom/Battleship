33926cf13d2a191e93e72d314e42e3934e23b531
Tomislav Prstec <toprstec@vsite.hr>
2025-04-02 20:57:03 +0200

    Added Grid and GridTests.

---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+
+namespace ModelTests;
+
+[TestClass]
+public class GridTests
+{
+    public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+    {
+        int rows = 5;
+        int columns = 10;
+        var grid = new Grid(rows, columns);
+        
+        Assert.AreEqual(50, grid.Squares.Count());
+    }
+}
