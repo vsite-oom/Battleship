a9e43bf4ed01f8b329f7034f5ce0c97bc7c0909e
Doris Groznica <dogroznica@vsite.hr>
2025-04-09 20:23:32 +0200

    Resolved errors.

---
+++ ModelTests/GridTests.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelTests;
+[TestClass]
+public class GridTests
+{
+    [TestMethod]
+    public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+    {
+        int rows = 5;
+        int columns = 10;
+
+        var grid = new Grid(rows, columns);
+        Assert.AreEqual(50, grid.Squares.Count());
+    }
+
+
+}
---
+++ ModelTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿using Model;
+namespace ModelTests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
