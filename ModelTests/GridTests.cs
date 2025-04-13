6fe3bb0d24343fb7c97e1d97f37011ecf6ed00a5
Tomislav Prstec <toprstec@vsite.hr>
2025-04-13 16:37:11 +0200

    Implement GetAvailablePlacements for horizontal ships.

--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -1,16 +1,27 @@
-﻿using Model;
-
-namespace ModelTests;
-
+﻿namespace ModelTests;
 [TestClass]
 public class GridTests
 {
+    [TestMethod]
     public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
     {
         int rows = 5;
         int columns = 10;
         var grid = new Grid(rows, columns);
-        
+
         Assert.AreEqual(50, grid.Squares.Count());
     }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid1x5Returns3PlacementsForShipWith3Squares()
+    {
+        int rows = 1;
+        int columns = 5;
+        int shipLength = 3;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
 }
