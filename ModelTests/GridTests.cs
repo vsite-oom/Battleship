3747bbf514fbc42a198447cabd7ed59ce6644dbd
Tomislav Prstec <toprstec@vsite.hr>
2025-04-13 16:44:47 +0200

    Implement GetVerticalAvailablePlacements for vertical ships.

--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -1,4 +1,5 @@
 ﻿namespace ModelTests;
+
 [TestClass]
 public class GridTests
 {
@@ -7,6 +8,7 @@ public class GridTests
     {
         int rows = 5;
         int columns = 10;
+
         var grid = new Grid(rows, columns);
 
         Assert.AreEqual(50, grid.Squares.Count());
@@ -22,6 +24,29 @@ public class GridTests
         var grid = new Grid(rows, columns);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid5x1Returns3PlacementsForShipWith3Squares()
+    {
+        int rows = 5;
+        int columns = 1;
+        int shipLength = 3;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid5x5Returns20PlacementsForShipWith4Squares()
+    {
+        int rows = 5;
+        int cols = 5;
+        int shipLength = 4;
+        var grid = new Grid(rows, cols);
+
+        Assert.AreEqual(20, grid.GetAvailablePlacements(shipLength).Count());
     }
 
 }
