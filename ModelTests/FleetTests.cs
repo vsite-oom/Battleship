443f6ac9be9bbe865a67550412cdd1c7392052f1
Tomislav Prstec <toprstec@vsite.hr>
2025-05-08 18:26:16 +0200

    Add ships to Fleet class.

---
+++ ModelTests/FleetTests.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+
+namespace ModelTests;
+
+[TestClass]
+public class FleetTests
+{
+    [TestMethod]
+    public void ConstructorCreatesEmptyFleet()
+    {
+        var fleet = new Fleet();
+
+        Assert.AreEqual(0, fleet.Ships.Count());
+    }
+}
