e66afcf9506f22f9f2d7d202ac14cd31ea8965f5
Tomislav Prstec <toprstec@vsite.hr>
2025-05-08 18:24:02 +0200

    Replaced loops with LimitedQueue in Grid and added test when squares are eliminated.

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,13 +1,6 @@
-﻿using Model;
-using System.Linq;
-
+﻿namespace Model;
 public class Grid
 {
-    public readonly int Rows;
-    public readonly int Columns;
-
-    private readonly Square?[,] squares;
-
     public Grid(int rows, int columns)
     {
         Rows = rows;
@@ -23,6 +16,11 @@ public class Grid
             }
         }
     }
+
+    public readonly int Rows;
+    public readonly int Columns;
+
+    private readonly Square?[,] squares;
 
     public IEnumerable<Square> Squares
     {
@@ -40,25 +38,20 @@ public class Grid
 
         for (int r = 0; r < Rows; ++r)
         {
-            int counter = 0;
+            var queue = new LimitedQueue<Square>(length);
             for (int c = 0; c < Columns; ++c)
             {
                 if (squares[r, c] != null)
                 {
-                    ++counter;
-                    if (counter >= length)
+                    queue.Enqueue(squares[r, c]!);
+                    if (queue.Count() == length)
                     {
-                        List<Square> temp = new List<Square>();
-                        for (int c1 = c - length + 1; c1 <= c; ++c1)
-                        {
-                            temp.Add(squares[r, c1]!);
-                        }
-                        result.Add(temp);
+                        result.Add(queue.ToArray());
                     }
                 }
                 else
                 {
-                    counter = 0;
+                    queue.Clear();
                 }
             }
         }
@@ -69,30 +62,29 @@ public class Grid
     {
         List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
 
-        for (int c = 0; c < Columns; c++)
+        for (int c = 0; c < Columns; ++c)
         {
-            int counter = 0;
-            for (int r = 0; r < Rows; r++)
+            var queue = new LimitedQueue<Square>(length);
+            for (int r = 0; r < Rows; ++r)
             {
                 if (squares[r, c] != null)
                 {
-                    ++counter;
-                    if (counter >= length)
+                    queue.Enqueue(squares[r, c]!);
+                    if (queue.Count() == length)
                     {
-                        List<Square> temp = new List<Square>();
-                        for (int r1 = r - length + 1; r1 <= r; ++r1)
-                        {
-                            temp.Add(squares[r1, c]!);
-                        }
-                        result.Add(temp);
+                        result.Add(queue.ToArray());
                     }
                 }
                 else
                 {
-                    counter = 0;
+                    queue.Clear();
                 }
             }
         }
         return result;
+    }
+    public void EleminateSquare(int row, int column)
+    {
+        squares[row, column] = null;
     }
 }
