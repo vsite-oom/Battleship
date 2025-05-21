3c3f39330cfe4bd086bd5e12955267ce661e50c1
Tomislav Prstec <toprstec@vsite.hr>
2025-05-21 19:47:56 +0200

    Add square states.

---
+++ Model/ShotsGrid.cs
@@ -0,0 +1,13 @@
+﻿namespace Model;
+
+public class ShotsGrid : Grid
+{
+    public ShotsGrid(int rows, int columns) : base(rows, columns)
+    {
+    }
+
+    protected override bool IsSquareAvailable(int row, int column)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -2,22 +2,39 @@
 
 namespace Model;
 
+public enum SquareState
+{
+    Intact,
+    Eliminated,
+    Missed,
+    Hit,
+    Sunken
+}
 public class Square
 {
-    public readonly int Row;
-    public readonly int Column;
-
     public Square(int row, int column)
     {
         Row = row;
         Column = column;
+        SquareState = SquareState.Intact;
     }
+
+    public readonly int Row;
+    public readonly int Column;
+
     public void Hit()
     {
-        hit = true;
+        SquareState = SquareState.Hit;
     }
 
-    public bool IsHit => hit;
-
-    private bool hit = false;
+    public void ChangeState(SquareState newState)
+    {
+        if ((int)newState > (int)SquareState)
+        {
+            SquareState = newState;
+        }
+    }
+    public bool IsHit => (int)SquareState >= (int)SquareState.Hit;
+    public SquareState SquareState { get; private set; }
 }
+
