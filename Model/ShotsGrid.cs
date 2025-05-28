81295872aa4172969a829c54ba4025584af49fbf
Tomislav Prstec <toprstec@vsite.hr>
2025-05-28 19:52:07 +0200

    Add tests for ShotsGrid.

--- Model/ShotsGrid.cs
+++ Model/ShotsGrid.cs
@@ -1,18 +1,29 @@
 ﻿namespace Model;
 
+public enum Direction
+{
+    Upwards,
+    Rightwards,
+    Downwards,
+    Leftwards
+}
+
 public class ShotsGrid : Grid
 {
     public ShotsGrid(int rows, int columns) : base(rows, columns)
     {
     }
-
     protected override bool IsSquareAvailable(int row, int column)
     {
         return squares[row, column]?.SquareState == SquareState.Intact;
     }
-
     public void ChangeSquareState(int row, int column, SquareState newState)
     {
         squares[row, column]!.ChangeState(newState);
+    }
+
+    public IEnumerable<Square> GetSquaresInDirection(int row, int column, Direction upwards)
+    {
+        throw new NotImplementedException();
     }
 }
