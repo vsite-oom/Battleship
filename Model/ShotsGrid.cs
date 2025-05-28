35393d4060192ae74d0afe86125fc3f104da69d8
Doris Groznica <dogroznica@vsite.hr>
2025-05-28 22:44:27 +0200

    Add square states.

---
+++ Model/ShotsGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public class ShotsGrid : Grid
+{
+    public ShotsGrid(int rows, int columns) : base(rows, columns)
+    {
+    }
+
+    protected override bool IsSquareAvailable(int row, int column)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,23 +8,47 @@ using System.Threading.Tasks;
 namespace Model;
 
 public class Square
+public enum SquareState
 {
     public readonly int Row;
-    public readonly int Column;
-    
+public readonly int Column;
+Intact,
+    Eliminated,
+    Missed,
+    Hit,
+    Sunken
+}
+
+public class Square
+{
     public Square(int row, int column)
     {
         Row = row;
         Column = column;
+        SquareState = SquareState.Intact;
     }
+
+    public readonly int Row;
+    public readonly int Column;
 
     public void Hit()
     {
         hit = true;
+        SquareState = SquareState.Hit;
+    }
+
+    public void ChangeState(SquareState newState)
+    {
+        if ((int)newState > (int)SquareState)
+        {
+            SquareState = newState;
+        }
     }
 
     public bool IsHit => hit;
+    public bool IsHit => (int)SquareState >= (int)SquareState.Hit;
 
     private bool hit = false;
 }
-
+    public SquareState SquareState { get; private set; }
+}
