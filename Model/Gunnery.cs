fa8dac8ad21ad3f4fc664fc5d34cdac1820e41f8
Doris Groznica <dogroznica@vsite.hr>
2025-05-28 22:03:53 +0200

    Sart Gunnery implementation.

---
+++ Model/Gunnery.cs
@@ -0,0 +1,39 @@
+﻿using Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public enum ShootingTactics
+{
+    Random,
+    Surrounding,
+    Inline
+}
+
+public class Gunnery
+{
+    public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+    {
+        recordGrid = new Grid(rows, columns);
+    }
+
+    public SquareCoordinate Next()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ProcessHitResult(HitResult hitResult)
+    {
+
+    }
+
+    public ShootingTactics ShootingTactics { get; private set; } = ShootingTactics.Random;
+
+    private readonly Grid recordGrid;
+
+    private ITargetSelector targetSelector = new RandomTargetSelector();
+}
