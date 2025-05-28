93b1b988ff741fa8c3959a32f07118f230eebc79
Doris Groznica <dogroznica@vsite.hr>
2025-05-28 21:53:20 +0200

    Implement Hit method in Ship class.

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -34,10 +34,21 @@ public class Ship
     {
         if (Contains(row, column) == false)
         {
+            var square = Squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column);
+            if (square == null)
             return HitResult.Missed;
         }
 
         throw new NotImplementedException();
+        square.Hit();
+
+        if (Squares.All(sq => sq.IsHit))
+        {
+            return HitResult.Sunken;
+        }
+
+        return HitResult.Hit;
     }
 }
+
 
