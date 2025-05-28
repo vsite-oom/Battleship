f70a8696923d7fce11ddfab93dc3ed89ac9789c1
Tomislav Prstec <toprstec@vsite.hr>
2025-05-28 19:50:49 +0200

    Change a way ShotsGrid sqare state is changed.

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -29,9 +29,12 @@ public class Ship
         square.Hit();
         if (Squares.All(sq => sq.IsHit))
         {
+            foreach (var sq in Squares)
+            {
+                sq.ChangeState(SquareState.Sunken);
+            }
             return HitResult.Sunken;
         }
-
          return HitResult.Hit;
     }
 }
