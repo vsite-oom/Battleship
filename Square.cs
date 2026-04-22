fe2338f8d2fa2b79f2099a92b4f8f763351fc78e
Ivan Ceranac <ivcerana@vsite.hr>
2026-04-22 19:05:40 +0200

    Square constructor implementation SquareTest added

--- Square.cs
+++ Square.cs
@@ -8,4 +8,12 @@ namespace Model;
 
 public class Square
 {
+    public readonly int Row;
+    public readonly int Column;
+
+    public Square(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
 }
