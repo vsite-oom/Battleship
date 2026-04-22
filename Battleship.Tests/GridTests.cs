c2bb356dab90b264c408994330a53c31867ec961
Paolo Stabile <pastabil@vsite.hr>
2026-04-22 14:56:42 +0200

    Grid constructor implementation and unit test.

---
+++ Battleship.Tests/GridTests.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Model;
+
+namespace Battleship.Tests;
+
+[TestClass]
+public class GridTests
+{
+    [TestMethod]
+    public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+    {
+        int rows = 5;
+        int columns = 10;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(50, grid.Squares.Count());
+    }
+}
