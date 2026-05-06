0446de1cfd835b1bbe65901595525487bd89f7bd
Paolo Stabile <pastabil@vsite.hr>
2026-05-06 16:46:01 +0200

    Add SquareEliminator class.

---
+++ Battleship.Model/SquareCoordinate.cs
@@ -0,0 +1,13 @@
+﻿namespace Battleship.Model;
+
+public class SquareCoordinate
+{
+    public SquareCoordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public readonly int Row;
+    public readonly int Column;
+}
