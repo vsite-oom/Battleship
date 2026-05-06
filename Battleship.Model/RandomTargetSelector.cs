0c24719968280031aeb7607c83c82c3f7c987539
Paolo Stabile <pastabil@vsite.hr>
2026-05-06 17:40:55 +0200

    Start Gunnery implementation.

---
+++ Battleship.Model/RandomTargetSelector.cs
@@ -0,0 +1,9 @@
+﻿namespace Battleship.Model;
+
+public class RandomTargetSelector : ITargetSelector
+{
+    public SquareCoordinate Next()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Battleship.Model/SurroundingTargetSelector.cs
@@ -0,0 +1,9 @@
+﻿namespace Battleship.Model;
+
+public class SurroundingTargetSelector : ITargetSelector
+{
+    public SquareCoordinate Next()
+    {
+        throw new NotImplementedException();
+    }
+}
