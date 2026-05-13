e3bafccaa87636b7e6233c4ca558f60dbe2faec6
Paolo Stabile <pastabil@vsite.hr>
2026-05-13 12:00:00 +0200

    Implement shooting result processing.

--- Battleship.Model/Gunnery.cs
+++ Battleship.Model/Gunnery.cs
@@ -13,7 +13,7 @@ public class Gunnery
 {
     public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
     {
-        recordGrid = new FleetGrid(rows, columns);
+        recordGrid = new ShotsGrid(rows, columns);
         this.shipLengths = new List<int>(shipLengths.OrderDescending());
         targetSelector = new RandomTargetSelector(recordGrid, this.shipLengths[0]);
     }
@@ -26,6 +26,7 @@ public class Gunnery
 
     public void ProcessHitResult(HitResult hitResult)
     {
+        RecordTargetResult(hitResult);
         switch (hitResult)
         {
             case HitResult.Missed:
@@ -51,6 +52,38 @@ public class Gunnery
         }
     }
 
+    private void RecordTargetResult(HitResult hitResult)
+    {
+        switch (hitResult)
+        {
+            case HitResult.Missed:
+                target.ChangeState(SquareState.Missed);
+                return;
+            case HitResult.Hit:
+                target.ChangeState(SquareState.Hit);
+                shipSquares.Add(target);
+                return;
+            case HitResult.Sunken:
+                MarkShipSunken();
+                return;
+        }
+    }
+
+    private void MarkShipSunken()
+    {
+        shipSquares.Add(target);
+        foreach (var square in shipSquares)
+        {
+            square.ChangeState(SquareState.Sunken);
+        }
+        var toEliminate = eliminator.ToEliminate(shipSquares, recordGrid.Rows, recordGrid.Columns);
+        foreach (var square in toEliminate)
+        {
+            recordGrid.GetSquare(square.Row, square.Column).ChangeState(SquareState.Eliminated);
+        }
+        shipSquares.Clear();
+    }
+
     private void ChangeTacticsToRandom()
     {
         ShootingTactics = ShootingTactics.Random;
@@ -71,11 +104,15 @@ public class Gunnery
 
     public ShootingTactics ShootingTactics { get; private set; } = ShootingTactics.Random;
 
-    private readonly FleetGrid recordGrid;
+    private readonly ShotsGrid recordGrid;
 
     private readonly List<int> shipLengths = [];
+
+    private List<Square> shipSquares = new List<Square>();
 
     private Square target;
 
     private ITargetSelector targetSelector;
+
+    private readonly SquareEliminator eliminator = new SquareEliminator();
 }
