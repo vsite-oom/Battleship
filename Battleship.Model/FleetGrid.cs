4e8125cc3d492e686143f9d10a66e4e0d440b490
Paolo Stabile <pastabil@vsite.hr>
2026-05-13 12:00:00 +0200

    Rename Grid to FleetGrid.

--- Battleship.Model/FleetGrid.cs
+++ Battleship.Model/FleetGrid.cs
@@ -1,8 +1,8 @@
 ﻿namespace Battleship.Model;
 
-public class Grid
+public class FleetGrid
 {
-    public Grid(int rows, int columns)
+    public FleetGrid(int rows, int columns)
     {
         Rows = rows;
         Columns = columns;
--- Battleship.Tests/GridTests.cs
+++ Battleship.Tests/GridTests.cs
@@ -11,7 +11,7 @@ public class GridTests
         int rows = 5;
         int columns = 10;
 
-        var grid = new Grid(rows, columns);
+        var grid = new FleetGrid(rows, columns);
 
         Assert.AreEqual(50, grid.Squares.Count());
     }
@@ -23,7 +23,7 @@ public class GridTests
         int columns = 5;
         int shipLength = 3;
 
-        var grid = new Grid(rows, columns);
+        var grid = new FleetGrid(rows, columns);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
     }
@@ -35,7 +35,7 @@ public class GridTests
         int columns = 1;
         int shipLength = 3;
 
-        var grid = new Grid(rows, columns);
+        var grid = new FleetGrid(rows, columns);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
     }
@@ -46,7 +46,7 @@ public class GridTests
         int rows = 5;
         int cols = 5;
         int shipLength = 4;
-        var grid = new Grid(rows, cols);
+        var grid = new FleetGrid(rows, cols);
 
         Assert.AreEqual(20, grid.GetAvailablePlacements(shipLength).Count());
     }
@@ -57,7 +57,7 @@ public class GridTests
         int rows = 1;
         int cols = 6;
         int shipLength = 2;
-        var grid = new Grid(rows, cols);
+        var grid = new FleetGrid(rows, cols);
         grid.EleminateSquare(0, 3);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
@@ -69,7 +69,7 @@ public class GridTests
         int rows = 8;
         int cols = 1;
         int shipLength = 2;
-        var grid = new Grid(rows, cols);
+        var grid = new FleetGrid(rows, cols);
         grid.EleminateSquare(3, 0);
         grid.EleminateSquare(5, 0);
 
