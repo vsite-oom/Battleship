0cb2b7abbe576c6205b7d25445545ae1b2f851f4
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 18:53:18 +0200

    Add square states.

---
+++ Battleship.Model/ShootGrid.cs
@@ -0,0 +1,13 @@
+﻿namespace Battleship.Model;
+
+public class ShotsGrid : Grid
+{
+    public ShotsGrid(int rows, int columns) : base(rows, columns)
+    {
+    }
+
+    protected override bool IsSquareAvailable(int row, int column)
+    {
+        throw new NotImplementedException();
+    }
+}
