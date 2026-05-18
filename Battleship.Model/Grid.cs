80a03bd148cc6a857176ffe94514d76fcd7a03ef
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 17:53:25 +0200

    Implement GetVerticalAvailablePlacements for vertical ships.

--- Battleship.Model/Grid.cs
+++ Battleship.Model/Grid.cs
@@ -35,14 +35,14 @@ namespace Battleship.Model
 
         public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
         {
-            return GetHorizontalAvailablePlacements(length);
+            return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
         }
 
         private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
         {
             List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
 
-            for (int r = 0; r < Rows; r++)
+            for (int r = 0; r < Rows; ++r)
             {
                 int counter = 0;
                 for (int c = 0; c < Columns; c++)
@@ -56,6 +56,36 @@ namespace Battleship.Model
                             for (int c1 = c - length + 1; c1 <= c; ++c1)
                             {
                                 temp.Add(squares[r, c1]!);
+                            }
+                            result.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+            return result;
+        }
+        private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+        {
+            List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+            for (int c = 0; c < Columns; c++)
+            {
+                int counter = 0;
+                for (int r = 0; r < Rows; r++)
+                {
+                    if (squares[r, c] != null)
+                    {
+                        ++counter;
+                        if (counter >= length)
+                        {
+                            List<Square> temp = new List<Square>();
+                            for (int r1 = r - length + 1; r1 <= r; ++r1)
+                            {
+                                temp.Add(squares[r1, c]!);
                             }
                             result.Add(temp);
                         }
