28e36dea07c79448c7fe50f4d7ebe70481ff301c
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 19:05:57 +0200

    Fix tests for tactics change. And fix tests for FleetGrid

--- Battleship.Tests/FleetGridTests.cs
+++ Battleship.Tests/FleetGridTests.cs
@@ -55,7 +55,7 @@ public class FleetGridTests
         int cols = 6;
         int shipLength = 2;
         var grid = new FleetGrid(rows, cols);
-        grid.EleminateSquare(0, 3);
+        grid.EliminateSquare(0, 3);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
     }
@@ -67,8 +67,8 @@ public class FleetGridTests
         int cols = 1;
         int shipLength = 2;
         var grid = new FleetGrid(rows, cols);
-        grid.EleminateSquare(3, 0);
-        grid.EleminateSquare(5, 0);
+        grid.EliminateSquare(3, 0);
+        grid.EliminateSquare(5, 0);
 
         Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
     }
