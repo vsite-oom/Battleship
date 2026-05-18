35df1eabad86f3c9e10ad5000a01da1b269fef1f
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 17:13:28 +0200

    Grid Constructor implementation and first unit test

---
+++ Battleship.Tests/GridTests.cs
@@ -0,0 +1,20 @@
+﻿using Battleship.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Battleship.Tests;
+
+[TestClass]
+public class GridTests
+{
+    [TestMethod]
+    public void ConstructorCreatesGridWith50SquaresFor5RowsAnd10Columns()
+    {
+        int rows = 5;
+        int columns = 10;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(50, grid.Squares.Count());
+    }
+}
--- Battleship.Tests/SquareTests.cs
+++ Battleship.Tests/SquareTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Battleship.Model;
 
 namespace Battleship.Tests;
 
