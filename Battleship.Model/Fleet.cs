2c4aa39cecae6ac6ceace017609b93c030b6ceb8
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 18:10:25 +0200

    Implement CreateShip method in Fleet class.

--- Battleship.Model/Fleet.cs
+++ Battleship.Model/Fleet.cs
@@ -9,5 +9,12 @@ namespace Battleship.Model
         private List<Ship> ships = new List<Ship>();
 
         public IEnumerable<Ship> Ships { get { return ships; } }
+
+
+        public void CreateShip(List<Square> squares)
+        {
+            var ship = new Ship(squares);
+            ships.Add(ship);
+        }
     }
 }
