bab4a765cd266174eae60e6ab8ad440391fbf6c3
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 18:33:19 +0200

    Implement Hit method for Fleet.

--- Battleship.Model/Fleet.cs
+++ Battleship.Model/Fleet.cs
@@ -6,7 +6,7 @@ namespace Battleship.Model
 {
     public class Fleet
     {
-        private List<Ship> ships = new List<Ship>();
+        private readonly List<Ship> ships = [];
 
         public IEnumerable<Ship> Ships { get { return ships; } }
 
@@ -19,7 +19,15 @@ namespace Battleship.Model
 
         public HitResult Hit(int row, int column)
         {
-            throw new NotImplementedException();
+            foreach (var ship in ships)
+            {
+                var result = ship.Hit(row, column);
+                if (result != HitResult.Missed)
+                {
+                    return result;
+                }
+            }
+            return HitResult.Missed;
         }
     }
 }
