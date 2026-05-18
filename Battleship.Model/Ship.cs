310860be392e906bf553496b8a180a2adfd7e8d5
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 18:22:35 +0200

    Implement Hit method in Ship class.

--- Battleship.Model/Ship.cs
+++ Battleship.Model/Ship.cs
@@ -26,12 +26,20 @@ namespace Battleship.Model
         }
         public HitResult Hit(int row, int column)
         {
-            if (Contains(row, column) == false)
+            var square = Squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column);
+            if (square == null)
             {
                 return HitResult.Missed;
             }
 
-            throw new NotImplementedException();
+            square.Hit();
+
+            if (Squares.All(sq => sq.IsHit))
+            {
+                return HitResult.Sunken;
+            }
+
+            return HitResult.Hit;
         }
     }
 }
