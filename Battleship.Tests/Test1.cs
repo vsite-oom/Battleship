e58c92f206a934575b7d4855bd8fc98b37a548a2
Sime Sprljan <sime.sprljan@vsite.hr>
2026-05-18 17:05:39 +0200

    Add Classes for building fleet and unit test project

---
+++ Battleship.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Battleship.Tests
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
