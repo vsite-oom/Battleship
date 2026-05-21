516c7e543e60ba32c16c1cc95ddcfa2a23746e91
Ivan Ceranac <ivcerana@vsite.hr>
2026-05-21 14:57:30 +0200

    add square eliminator class and test

---
+++ model/SquareCoordinate.cs
@@ -0,0 +1,13 @@
+﻿namespace model;
+
+public class SquareCoordinate
+{
+    public SquareCoordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public readonly int Row;
+    public readonly int Column;
+}
