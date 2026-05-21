e767d1e0b392610cf2a367f9a5c63b3a0744aa53
Ivan Ceranac <ivcerana@vsite.hr>
2026-05-21 15:07:45 +0200

    starting shooting implementation

--- model/Ship.cs
+++ model/Ship.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace model;
 
+public enum HitResult
+{
+    Missed,
+    Hit,
+    Sunken
+}
+
 public class Ship
 {
     public Ship(IEnumerable<Square> squares)
@@ -19,4 +26,22 @@ public class Ship
     {
         return Squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column) != null;
     }
+    public HitResult Hit(int row, int column)
+    {
+        var square = Squares.FirstOrDefault(sq => sq.Row == row && sq.Column == column);
+        if (square == null)
+        {
+            return HitResult.Missed;
+        }
+
+        square.Hit();
+
+        if (Squares.All(sq => sq.IsHit))
+        {
+            return HitResult.Sunken;
+        }
+
+        return HitResult.Hit;
+    }
+
 }
--- model/Square.cs
+++ model/Square.cs
@@ -16,4 +16,12 @@ public class Square
         Row = row;
         Column = column;
     }
+    public void Hit()
+    {
+        hit = true;
+    }
+
+    public bool IsHit => hit;
+
+    private bool hit = false;
 }
